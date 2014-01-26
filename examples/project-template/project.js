1364ca654efd06d2e6d47005ed748184a37efe90
Petr Volny <petr.volny@gooddata.com>
2014-01-26 22:24:55 +0100

    CODE: Added project-template example with bare minimum code with login

---
+++ examples/project-template/project.js
@@ -0,0 +1,16 @@
+// Copyright (C) 2007-2013, GoodData(R) Corporation. All rights reserved.
+var projectId = 'GoodSalesDemo',
+    user = '',
+    passwd = '';
+
+// Show login info
+$('body').append('<div class="login-loader">Logging in...</div>');
+
+sdk.login(user, passwd).then(function() {
+    // Loged in
+    $('div.login-loader').remove();
+    $('body').append('<div class="loading">Loading data...</div>');
+
+    // Do your stuff here
+    // ...
+});
