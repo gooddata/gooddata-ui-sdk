edbe22462028c52e14e0bda5325634c19b800200
Adam Kloboucnik <adam.kloboucnik@gooddata.com>
2014-03-13 16:27:48 +0100

    Removed unused doc npm's and grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,7 +121,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-grizzly');
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
 
