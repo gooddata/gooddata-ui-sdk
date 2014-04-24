7d40f8a680fa2e0df60e42c39353a833aa6d6aaf
Lukas Krecan <lukas.krecan@gooddata.com>
2014-04-24 11:54:10 +0200

    FEATURE: CL-4041 Following Location header after 202 response

--- test/xhr_test.js
+++ test/xhr_test.js
@@ -17,7 +17,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
 
         beforeEach(function() {
             var mock = sinon.mock($);
-            /** mpck result for first three calls of $.ajax */
+            /** mock result for first three calls of $.ajax */
             for (var i = 0; i < 3; i++) {
                 d.push($.Deferred());
                 expects.push(mock.expects('ajax').returns(d[i]));
@@ -30,6 +30,15 @@ define(['gooddata', 'jquery'], function(gd, $) {
             if ($.ajax.restore) $.ajax.restore();
         });
 
+        function mockResponse(status, headers) {
+            return {
+                status: status,
+                getResponseHeader: function(header) {
+                    return headers ? headers[header] : null;
+                }
+            };
+        }
+
         describe('$.ajax request', function() {
             it('should handle successful request', function(done) {
                 xhr.ajax('/some/url').done(function(data, textStatus, xhr) {
@@ -41,7 +50,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 var settings = expects[0].lastCall.args[0];
                 expect(settings.url).to.be('/some/url');
                 expect(settings.contentType).to.be('application/json');
-                d[0].resolve('Hello', '', { status: 200});
+                d[0].resolve('Hello', '', mockResponse(200));
             });
 
             it('should stringify JSON data for GDC backend', function(done) {
@@ -53,7 +62,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 });
                 var settings = expects[0].lastCall.args[0];
                 expect(settings.data).to.be('{"foo":"bar"}');
-                d[0].resolve('Ok', '', { status: 200});
+                d[0].resolve('Ok', '', mockResponse(200));
             });
 
             it('should handle unsuccessful request', function(done) {
@@ -62,7 +71,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     expect(xhr.status).to.be(404);
                     done();
                 });
-                d[0].reject({ status: 404});
+                d[0].reject(mockResponse(404));
             });
 
             it('should support url in settings', function(done) {
@@ -73,7 +82,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 });
                 var settings = expects[0].lastCall.args[0];
                 expect(settings.url).to.be('/some/url');
-                d[0].resolve('Hello', '', { status: 200});
+                d[0].resolve('Hello', '', mockResponse(200));
             });
 
             it('should work with sucess callback in settings', function(done) {
@@ -82,7 +91,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     expect(xhr.status).to.be(200);
                     done();
                 }});
-                d[0].resolve('Hello', '', { status: 200});
+                d[0].resolve('Hello', '', mockResponse(200));
             });
 
             it('should work with error callback in settings', function(done) {
@@ -90,7 +99,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     expect(xhr.status, 404);
                     done();
                 }});
-                d[0].reject({ status: 404});
+                d[0].reject(mockResponse(404));
             });
 
             it('should work with complete callback in settings for success', function(done) {
@@ -104,7 +113,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 xhr.ajax({ url: '/some/url', complete: function() {
                     done();
                 }});
-                d[0].reject({ status: 404});
+                d[0].reject(mockResponse(404));
             });
         });
 
@@ -118,9 +127,9 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     done();
                 });
                 fakeJqXhr(options, d);
-                d[0].reject({ status: 401}); //first request
-                d[1].resolve({}, '', { status: 200}); //token request
-                d[2].resolve('Hello', '', { status: 200}); //request retry
+                d[0].reject(mockResponse(401)); //first request
+                d[1].resolve({}, '', mockResponse(200)); //token request
+                d[2].resolve('Hello', '', mockResponse(200)); //request retry
             });
 
             it('should fail if token renewail fails and unathorize handler is not set', function(done) {
@@ -132,8 +141,8 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     done();
                 });
                 fakeJqXhr(options, d);
-                d[0].reject({ status: 401}); //first request
-                d[1].reject({ status: 401}); //token request
+                d[0].reject(mockResponse(401)); //first request
+                d[1].reject(mockResponse(401)); //token request
             });
 
             it('should invole unathorized handler is token request fail', function(done) {
@@ -148,8 +157,8 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 };
                 fakeJqXhr(options, d);
                 xhr.ajax(options);
-                d[0].reject({ status: 401}); //first request
-                d[1].reject({ status: 401}); //token request
+                d[0].reject(mockResponse(401)); //first request
+                d[1].reject(mockResponse(401)); //token request
             });
 
             it('should correctly handle multiple requests with token request in progress', function(done) {
@@ -164,7 +173,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 $.extend(d[1], optionsSecond);
 
                 xhr.ajax(optionsFirst);
-                d[0].reject({ status: 401});
+                d[0].reject(mockResponse(401));
 
                 // now, token request should be in progress
                 // so this "failure" should continue after
@@ -175,7 +184,7 @@ define(['gooddata', 'jquery'], function(gd, $) {
                 });
 
                 // simulate token request failed
-                d[1].reject({ status: 403});
+                d[1].reject(mockResponse(403));
             });
         });
 
@@ -190,9 +199,9 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     expect(data).to.be('OK');
                     done();
                 });
-                d[0].resolve(null, '', { status: 202});
-                d[1].resolve(null, '', { status: 202});
-                d[2].resolve('OK', '', { status: 200});
+                d[0].resolve(null, '', mockResponse(202));
+                d[1].resolve(null, '', mockResponse(202));
+                d[2].resolve('OK', '', mockResponse(200));
             });
 
             it('should correctly reject after retry 404', function(done) {
@@ -205,9 +214,61 @@ define(['gooddata', 'jquery'], function(gd, $) {
                     expect(xhr.status).to.be(404);
                     done();
                 });
-                d[0].resolve(null, '', { status: 202});
-                d[1].resolve(null, '', { status: 202});
-                d[2].reject({ status: 404});
+                d[0].resolve(null, '', mockResponse(202));
+                d[1].resolve(null, '', mockResponse(202));
+                d[2].reject(mockResponse(404));
+            });
+
+        });
+
+        describe('$.ajax polling with different location', function() {
+            it('should retry request after delay', function(done) {
+                var options = {
+                    url: '/some/url',
+                    pollDelay: 0
+                };
+                fakeJqXhr(options, d);
+                xhr.ajax(options).done(function(data) {
+                    expect(data).to.be('OK');
+                    expect(expects[2].lastCall.args[0].url).to.be('/other/url');
+
+                    done();
+                });
+                d[0].resolve(null, '', mockResponse(202, {'Location': '/other/url'}));
+                d[1].resolve(null, '', mockResponse(202, {'Location': '/other/url'}));
+                d[2].resolve('OK', '', mockResponse(200));
+            });
+
+            it('should folow multiple redirects', function(done) {
+                var options = {
+                    url: '/some/url',
+                    pollDelay: 0
+                };
+                fakeJqXhr(options, d);
+                xhr.ajax(options).done(function(data) {
+                    expect(data).to.be('OK');
+                    expect(expects[2].lastCall.args[0].url).to.be('/other/url2');
+
+                    done();
+                });
+                d[0].resolve(null, '', mockResponse(202, {'Location': '/other/url'}));
+                d[1].resolve(null, '', mockResponse(202, {'Location': '/other/url2'}));
+                d[2].resolve('OK', '', mockResponse(200));
+            });
+
+            it('should correctly reject after retry 404', function(done) {
+                var options = {
+                    url: '/some/url',
+                    pollDelay: 0
+                };
+                fakeJqXhr(options, d);
+                xhr.ajax(options).fail(function(xhr) {
+                    expect(xhr.status).to.be(404);
+                    done();
+                });
+                d[0].resolve(null, '', mockResponse(202, {'Location': '/other/url'}));
+                d[1].resolve(null, '', mockResponse(202, {'Location': '/other/url'}));
+                d[2].reject(mockResponse(404));
             });
 
         });
