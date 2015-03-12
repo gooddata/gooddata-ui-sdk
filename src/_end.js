3d389a0a05c37b8c27e8490eb6aba3d9653adc30
Adam Kloboucnik <a.kloboucnik@gmail.com>
2015-03-12 14:54:50 +0100

    CODE: Require out, webpack in

--- src/_end.js
+++
@@ -1,5 +0,0 @@
-  // Ask loader to synchronously require the
-  // module value for 'gooddata' here and return it as the
-  // value to use for the public API for the built file.
-  return require('gooddata');
-}));
--- src/execution.js
+++ src/execution.js
@@ -1,5 +1,5 @@
 // Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
-define(['xhr'], function(xhr) {
+define(['./xhr'], function(xhr) {
     'use strict';
 
     /**
