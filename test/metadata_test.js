f238bc633d284aa5de324e3e8354ac63f39ac2c7
Petr Benes <petr.benes@gooddata.com>
2016-08-16 14:11:30 +0200

    RELATED: ONE-115 Extract methods related to bulk dashboard load
    getObjectUsing (using2), getObjects (new bulk api)

--- test/metadata_test.js
+++ test/metadata_test.js
@@ -1,6 +1,8 @@
 // Copyright (C) 2007-2014, GoodData(R) Corporation. All rights reserved.
 /*eslint func-names:0*/
 import * as md from '../src/metadata';
+import * as xhr from '../src/xhr';
+import { range, find } from 'lodash';
 
 describe('metadata', () => {
     let server;
@@ -293,6 +295,198 @@ describe('metadata', () => {
 
                 md.getObjectUri('myFakeProjectId', 'label.foo.bar').then(function(result) {
                     expect(result).to.be('/foo/bar/attr');
+                    done();
+                });
+            });
+        });
+
+        describe('getObjectUsing', () => {
+            let postSpy;
+
+            const projectId = 'myFakeProjectId';
+            const object = `/gdc/md/${projectId}/obj/1`;
+            const types = ['firstType', 'secondType'];
+            const using2Uri = `/gdc/md/${projectId}/using2`;
+
+            const respondEntries = [{
+                content: {},
+                meta: {}
+            }];
+
+            beforeEach(() => {
+                postSpy = sinon.spy(xhr, 'post');
+            });
+
+            afterEach(() => {
+                postSpy.restore();
+            });
+
+            it('should load object dependencies', done => {
+                server.respondWith(
+                    'POST',
+                    using2Uri,
+                    [200, {'Content-Type': 'application/json'},
+                        JSON.stringify({ entries: respondEntries })]
+                );
+
+                md.getObjectUsing(projectId, object, { types }).then(result => {
+                    expect(postSpy.calledWith(using2Uri, {
+                        data: JSON.stringify({
+                            inUse: {
+                                uri: object,
+                                types,
+                                nearest: 0
+                            }
+                        })
+                    })).to.be(true);
+                    expect(result).to.eql(respondEntries);
+                    done();
+                });
+            });
+
+            it('should be properly called with nearest when requested', done => {
+                server.respondWith(
+                    'POST',
+                    using2Uri,
+                    [200, {'Content-Type': 'application/json'},
+                        JSON.stringify({ entries: respondEntries })]
+                );
+
+                const nearest = true;
+
+                md.getObjectUsing(projectId, object, { types, nearest }).then(() => {
+                    expect(postSpy.calledWith(using2Uri, {
+                        data: JSON.stringify({
+                            inUse: {
+                                uri: object,
+                                types,
+                                nearest: 1
+                            }
+                        })
+                    })).to.be(true);
+                    done();
+                });
+            });
+
+            it('should return rejected promise if 400 returned from backend', done => {
+                server.respondWith(
+                    'POST',
+                    using2Uri,
+                    [400, {'Content-Type': 'application/json'},
+                        JSON.stringify({})]
+                );
+
+                md.getObjectUsing(projectId, object, { types }).then(() => {
+                    expect().fail('Should reject the promise on 400 response');
+                    done();
+                }).fail(() => {
+                    done();
+                });
+            });
+        });
+
+        describe('getObjects', () => {
+            let postSpy;
+
+            const generateUrisAndResponse = (projectId, number) => {
+                const uris = range(number).map(i => `/gdc/md/${projectId}/obj/${i}`);
+                const respondEntries = uris.map(uri => ({
+                    content: {},
+                    meta: { uri }
+                }));
+
+                return { uris, respondEntries };
+            };
+
+            const projectId = 'myFakeProjectId';
+            const getUri = `/gdc/md/${projectId}/objects/get`;
+
+            beforeEach(() => {
+                postSpy = sinon.spy(xhr, 'post');
+            });
+
+            afterEach(() => {
+                postSpy.restore();
+            });
+
+            it('should load elements', done => {
+                const { uris, respondEntries } = generateUrisAndResponse(projectId, 2);
+
+                server.respondWith(
+                    'POST',
+                    getUri,
+                    [200, {'Content-Type': 'application/json'},
+                        JSON.stringify({ objects: {
+                            items: respondEntries
+                        }})]
+                );
+
+                md.getObjects(projectId, uris).then(result => {
+                    expect(postSpy.calledOnce).to.be(true);
+                    expect(postSpy.calledWith(getUri, {
+                        data: JSON.stringify({
+                            get: {
+                                items: uris
+                            }
+                        })
+                    })).to.be(true);
+
+                    expect(result).to.eql(respondEntries);
+                    done();
+                });
+            });
+
+            it('should load elements chunked', done => {
+                const { uris, respondEntries } = generateUrisAndResponse(projectId, 80);
+
+                server.respondWith(request => {
+                    const requestBody = JSON.parse(request.requestBody);
+
+                    // respond with only those items which were requested
+                    const respondItems = requestBody.get.items.map(itemUri =>
+                        find(respondEntries, responseItem => responseItem.meta.uri === itemUri));
+
+                    request.respond(200, {'Content-Type': 'application/json'},
+                        JSON.stringify({ objects: {
+                            items: respondItems
+                        }}));
+                });
+
+                md.getObjects(projectId, uris).then(result => {
+                    expect(postSpy.calledTwice).to.be(true);
+                    expect(postSpy.calledWith(getUri, {
+                        data: JSON.stringify({
+                            get: {
+                                items: uris.slice(0, 50)
+                            }
+                        })
+                    })).to.be(true);
+                    expect(postSpy.calledWith(getUri, {
+                        data: JSON.stringify({
+                            get: {
+                                items: uris.slice(50, 80)
+                            }
+                        })
+                    })).to.be(true);
+
+                    expect(result).to.eql(respondEntries);
+                    done();
+                });
+            });
+
+            it('should return rejected promise if 400 returned from backend', done => {
+                const { uris } = generateUrisAndResponse(projectId, 5);
+                server.respondWith(
+                    'POST',
+                    getUri,
+                    [400, {'Content-Type': 'application/json'},
+                        JSON.stringify({})]
+                );
+
+                md.getObjects(projectId, uris).then(() => {
+                    expect().fail('Should reject the promise on 400 response');
+                    done();
+                }).fail(() => {
                     done();
                 });
             });
