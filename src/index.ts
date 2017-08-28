b3e0f8bcbadaf87c59c9d6850c8ecb6ddf4ed1e3
Tomáš Vojtášek <tomas.vojtasek@gooddata.com>
2017-08-28 12:52:42 +0200

    RELATED: RAIL-283 Filter subscriber & publisher components

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,11 @@ import { Visualization } from './components/uri/Visualization';
 import { ErrorStates, ErrorCodes } from './constants/errorStates';
 import { IDrillableItem } from './interfaces/DrillableItem';
 import { IVisualizationProperties } from './interfaces/VisualizationProperties';
-import { AttributeFilter } from './components/base/AttributeFilter';
 import * as promise from './helpers/promise';
+import { AttributeFilter } from './components/filters/AttributeFilter/AttributeFilter';
+import { FilterSubscriber } from './components/filters/FilterSubscriber';
+import { FilterPublisher } from './components/filters/FilterPublisher';
+import { filtersReducer, REDUX_STATE_PATH } from './components/filters/redux/filtersReducer';
 
 export {
     BaseChart,
@@ -20,5 +23,10 @@ export {
     IDrillableItem,
     IVisualizationProperties,
     promise,
-    AttributeFilter
+    AttributeFilter,
+    FilterSubscriber,
+    FilterPublisher,
+
+    filtersReducer,
+    REDUX_STATE_PATH
 };
