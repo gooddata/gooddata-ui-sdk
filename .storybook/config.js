b294858ce7041872a4a5911aefdc930fdc241bfa
Pavel Langhammer <pavel.langhammer@gooddata.com>
2017-09-05 13:52:03 +0200

    RELATED: RAIL-269 show version & commit hash in storybook name

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+import pkg from '../package.json';
 
 const req = require.context('../stories', true, /\.tsx?$/);
 
 function loadStories() {
     req.keys().forEach(filename => req(filename));
 }
+
+setOptions({
+    name: `React Components v${pkg.version} ${__COMMIT_HASH__}`,
+    downPanelInRight: true
+});
 
 configure(loadStories, module);
