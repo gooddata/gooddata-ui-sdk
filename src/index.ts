5fe887a558ab6093ab673ba608d14b849a73c72b
Tomáš Vojtášek <tomas.vojtasek@gooddata.com>
2017-09-11 10:33:05 +0200

    BUGFIX: RAIL-302 Redux components in separate entrypoint

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,6 @@ import { IDrillableItem } from './interfaces/DrillableItem';
 import { IVisualizationProperties } from './interfaces/VisualizationProperties';
 import * as promise from './helpers/promise';
 import { AttributeFilter } from './components/filters/AttributeFilter/AttributeFilter';
-import { FilterSubscriber } from './components/filters/FilterSubscriber';
-import { FilterPublisher } from './components/filters/FilterPublisher';
-import { filtersReducer, REDUX_STATE_PATH } from './components/filters/redux/filtersReducer';
 
 export {
     BaseChart,
@@ -23,10 +20,5 @@ export {
     IDrillableItem,
     IVisualizationProperties,
     promise,
-    AttributeFilter,
-    FilterSubscriber,
-    FilterPublisher,
-
-    filtersReducer,
-    REDUX_STATE_PATH
+    AttributeFilter
 };
