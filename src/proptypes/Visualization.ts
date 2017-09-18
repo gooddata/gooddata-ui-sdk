4295f630c7edd5f9cef6854abe511a1a5808c8be
Jiri Zajic <jiri.zajic@gooddata.com>
2017-09-18 12:16:45 +0200

    BUGFIX: BB-292 Eventing cannot be set when using @gooddata/react-components

--- src/proptypes/Visualization.ts
+++ src/proptypes/Visualization.ts
@@ -2,11 +2,13 @@ import * as PropTypes from 'prop-types';
 import chartConfig from './ChartConfig';
 import events from './Events';
 import filters from './Filters';
+import drillableItem from './DrillableItem';
 
 export const visualizationPropTypes = {
     ...chartConfig,
     ...events,
     ...filters,
+    drillableItems: PropTypes.arrayOf(PropTypes.shape(drillableItem)),
     projectId: PropTypes.string.isRequired,
     identifier: PropTypes.string,
     uri: PropTypes.string,
