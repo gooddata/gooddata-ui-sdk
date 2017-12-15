gooddata/gooddata-react-components

RELATED: BB-334 Totals edit mode

src/components/uri/Visualization.tsx
@@ -18,6 +18,7 @@ import { IEvents } from '../../interfaces/Events';
 import { VisualizationPropType, Requireable } from '../../proptypes/Visualization';
 import { VisualizationTypes, VisType } from '../../constants/visualizationTypes';
 import { IDrillableItem } from '../../interfaces/DrillEvents';
+import { ITotal } from '../../interfaces/Totals';
 import { IDataSource } from '../../interfaces/DataSource';
 import { ISubject } from '../../helpers/async';
 
@@ -57,6 +58,8 @@ export interface IVisualizationProps extends IEvents {
     config?: IChartConfig;
     filters?: AFM.FilterItem[];
     drillableItems?: IDrillableItem[];
+    totals?: ITotal[];
+    totalsEditAllowed?: boolean;
     uriResolver?: (projectId: string, uri?: string, identifier?: string) => Promise<string>;
     fetchVisObject?: (visualizationUri: string) => Promise<VisualizationObject.IVisualizationObject>;
     BaseChartComponent?: any;
@@ -185,6 +188,8 @@ export class Visualization extends React.Component<IVisualizationProps, IVisuali
         const {
             drillableItems,
             onFiredDrillEvent,
+            totals,
+            totalsEditAllowed,
             onError,
             onLoadingChanged,
             locale,
@@ -202,6 +207,8 @@ export class Visualization extends React.Component<IVisualizationProps, IVisuali
                         resultSpec={resultSpec}
                         drillableItems={drillableItems}
                         onFiredDrillEvent={onFiredDrillEvent}
+                        totals={totals}
+                        totalsEditAllowed={totalsEditAllowed}
                         onError={onError}
                         onLoadingChanged={onLoadingChanged}
                         locale={locale}

src/interfaces/Totals.ts
@@ -1,0 +1,5 @@
+export interface ITotal {
+    alias?: string;
+    type: string;
+    outputMeasureIndexes: number[];
+}

gooddata/gooddata-ui-sdk

src/components/uri/Visualization.tsx
@@ -18,6 +18,7 @@ import { IEvents } from '../../interfaces/Events';
 import { VisualizationPropType, Requireable } from '../../proptypes/Visualization';
 import { VisualizationTypes, VisType } from '../../constants/visualizationTypes';
 import { IDrillableItem } from '../../interfaces/DrillEvents';
+import { ITotalItem } from '../../interfaces/Totals';
 import { IDataSource } from '../../interfaces/DataSource';
 import { ISubject } from '../../helpers/async';
 
@@ -57,6 +58,8 @@ export interface IVisualizationProps extends IEvents {
     config?: IChartConfig;
     filters?: AFM.FilterItem[];
     drillableItems?: IDrillableItem[];
+    totals?: ITotalItem[];
+    totalsEditAllowed?: boolean;
     uriResolver?: (projectId: string, uri?: string, identifier?: string) => Promise<string>;
     fetchVisObject?: (visualizationUri: string) => Promise<VisualizationObject.IVisualizationObject>;
     BaseChartComponent?: any;
@@ -184,6 +187,8 @@ export class Visualization extends React.Component<IVisualizationProps, IVisuali
         const {
             drillableItems,
             onFiredDrillEvent,
+            totals,
+            totalsEditAllowed,
             onError,
             onLoadingChanged,
             locale,
@@ -201,6 +206,8 @@ export class Visualization extends React.Component<IVisualizationProps, IVisuali
                         resultSpec={resultSpec}
                         drillableItems={drillableItems}
                         onFiredDrillEvent={onFiredDrillEvent}
+                        totals={totals}
+                        totalsEditAllowed={totalsEditAllowed}
                         onError={onError}
                         onLoadingChanged={onLoadingChanged}
                         locale={locale}

src/interfaces/Totals.ts
@@ -1,0 +1,5 @@
+export interface ITotalItem {
+    alias?: string;
+    type: string;
+    outputMeasureIndexes: number[];
+}