dfff0b1abef8684aef392cf96ce204faa2d94058
Pavel Zbytovsky <pavel.zbytovsky@gooddata.com>
2018-02-08 17:05:10 +0100

    RELATED: RAIL-580 add deterministic mock-js for storybook

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -3,6 +3,7 @@ const register = require('@gooddata/mock-js');
 function registerMocks(app) {
     const schema = require('../mock-schema.json');
     const config = {
+        randomSeed: 'reactcomponents',
         pollCount: 1
     };
     return register.default(app, { schema, config });
