14e17756e7924aa613a2274759f4fb67c9f02ca9
Nikola Čech <nikola.cech@gooddata.com>
2018-05-25 14:51:59 +0200

    FEATURE: RAIL-894 Move menu to sidebar

--- examples/src/components/utils/Header.jsx
+++ examples/src/components/utils/Header.jsx
@@ -34,14 +34,14 @@ class Header extends React.Component {
         return <div className="gd-header-menu-item button-logout" onClick={this.props.logoutAction}>Logout</div>;
     }
     render() {
-        const { location: { pathname }, mainRoutes, routes } = this.props;
+        const { location: { pathname }, topNavigationRoutes, routes } = this.props;
         const href = pathname;
         const currentRoute = (href !== undefined && routes.find(link => (link.path === BASEPATH + href))) || null;
         const pageTitle = currentRoute === null || currentRoute.title === appName
             ? appName
             : `${currentRoute.title} | ${appName}`;
 
-        const navigationElements = mainRoutes.map(({ path, title, exact = false }) => (
+        const navigationElements = topNavigationRoutes.map(({ path, title, exact = false }) => (
             <li key={path}>
                 <NavLink to={path} className="gd-header-menu-item" activeClassName="active" exact={exact}>
                     <span>{title}</span>
@@ -58,8 +58,11 @@ class Header extends React.Component {
                     }
 
                     .gd-header {
+                        position: fixed;
+                        top: 0;
+                        right: 0;
+                        left: 0;
                         z-index: 1;
-                        flex: 1 1 100%;
                         justify-content: center;
                         align-items: center;
                         width: 100%;
@@ -71,14 +74,11 @@ class Header extends React.Component {
 
                     .gd-header-inner {
                         display: flex;
+                        justify-content: space-between;
                         align-items: center;
                         width: 100%;
                         height: 100%;
                         max-width: 1400px;
-                    }
-
-                    .gd-header-menu-wrapper {
-                        height: 100%;
                     }
 
                     .gd-header-logo-img {
@@ -86,6 +86,7 @@ class Header extends React.Component {
                     }
 
                     .gd-header-inner :global(.gd-header-logo) {
+                        margin-right: 0;
                         opacity: 0.9;
                     }
 
@@ -95,48 +96,53 @@ class Header extends React.Component {
                         opacity: 1;
                     }
 
+                    .gd-header-menu-section {
+                        padding: 0 20px;
+                    }
+
                     .gd-header-inner :global(.gd-header-menu-item) {
-                        box-sizing: border-box;
-                        display: flex;
-                        align-items: center;
-                        height: 100%;
+                        height: 62px;
+                        margin: 0 23px;
+                        padding: 0 22px;
                         border-bottom: 2px solid transparent;
                         color: rgba(0, 0, 0, 0.7);
                         font-size: 15px;
-                        font-weight: normal;
-                        white-space: nowrap;
-                        line-height: 19px;
+                        line-height: 62px;
+                        transition: all 0.2s;
                     }
 
                     .gd-header-inner :global(.gd-header-menu-item:hover),
                     .gd-header-inner :global(.gd-header-menu-item:focus),
                     .gd-header-inner :global(.gd-header-menu-item:active) {
                         border-color: #464e56;
+                        color: rgba(0, 0, 0, 0.9);
                     }
 
                     .gd-header-inner :global(.gd-header-menu-item.active) {
+                        color: rgba(0, 0, 0, 0.9);
                         border-color: #14b2e2;
+                        font-weight: normal;
                     }
 
                     .gd-header-inner :global(.button-login),
                     .gd-header-inner :global(.button-logout) {
-                        height: 30px;
+                        height: 28px;
                         margin: 0;
                         padding: 0 22px;
                         border: 1px solid rgba(0, 0, 0, 0.5);
                         border-radius: 100px;
                         color: black;
                         font-size: 12px;
-                        line-height: 28px;
+                        line-height: 26px;
                     }
 
-                    .gd-header-menu,
-                    .gd-header-menu-section {
-                        flex: 1 1 auto;
-                    }
-
-                    .gd-header-menu-section {
-                        flex-wrap: wrap;
+                    .gd-header-inner :global(.button-login:hover),
+                    .gd-header-inner :global(.button-login:focus),
+                    .gd-header-inner :global(.button-login:active),
+                    .gd-header-inner :global(.button-logout:hover),
+                    .gd-header-inner :global(.button-logout:focus),
+                    .gd-header-inner :global(.button-logout:active) {
+                        border-color: #000;
                     }
                 `}</style>
                 <Helmet>
@@ -147,12 +153,18 @@ class Header extends React.Component {
                 </Helmet>
                 <div className="gd-header header-6 is-loaded">
                     <div className="gd-header-inner">
-                        <Link to="/" className="gd-header-logo gd-header-measure">
+                        <a href="https://sdk.gooddata.com/gooddata-ui/" className="gd-header-logo gd-header-measure">
                             <img src={logo} alt={appName} className="gd-header-logo-img" />
-                        </Link>
+                        </a>
                         <div className="gd-header-stretch gd-header-menu-wrapper">
                             <div className="gd-header-menu gd-header-menu-horizontal">
                                 <ul className="gd-header-menu-section gd-header-measure">
+                                    <li>
+                                        <a href="https://sdk.gooddata.com/gooddata-ui/docs/about_gooddataui.html" className="gd-header-menu-item">Docs</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://sdk.gooddata.com/gooddata-ui/docs/support_options.html" className="gd-header-menu-item">Support</a>
+                                    </li>
                                     {navigationElements}
                                 </ul>
                             </div>
@@ -167,14 +179,14 @@ class Header extends React.Component {
 
 Header.propTypes = {
     location: PropTypes.object.isRequired,
-    mainRoutes: PropTypes.array,
+    topNavigationRoutes: PropTypes.array,
     routes: PropTypes.array,
     isUserLoggedIn: PropTypes.bool,
     logoutAction: PropTypes.func.isRequired
 };
 Header.defaultProps = {
     isUserLoggedIn: null,
-    mainRoutes: [],
+    topNavigationRoutes: [],
     routes: []
 };
 
