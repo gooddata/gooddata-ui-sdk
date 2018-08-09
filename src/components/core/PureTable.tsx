naibinh/gooddata-react-components

SD-52: Respect regional number format setting also in new UI

src/components/core/PureTable.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import noop = require('lodash/noop');
 import difference = require('lodash/difference');
 import uniq = require('lodash/uniq');
+import { get } from 'lodash';
 
 import { AFM, VisualizationObject } from '@gooddata/typings';
 
@@ -183,7 +184,9 @@ class SimpleTable extends
             totalsEditAllowed,
             execution
         } = this.props;
-
+        
+        const numberSeparator = get(this.props, 'config.numberSeparator', undefined);
+        
         // Short term solution (See BB-641)
         const indexedTotals = convertToIndexedTotals(totals, dataSource.getAfm(), resultSpec);
 
@@ -199,7 +202,7 @@ class SimpleTable extends
                                 fixEmptyHeaderItems(execution.executionResult, props.emptyHeaderString)
                             }
                             afterRender={afterRender}
-                            config={{ stickyHeaderOffset }}
+                            config={{ stickyHeaderOffset, numberSeparator }}
                             drillableItems={drillableItems}
                             height={height}
                             maxHeight={maxHeight}

src/components/visualizations/table/TableTransformation.tsx
@@ -90,7 +90,7 @@ export class TableTransformation extends React.Component<ITableTransformationPro
         const { sortBy, sortDir } = sortItem ? getSortInfo(sortItem, headers) : EMPTY_SORT_INFO;
 
         const tableProps: ITableProps = {
-            ...pick(config, ['rowsPerPage', 'onMore', 'onLess', 'sortInTooltip', 'stickyHeaderOffset']),
+            ...pick(config, ['rowsPerPage', 'onMore', 'onLess', 'sortInTooltip', 'stickyHeaderOffset', 'numberSeparator']),
             afterRender,
             totalsWithData,
             totalsEditAllowed,

gooddata/gooddata-ui-sdk

src/components/core/PureTable.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import noop = require('lodash/noop');
 import difference = require('lodash/difference');
 import uniq = require('lodash/uniq');
+import { get } from 'lodash';
 
 import { AFM, VisualizationObject } from '@gooddata/typings';
 
@@ -184,6 +185,8 @@ class SimpleTable extends
             execution
         } = this.props;
 
+        const separators = get(this.props, 'config.separators', undefined);
+
         // Short term solution (See BB-641)
         const indexedTotals = convertToIndexedTotals(totals, dataSource.getAfm(), resultSpec);
 
@@ -199,7 +202,7 @@ class SimpleTable extends
                                 fixEmptyHeaderItems(execution.executionResult, props.emptyHeaderString)
                             }
                             afterRender={afterRender}
-                            config={{ stickyHeaderOffset }}
+                            config={{ stickyHeaderOffset, separators }}
                             drillableItems={drillableItems}
                             height={height}
                             maxHeight={maxHeight}

src/components/visualizations/table/TableTransformation.tsx
@@ -90,7 +90,7 @@ export class TableTransformation extends React.Component<ITableTransformationPro
         const { sortBy, sortDir } = sortItem ? getSortInfo(sortItem, headers) : EMPTY_SORT_INFO;
 
         const tableProps: ITableProps = {
-            ...pick(config, ['rowsPerPage', 'onMore', 'onLess', 'sortInTooltip', 'stickyHeaderOffset']),
+            ...pick(config, ['rowsPerPage', 'onMore', 'onLess', 'sortInTooltip', 'stickyHeaderOffset', 'separators']),
             afterRender,
             totalsWithData,
             totalsEditAllowed,