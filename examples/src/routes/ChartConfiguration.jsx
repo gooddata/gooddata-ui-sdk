ba7646ebe18eeee3ea907d3650e6f3b30297bef9
minhluu <minh.luu@gooddata.com>
2019-04-25 17:08:42 +0700

    FEATURE: QA-9454 Customize embedded insight on SDK dashboard

--- examples/src/routes/ChartConfiguration.jsx
+++ examples/src/routes/ChartConfiguration.jsx
@@ -4,17 +4,29 @@ import ExampleWithSource from "../components/utils/ExampleWithSource";
 
 import BarChartDynamicExample from "../components/BarChartDynamicExample";
 import BarChartDynamicExampleSRC from "!raw-loader!../components/BarChartDynamicExample"; // eslint-disable-line
+import ConfigurationColumnChartExample from "../components/ConfigurationColumnChartExample";
+import ConfigurationColumnChartExampleSRC from "!raw-loader!../components/ConfigurationColumnChartExample"; // eslint-disable-line
 import PieChartColorMappingExample from "../components/PieChartColorMappingExample";
 import PieChartColorMappingExampleSRC from "!raw-loader!../components/PieChartColorMappingExample"; // eslint-disable-line
 
 export const BarChartDynamic = () => (
     <div>
         <div>
-            <h1>Bar Chart with dynamic colors example</h1>
+            <h1>Customize chart bucket component</h1>
 
             <hr className="separator" />
 
             <ExampleWithSource for={BarChartDynamicExample} source={BarChartDynamicExampleSRC} />
+        </div>
+        <div>
+            <h1>Customize chart visualization</h1>
+
+            <hr className="separator" />
+
+            <ExampleWithSource
+                for={ConfigurationColumnChartExample}
+                source={ConfigurationColumnChartExampleSRC}
+            />
         </div>
         <div>
             <h1>Pie Chart with color mapping</h1>
