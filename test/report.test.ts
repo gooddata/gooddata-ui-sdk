f4bc032faf714f9ee48ae95f1f14f323951bff1c
lvkhanh <lvkhanh@lhv.vn>
2019-06-13 14:31:34 +0700

    BUGFIX: SD-502 update error status for export

--- test/report.test.ts
+++ test/report.test.ts
@@ -2,9 +2,16 @@
 import "isomorphic-fetch";
 import * as fetchMock from "fetch-mock";
 import { ReportModule } from "../src/report";
-import { XhrModule } from "../src/xhr";
+import { XhrModule, ApiResponseError } from "../src/xhr";
 import { IExportConfig, IExportResponse } from "../src/interfaces";
 import { mockPollingRequest } from "./helpers/polling";
+import {
+    SUCCESS_REQUEST_STATUS,
+    ACCEPTED_REQUEST_STATUS,
+    BAD_REQUEST_STATUS,
+    BAD_REQUEST_MESSAGE,
+    ERROR_RESTRICTED_MESSAGE,
+} from "../src/constants/errors";
 
 const mockedReportModule = () => new ReportModule(new XhrModule(fetch, {}));
 
@@ -22,13 +29,13 @@ describe("report", () => {
         describe("exportResult", () => {
             it("should return created file", () => {
                 fetchMock.mock(projectUri, {
-                    status: 200,
+                    status: SUCCESS_REQUEST_STATUS,
                     body: { uri: createdReport },
                 });
 
                 const mockTask = (status: number) => ({ status, uri: createdReport });
-                const finishedTask = mockTask(200);
-                const runningTask = mockTask(202);
+                const finishedTask = mockTask(SUCCESS_REQUEST_STATUS);
+                const runningTask = mockTask(ACCEPTED_REQUEST_STATUS);
                 mockPollingRequest(createdReport, runningTask, finishedTask);
 
                 const exportConfig: IExportConfig = {
@@ -43,8 +50,8 @@ describe("report", () => {
             });
 
             it("should return error when polling fail", () => {
-                const finishedTask = { status: 400 };
-                const runningTask = { status: 202, uri: createdReport };
+                const finishedTask = { status: BAD_REQUEST_STATUS };
+                const runningTask = { status: ACCEPTED_REQUEST_STATUS, uri: createdReport };
 
                 fetchMock.mock(projectUri, finishedTask);
 
@@ -63,12 +70,12 @@ describe("report", () => {
 
             it("should return restricted error", () => {
                 const finishedTask = {
-                    status: 400,
+                    status: BAD_REQUEST_STATUS,
                     // tslint:disable-next-line:max-line-length
                     body:
                         '{"error":{"message":"During export we\'ve detected user error: Export to required format is not allowed for data flagged as restricted."}}',
                 };
-                const runningTask = { status: 202, uri: createdReport };
+                const runningTask = { status: ACCEPTED_REQUEST_STATUS, uri: createdReport };
 
                 fetchMock.mock(projectUri, finishedTask);
 
@@ -82,15 +89,15 @@ describe("report", () => {
 
                 return mockedReportModule()
                     .exportResult(projectId, executionResult, exportConfig, { pollStep: 1 })
-                    .then(null, (error: Error) =>
-                        expect(error.message).toBe(
-                            "You cannot export this insight because it contains restricted data.",
-                        ),
-                    );
+                    .then(null, (error: ApiResponseError) => {
+                        expect(error.response.status).toBe(BAD_REQUEST_STATUS);
+                        expect(error.response.statusText).toBe(BAD_REQUEST_MESSAGE);
+                        expect(error.message).toBe(ERROR_RESTRICTED_MESSAGE);
+                    });
             });
 
             it("should reject with 400 when resource fails", () => {
-                fetchMock.mock(projectUri, 400);
+                fetchMock.mock(projectUri, BAD_REQUEST_STATUS);
 
                 return mockedReportModule()
                     .exportResult(projectId, executionResult)
