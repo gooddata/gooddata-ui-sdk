c124d82c24069d75ee487a6aee89fc65a95c7d13
naibinh <binh.nguyen@gooddata.com>
2019-08-06 15:52:05 +0700

    RELATED: SD-559 add filter context type

---
+++ src/DashboardExport.ts
@@ -0,0 +1,29 @@
+// (C) 2019 GoodData Corporation
+import { ExtendedDateFilters } from './ExtendedDateFilters';
+
+export namespace DashboardExport {
+    export type RelativeType = 'relative';
+    export type AbsoluteType = 'absolute';
+    export type DateFilterType = RelativeType | AbsoluteType;
+
+    export interface IAttributeFilter {
+        attributeFilter: {
+            displayForm: string;
+            negativeSelection: boolean;
+            attributeElements: string[];
+        };
+    }
+
+    export interface IDateFilter {
+        dateFilter: {
+            type: DateFilterType;
+            granularity: ExtendedDateFilters.DateFilterGranularity;
+            from: ExtendedDateFilters.DateString | number;
+            to: ExtendedDateFilters.DateString | number;
+            dataSet?: string;
+            attribute?: string;
+        };
+    }
+
+    export type FilterContextItem = IAttributeFilter | IDateFilter;
+}
