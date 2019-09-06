2592e53019038d2027a1d0d536a420032b59cfab
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-09-06 08:56:24 +0200

    checkpoint: remove useless inheritance in stateless base chart

--- libs/gooddata-react-components/src/components/core/base/NewBaseChart.tsx
+++ libs/gooddata-react-components/src/components/core/base/NewBaseChart.tsx
@@ -1,7 +1,11 @@
 // (C) 2007-2018 GoodData Corporation
 import * as React from "react";
+import { ErrorStates } from "../../..";
+import { generateErrorMap, IErrorMap } from "../../../helpers/errorHandlers";
 import { withEntireDataView } from "../../exp/NewLoadingHOC";
 import { ILoadingInjectedProps, defaultCommonVisProps, IBaseChartProps } from "../../exp/props";
+import { IErrorProps } from "../../simple/ErrorComponent";
+import { ILoadingProps } from "../../simple/LoadingComponent";
 import noop = require("lodash/noop");
 
 import { Visualization } from "../../visualizations/NewVisualization";
@@ -9,17 +13,47 @@ import { Visualization } from "../../visualizations/NewVisualization";
 import { IntlWrapper } from "./IntlWrapper";
 import { IntlTranslationsProvider, ITranslationsComponentProps } from "./TranslationsProvider";
 import { fixEmptyHeaderItems2 } from "./utils/fixEmptyHeaderItems";
-import { BaseVisualization } from "./NewBaseVisualization";
 import { getValidColorPalette2 } from "../../visualizations/utils/color";
 
-export class StatelessBaseChart extends BaseVisualization<IBaseChartProps & ILoadingInjectedProps, {}> {
-    public static defaultProps: Partial<IBaseChartProps & ILoadingInjectedProps> = {
+type Props = IBaseChartProps & ILoadingInjectedProps;
+
+export class StatelessBaseChart extends React.Component<Props, {}> {
+    public static defaultProps: Partial<Props> = {
         ...defaultCommonVisProps,
         onDataTooLarge: noop,
         onLegendReady: noop,
         config: {},
         visualizationComponent: Visualization,
     };
+
+    private errorMap: IErrorMap;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.errorMap = generateErrorMap(props.intl);
+    }
+
+    public render(): JSX.Element {
+        const { dataView, error, isLoading } = this.props;
+
+        const ErrorComponent = this.props.ErrorComponent as React.ComponentType<IErrorProps>;
+        const LoadingComponent = this.props.LoadingComponent as React.ComponentType<ILoadingProps>;
+
+        if (error) {
+            const errorProps = this.errorMap[
+                this.errorMap.hasOwnProperty(error) ? error : ErrorStates.UNKNOWN_ERROR
+            ];
+            return ErrorComponent ? <ErrorComponent code={error} {...errorProps} /> : null;
+        }
+
+        // when in pageble mode (getPage present) never show loading (its handled by the component)
+        if (isLoading || !dataView) {
+            return LoadingComponent ? <LoadingComponent /> : null;
+        }
+
+        return this.renderVisualization();
+    }
 
     public renderVisualization(): JSX.Element {
         const { afterRender, height, locale, config, type, dataView, onDataTooLarge, pushData } = this.props;
@@ -35,17 +69,17 @@ export class StatelessBaseChart extends BaseVisualization<IBaseChartProps & ILoa
 
                         return (
                             <this.props.visualizationComponent
+                                locale={locale}
                                 dataView={dataView}
                                 height={height}
                                 config={fullConfig}
+                                numericSymbols={translationProps.numericSymbols}
+                                drillableItems={this.props.drillableItems}
                                 afterRender={afterRender}
                                 onDataTooLarge={onDataTooLarge}
                                 onNegativeValues={this.props.onNegativeValues}
-                                drillableItems={this.props.drillableItems}
                                 onFiredDrillEvent={this.props.onFiredDrillEvent}
                                 onLegendReady={this.props.onLegendReady}
-                                numericSymbols={translationProps.numericSymbols}
-                                locale={locale}
                                 pushData={pushData}
                             />
                         );
