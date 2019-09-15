ec5a967f66abf519047293ebf6a4a63f7b8e241b
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-09-15 21:13:33 +0200

    Expand basic app with more charts

--- examples/basic-react-app/src/App.tsx
+++ examples/basic-react-app/src/App.tsx
@@ -1,33 +1,84 @@
 // (C) 2019 GoodData Corporation
 import React from "react";
 import "./App.css";
-import { BarChart } from "@gooddata/sdk-ui";
+import { AreaChart, BarChart, BubbleChart } from "@gooddata/sdk-ui";
 import { backend, initialize } from "./backend";
+import { Col, Container, Row } from "react-grid-system";
+import { AgentName, AvgDuration, EndpointName, SumOfCalls, workspace } from "./model";
 
 initialize();
 const analyticalBackend = backend();
-
-console.log(analyticalBackend);
+const height = 400;
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <BarChart
-                backend={analyticalBackend}
-                workspace="gtl83h4doozbp26q0kf5qg8uiyu4glyn"
-                measures={[
-                    {
-                        measure: {
-                            localIdentifier: "blabla",
-                            definition: {
-                                measureDefinition: {
-                                    item: { identifier: "fact.endpoint_calls.calls" },
+            <Container>
+                <Row>
+                    <Col sm={6}>
+                        <AreaChart
+                            backend={analyticalBackend}
+                            workspace={workspace}
+                            height={height}
+                            measures={[SumOfCalls]}
+                            viewBy={AgentName}
+                        />
+                    </Col>
+                    <Col sm={6}>
+                        <AreaChart
+                            backend={analyticalBackend}
+                            workspace={workspace}
+                            height={height}
+                            measures={[SumOfCalls]}
+                            viewBy={AgentName}
+                            stackBy={EndpointName}
+                            config={{
+                                legend: {
+                                    enabled: false,
                                 },
-                            },
-                        },
-                    },
-                ]}
-            />
+                            }}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <BarChart
+                            backend={analyticalBackend}
+                            workspace={workspace}
+                            measures={[SumOfCalls]}
+                            viewBy={AgentName}
+                            height={height}
+                        />
+                    </Col>
+                    <Col sm={6}>
+                        <BarChart
+                            backend={analyticalBackend}
+                            workspace={workspace}
+                            measures={[SumOfCalls]}
+                            stackBy={AgentName}
+                            height={height}
+                            config={{
+                                legend: {
+                                    enabled: false,
+                                },
+                            }}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <BubbleChart
+                            backend={analyticalBackend}
+                            workspace={workspace}
+                            height={height}
+                            xAxisMeasure={AvgDuration}
+                            size={SumOfCalls}
+                            viewBy={AgentName}
+                        />
+                    </Col>
+                    <Col sm={6}></Col>
+                </Row>
+            </Container>
         </div>
     );
 };
