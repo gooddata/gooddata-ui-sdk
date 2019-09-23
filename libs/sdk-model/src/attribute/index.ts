b02c11e19c778e688bd3bac8a2adc4dda0d726ea
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-09-23 13:25:58 +0200

    Add docs and input checks to IAttribute

--- libs/sdk-model/src/attribute/index.ts
+++ libs/sdk-model/src/attribute/index.ts
@@ -1,22 +1,54 @@
 // (C) 2019 GoodData Corporation
 import { Identifier, isUriQualifier, ObjQualifier, isIdentifierQualifier } from "../base";
 import isEmpty = require("lodash/isEmpty");
+import invariant from "ts-invariant";
 
 /**
- * TODO: SDK8: Add docs
+ * Attribute is our nomenclature for 'dimension' as typically used in multi-dimensional BI modeling. The attribute
+ * specifies on what dimension to slice/dice the measures.
+ *
+ * In GoodData, each attribute can have multiple different display forms. Display forms essentially provide different
+ * representations for elements in the dimension.
  *
  * @public
  */
 export interface IAttribute {
     attribute: {
+        /**
+         * User-assigned id of the attribute. The value is used to cross-reference model elements that are part
+         * of an execution.
+         *
+         * The constraint is that the local identifiers of all measures and attributes in an execution definition MUST
+         * be unique.
+         */
         localIdentifier: Identifier;
+
+        /**
+         * Reference to display form to use for element representations.
+         *
+         * The display form can be referenced by either URL of the display form resource on backend OR by
+         * unique, backend-recognized, identifier of the display form.
+         *
+         * Note: specifying display forms by URI is discouraged and WILL be deprecated in the future. The
+         * specification of URL has several drawbacks:
+         *
+         * -  Display form URLs are tied to particular analytical workspace; this makes any application that uses
+         *    URL-specified display forms workspace-specific.
+         * -  Display for URLs are not supported by all GoodData backends == this makes any application that uses
+         *    URL-specified display form backend-specific
+         */
         displayForm: ObjQualifier;
+
+        /**
+         * This is user-assigned alias of the attribute; it is opaque for the backend. When backend returns
+         * metadata about execution results, it WILL include this user-assigned alias in the metadata.
+         */
         alias?: string;
     };
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Type guard checking whether object is an instance of IAttribute.
  *
  * @public
  */
@@ -25,21 +57,21 @@ export function isAttribute(obj: any): obj is IAttribute {
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Defines function signature for measure predicates.
  *
  * @public
  */
 export type AttributePredicate = (attribute: IAttribute) => boolean;
 
 /**
- * TODO: SDK8: Add docs
+ * Predicate that returns true for any attribute.
  *
  * @public
  */
 export const anyAttribute: AttributePredicate = _ => true;
 
 /**
- * TODO: SDK8: Add docs
+ * Factory function for attribute predicate which evaluates true for attributes that match particular ID.
  *
  * @public
  */
@@ -51,58 +83,85 @@ export const idMatchAttribute: (id: string) => AttributePredicate = id => attr =
 //
 
 /**
- * TODO: SDK8: Add docs
+ * Gets local identifier of an attribute.
  *
+ * @param attribute - attribute to work with
+ * @returns value of local identifier
  * @public
  */
-export function attributeId(a: IAttribute): string {
-    return a.attribute.localIdentifier;
+export function attributeId(attribute: IAttribute): string {
+    invariant(attribute, "attribute must not be undefined");
+
+    return attribute.attribute.localIdentifier;
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Gets URI of attribute's display form to use and get attribute element values from.
  *
+ * @param attribute - attribute to work with, may be undefined == result is undefined
+ * @returns display form URI as string, undefined if display form not specified using URI
  * @public
  */
-export function attributeUri(a: IAttribute): string | undefined {
-    if (!a) {
+export function attributeUri(attribute: IAttribute): string | undefined {
+    if (!attribute) {
         return undefined;
     }
 
-    return isUriQualifier(a.attribute.displayForm) ? a.attribute.displayForm.uri : undefined;
+    return isUriQualifier(attribute.attribute.displayForm) ? attribute.attribute.displayForm.uri : undefined;
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Gets identifier of attribute's display form to use and get attribute element values from.
  *
+ * @param attribute - attribute to work with, may be undefined == result is undefined
+ * @returns display form identifier as tring, undefined if display for not specified using identifier
  * @public
  */
-export function attributeIdentifier(a: IAttribute): string | undefined {
-    if (!a) {
+export function attributeIdentifier(attribute: IAttribute): string | undefined {
+    if (!attribute) {
         return undefined;
     }
 
-    return isIdentifierQualifier(a.attribute.displayForm) ? a.attribute.displayForm.identifier : undefined;
+    return isIdentifierQualifier(attribute.attribute.displayForm)
+        ? attribute.attribute.displayForm.identifier
+        : undefined;
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Calculates attribute's unique fingerprint. The contract here is that two attribute definitions which lead to same
+ * results computed by the backend SHOULD return same fingerprint.
  *
+ * @param attribute - attribute to work with, must be defined
+ * @returns opaque string
  * @public
  */
 export function attributeFingerprint(attribute: IAttribute): string {
+    invariant(attribute, "attribute must not be undefined");
+
     return JSON.stringify(attribute);
 }
 
 /**
- * TODO: SDK8: Add docs
+ * Given list of attributes, returns first-found attribute matching the provided predicate.
  *
+ * If no predicate is provided, then the function defaults to anyAttribute predicate - meaning first found attribute
+ * will be returned.
+ *
+ * This function also provides convenience to find attribute by its local identifier - if you pass predicate as
+ * string the function will automatically create idMatchAttribute predicate.
+ *
+ * @param attributes - list of attributes to work with, may be undefined == no match
+ * @param idOrFun - attribute identifier or instance of AttributePredicate; {@link anyAttribute} predicate is default
  * @public
  */
 export function attributesFind(
     attributes: IAttribute[],
     idOrFun: string | AttributePredicate = anyAttribute,
 ): IAttribute | undefined {
+    if (!attributes || !attributes.length) {
+        return;
+    }
+
     const predicate = typeof idOrFun === "string" ? idMatchAttribute(idOrFun) : idOrFun;
 
     return attributes.find(predicate);
