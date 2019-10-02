20f4e7ae583d73ccd078c813f7a2a1ecf5c777db
Dan Homola <dan.homola@gooddata.com>
2019-10-02 16:26:07 +0200

    Separate InsightViewDemo to its own file
    RAIL-1765

--- examples/bear-react-app/src/App.tsx
+++ examples/bear-react-app/src/App.tsx
@@ -1,80 +1,28 @@
 // (C) 2019 GoodData Corporation
-import React, { useState } from "react";
+import React from "react";
 import { Col, Container, Row } from "react-grid-system";
 
-import {
-    AreaChart,
-    BarChart,
-    BubbleChart,
-    DonutChart,
-    Executor,
-    Kpi,
-    PieChart,
-    InsightView,
-} from "@gooddata/sdk-ui";
+import { AreaChart, BarChart, BubbleChart, DonutChart, Executor, Kpi, PieChart } from "@gooddata/sdk-ui";
 
 import "./App.css";
 import { backend, initialize } from "./backend";
 import { AgentName, AvgDuration, EndpointName, SumOfCalls, workspace } from "./model";
+import InsightViewDemo from "./InsightViewDemo";
 
 initialize();
 const analyticalBackend = backend();
 const height = 400;
 
 const App: React.FC = () => {
-    const [useValidRequest, setUseValidRequest] = useState(true);
-    const [visId, setVisId] = useState("abgkddfHcFon");
-
     return (
         <div className="App">
             <Container>
                 <Row>
                     <Col>
-                        <input
-                            type="checkbox"
-                            checked={useValidRequest}
-                            onChange={e => setUseValidRequest(e.target.checked)}
-                        />
-                        <input value={visId} onChange={e => setVisId(e.target.value)} />
-                        <InsightView
-                            backend={analyticalBackend}
-                            id={visId}
-                            workspace={workspace}
-                            visualizationProps={{
-                                custom: {
-                                    drillableItems: [
-                                        {
-                                            identifier: "label.method.method",
-                                        },
-                                    ],
-                                },
-                                dimensions: {
-                                    height: 300,
-                                },
-                            }}
-                            onDrill={e => {
-                                console.log("AAAAA", e);
-                            }}
-                            filters={
-                                [
-                                    // {
-                                    //     relativeDateFilter: {
-                                    //         dataSet: {
-                                    //             uri: "/gdc/md/gtl83h4doozbp26q0kf5qg8uiyu4glyn/obj/344",
-                                    //         },
-                                    //         from: -10,
-                                    //         to: 0,
-                                    //         granularity: useValidRequest
-                                    //             ? "GDC.time.month"
-                                    //             : "GDC.time.monthAAAAA",
-                                    //     },
-                                    // },
-                                ]
-                            }
-                        />
+                        <InsightViewDemo backend={analyticalBackend} workspace={workspace} />
                     </Col>
                 </Row>
-                {/*
+
                 <Row>
                     <Col>
                         <Executor
@@ -218,7 +166,6 @@ const App: React.FC = () => {
                         />
                     </Col>
                 </Row>
-                 */}
             </Container>
         </div>
     );
