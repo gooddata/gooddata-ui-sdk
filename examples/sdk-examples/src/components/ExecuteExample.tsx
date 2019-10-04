51d62b6557aa4a58edf5a62c323abd7e41bf4047
Matyas Kandl <matyas.kandl@gooddata.com>
2019-10-04 10:21:34 +0200

    Fix Execute example

--- examples/sdk-examples/src/components/ExecuteExample.tsx
+++ examples/sdk-examples/src/components/ExecuteExample.tsx
@@ -1,11 +1,15 @@
 // (C) 2007-2019 GoodData Corporation
-/* eslint-disable react/jsx-closing-tag-location */
-import React, { useState } from "react";
-import { Executor, LoadingComponent, ErrorComponent, Model } from "@gooddata/sdk-ui";
-import get from "lodash/get";
+import React, { useState } from 'react';
+import {
+    Executor,
+    LoadingComponent,
+    ErrorComponent,
+    Model
+} from '@gooddata/sdk-ui';
+import get from 'lodash/get';
 
-import { totalSalesIdentifier, projectId } from "../utils/fixtures";
-import { useBackend } from "../backend";
+import { totalSalesIdentifier, projectId } from '../utils/fixtures';
+import { useBackend } from '../backend';
 
 interface IExecuteExampleState {
     executionNumber: number;
@@ -14,20 +18,44 @@ interface IExecuteExampleState {
 
 const resultStyle = {
     maxHeight: 200,
-    maxWidth: "100%",
-    overflow: "auto",
-    padding: "1rem",
-    backgroundColor: "#EEE",
+    maxWidth: '100%',
+    overflow: 'auto',
+    padding: '1rem',
+    backgroundColor: '#EEE'
 };
 
 export const ExecuteExample: React.FC = () => {
     const backend = useBackend();
-    const [{ willFail, executionNumber }, setState] = useState<IExecuteExampleState>({
+    const [{ willFail }, setState] = useState<IExecuteExampleState>({
         executionNumber: 0,
-        willFail: true,
+        willFail: false
     });
 
-    const measure = Model.measure(willFail ? totalSalesIdentifier : null).localIdentifier("measure");
+    const retry = () => {
+        setState(({ executionNumber }) => {
+            const nextExecutionNumber = executionNumber + 1;
+            return {
+                executionNumber: nextExecutionNumber,
+                willFail: nextExecutionNumber % 2 !== 0
+            };
+        });
+    };
+
+    const retryButton = (
+        <p>
+            <button
+                onClick={retry}
+                className="gd-button gd-button-action s-retry-button"
+            >
+                Retry
+            </button>
+            &ensp;(fails every second attempt)
+        </p>
+    );
+
+    const measure = Model.measure(
+        willFail ? null : totalSalesIdentifier
+    ).localIdentifier('measure');
 
     const execution = backend
         .workspace(projectId)
@@ -37,34 +65,19 @@ export const ExecuteExample: React.FC = () => {
     return (
         <div>
             <Executor execution={execution}>
-                {({ error, fetch, isLoading, result }) => {
-                    const retry = () => {
-                        fetch();
-                        setState({
-                            executionNumber: executionNumber + 1,
-                            willFail: executionNumber % 2 ? true : false,
-                        });
-                    };
-
-                    const retryButton = (
-                        <p>
-                            <button onClick={retry} className="gd-button gd-button-action s-retry-button">
-                                Retry
-                            </button>
-                            &ensp;(fails every second attempt)
-                        </p>
-                    );
-
+                {({ error, isLoading, result }) => {
                     if (error) {
                         return (
                             <div>
                                 {retryButton}
                                 <div className="gd-message error">
-                                    <div className="gd-message-text">Oops, simulated error! Retry?</div>
+                                    <div className="gd-message-text">
+                                        Oops, simulated error! Retry?
+                                    </div>
                                 </div>
                                 <ErrorComponent
                                     message="There was an error getting your execution"
-                                    description={JSON.stringify(error, null, "  ")}
+                                    description={JSON.stringify(error, null, 2)}
                                 />
                             </div>
                         );
@@ -73,7 +86,9 @@ export const ExecuteExample: React.FC = () => {
                         return (
                             <div>
                                 <div className="gd-message progress">
-                                    <div className="gd-message-text">Loading…</div>
+                                    <div className="gd-message-text">
+                                        Loading…
+                                    </div>
                                 </div>
                                 <LoadingComponent />
                             </div>
@@ -83,22 +98,28 @@ export const ExecuteExample: React.FC = () => {
                     const data = get(result.data(), [0, 0]);
                     return (
                         <div>
-                            <style jsx>{`
-                                .kpi {
-                                    height: 60px;
-                                    margin: 10px 0;
-                                    font-size: 50px;
-                                    line-height: 60px;
-                                    white-space: nowrap;
-                                    vertical-align: bottom;
-                                    font-weight: 700;
-                                }
-                            `}</style>
+                            <style jsx>
+                                {`
+                                    .kpi {
+                                        height: 60px;
+                                        margin: 10px 0;
+                                        font-size: 50px;
+                                        line-height: 60px;
+                                        white-space: nowrap;
+                                        vertical-align: bottom;
+                                        font-weight: 700;
+                                    }
+                                `}
+                            </style>
                             {retryButton}
                             <p className="kpi s-execute-kpi">{data}</p>
                             <p>Full execution response and result as JSON:</p>
                             <pre style={resultStyle}>
-                                {JSON.stringify({ result, isLoading, error }, null, 2)}
+                                {JSON.stringify(
+                                    { result, isLoading, error },
+                                    null,
+                                    2
+                                )}
                             </pre>
                         </div>
                     );
