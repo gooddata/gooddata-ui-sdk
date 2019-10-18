5746202086ec5485649eb2dbb47bbc60a4c725fc
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-10-18 20:29:23 +0200

    Add tests for sort functions

--- libs/sdk-model/src/base/tests/typeGuards.test.ts
+++ libs/sdk-model/src/base/tests/typeGuards.test.ts
@@ -4,6 +4,7 @@ import { InvalidInputTestCases } from "../../../__mocks__/typeGuards";
 import { isDimension, MeasureGroupIdentifier, newDimension } from "../dimension";
 import { Won } from "../../../__mocks__/model";
 import { isIdentifierRef, isLocalIdRef, isUriRef } from "../index";
+import { isAttributeLocator, isAttributeSort, isMeasureLocator, isMeasureSort } from "../sort";
 
 describe("dimension type guard", () => {
     describe("isDimension", () => {
@@ -54,6 +55,70 @@ describe("dimension type guard", () => {
 
         it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
             expect(isLocalIdRef(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isAttributeSort", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [
+                true,
+                "attribute sort",
+                { attributeSortItem: { direction: "asc", attributeIdentifier: "localId1" } },
+            ],
+            [false, "measure sort", { measureSortItem: { direction: "asc", locators: [] } }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isAttributeSort(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isMeasureSort", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [
+                false,
+                "attribute sort",
+                { attributeSortItem: { direction: "asc", attributeIdentifier: "localId1" } },
+            ],
+            [true, "measure sort", { measureSortItem: { direction: "asc", locators: [] } }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isMeasureSort(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isAttributeLocator", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [
+                true,
+                "attribute locator",
+                { attributeLocatorItem: { attributeIdentifier: "localId", element: "value" } },
+            ],
+            [false, "measure locator", { measureLocatorItem: { measureIdentifier: "localId" } }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isAttributeLocator(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isMeasureLocator", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [
+                false,
+                "attribute locator",
+                { attributeLocatorItem: { attributeIdentifier: "localId", element: "value" } },
+            ],
+            [true, "measure locator", { measureLocatorItem: { measureIdentifier: "localId" } }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isMeasureLocator(input)).toBe(expectedResult);
         });
     });
 });
