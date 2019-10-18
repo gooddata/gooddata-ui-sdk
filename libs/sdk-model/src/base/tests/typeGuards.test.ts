0bd701b72ebfb3d8b52724ee8c2c44fc06e60179
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-10-18 17:30:16 +0200

    Add tests for ObjRef types

--- libs/sdk-model/src/base/tests/typeGuards.test.ts
+++ libs/sdk-model/src/base/tests/typeGuards.test.ts
@@ -3,17 +3,57 @@
 import { InvalidInputTestCases } from "../../../__mocks__/typeGuards";
 import { isDimension, MeasureGroupIdentifier, newDimension } from "../dimension";
 import { Won } from "../../../__mocks__/model";
+import { isIdentifierRef, isLocalIdRef, isUriRef } from "../index";
 
 describe("dimension type guard", () => {
     describe("isDimension", () => {
-        const TEST_DATA: Array<[boolean, string, any]> = [
+        const Scenarios: Array<[boolean, string, any]> = [
             ...InvalidInputTestCases,
             [true, "dimension", newDimension([MeasureGroupIdentifier])],
             [false, "measure", Won],
         ];
 
-        it.each(TEST_DATA)("should return %s when input is %s", (expectedResult, _desc, input) => {
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
             expect(isDimension(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isUriRef", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [true, "uri ref", { uri: "/" }],
+            [false, "identifier ref", { identifier: "id" }],
+            [false, "localId ref", { localIdentifier: "localId" }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isUriRef(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isIdentifierRef", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [false, "uri ref", { uri: "/" }],
+            [true, "identifier ref", { identifier: "id" }],
+            [false, "localId ref", { localIdentifier: "localId" }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isIdentifierRef(input)).toBe(expectedResult);
+        });
+    });
+
+    describe("isLocalIdRef", () => {
+        const Scenarios: Array<[boolean, string, any]> = [
+            ...InvalidInputTestCases,
+            [false, "uri ref", { uri: "/" }],
+            [false, "identifier ref", { identifier: "id" }],
+            [true, "localId ref", { localIdentifier: "localId" }],
+        ];
+
+        it.each(Scenarios)("should return %s when input is %s", (expectedResult, _desc, input) => {
+            expect(isLocalIdRef(input)).toBe(expectedResult);
         });
     });
 });
