d528bd664144bdb8f3fe51681ae477084c28377e
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-10-21 11:05:29 +0200

    Add tests for def factories and default dim generator
    - Added insight factory and builder - for now as part of test
      infrastructure

---
+++ libs/sdk-model/__mocks__/insights.ts
@@ -0,0 +1,71 @@
+// (C) 2019 GoodData Corporation
+import { SortItem, VisualizationProperties } from "../src";
+import { IBucket } from "../src/buckets";
+import { IInsight } from "../src/insight";
+import { IFilter } from "../src/filter";
+import identity = require("lodash/identity");
+
+/*
+ * Factory & builder for insight instances. Keeping it in test infrastructure for now, will see later on
+ * whether we should move it to prod code and expose on public API.
+ */
+
+export function newInsight(visClassId: string, modifications: InsightsModifications = identity): IInsight {
+    const builder = new InsightBuilder(visClassId);
+
+    return modifications(builder).build();
+}
+
+export type InsightsModifications = (builder: InsightBuilder) => InsightBuilder;
+
+export class InsightBuilder {
+    private insight: IInsight["insight"];
+
+    constructor(visClassId: string) {
+        this.insight = {
+            visualizationClassIdentifier: visClassId,
+            title: "Untitled",
+            buckets: [],
+            filters: [],
+            sorts: [],
+            properties: [],
+            identifier: "random",
+        };
+    }
+
+    public title = (title: string): InsightBuilder => {
+        this.insight.title = title;
+
+        return this;
+    };
+
+    public buckets = (buckets: IBucket[]): InsightBuilder => {
+        this.insight.buckets = buckets;
+
+        return this;
+    };
+
+    public filters = (filters: IFilter[]): InsightBuilder => {
+        this.insight.filters = filters;
+
+        return this;
+    };
+
+    public sorts = (sorts: SortItem[]): InsightBuilder => {
+        this.insight.sorts = sorts;
+
+        return this;
+    };
+
+    public properties = (properties: VisualizationProperties): InsightBuilder => {
+        this.insight.properties = properties;
+
+        return this;
+    };
+
+    public build = (): IInsight => {
+        return {
+            insight: this.insight,
+        };
+    };
+}
