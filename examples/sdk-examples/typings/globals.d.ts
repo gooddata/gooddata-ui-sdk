abaa6d9832d12100e343f376cd4281e1306c1ac3
Matyas Kandl <matyas.kandl@gooddata.com>
2019-10-21 21:45:46 +0200

    Add fork-ts-checker-webpack-plugin

--- examples/sdk-examples/typings/globals.d.ts
+++ examples/sdk-examples/typings/globals.d.ts
@@ -1,2 +1,15 @@
 // Globals injected by webpack
 declare var BASEPATH: string;
+
+declare module "*.png" {
+    const contents: string;
+    export = contents;
+}
+declare module "*.svg" {
+    const contents: string;
+    export = contents;
+}
+declare module "*.ico" {
+    const contents: string;
+    export = contents;
+}
