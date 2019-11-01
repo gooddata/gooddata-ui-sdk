e8aeeb4ee75248bdac106c5b8e8ef5b01eab51f1
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-11-01 12:01:40 +0100

    Initialize new SDK package sdk-ui-tests

---
+++ libs/sdk-ui-tests/__mocks__/styleMock.ts
@@ -0,0 +1,2 @@
+// (C) 2019 GoodData Corporation
+module.exports = {};
---
+++ libs/sdk-ui-tests/tests/smoke.test.ts
@@ -0,0 +1,7 @@
+// (C) 2007-2019 GoodData Corporation
+
+describe("smoke test", () => {
+    it("smokes", () => {
+        expect(true).toBeTruthy();
+    });
+});
