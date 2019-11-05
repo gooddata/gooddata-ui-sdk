f3992692067cc59f62db1ce34e7e13734b709b7d
Dan Homola <dan.homola@gooddata.com>
2019-11-05 16:15:25 +0100

    Add TSDoc for getAttributeDisplayForm
    JIRA: RAIL-1766

--- libs/sdk-backend-spi/src/metadata/index.ts
+++ libs/sdk-backend-spi/src/metadata/index.ts
@@ -11,6 +11,10 @@ export interface IWorkspaceMetadata {
     getVisualizationClass(id: string): Promise<IVisualizationClass>;
     getVisualizationClasses(): Promise<IVisualizationClass[]>;
     getInsight(id: string): Promise<IInsight>;
-
+    /**
+     * Gets the attribute display form with the provided identifier.
+     * @param id - identifier of the attribute display form to retrieve
+     * @public
+     */
     getAttributeDisplayForm(id: string): Promise<IAttributeDisplayForm>;
 }
