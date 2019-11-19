c8a9b7904f719fa3f8715a9bddfed2f8cd2e5e28
Petr Benes <petr.benes@gooddata.com>
2019-11-19 11:05:44 +0100

    Unify jest configuration files across all libraries
    JIRA ID: RAIL-1850

---
+++ common/config/jest/jest.config.base.js
@@ -0,0 +1,18 @@
+// (C) 2019 GoodData Corporation
+module.exports = {
+    preset: "ts-jest",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+    testRegex: "(\\.test)\\.(tsx?)$",
+    collectCoverage: false,
+    moduleFileExtensions: ["ts", "js", "tsx"],
+    moduleNameMapper: {
+        "^[./a-zA-Z0-9$_-]+\\.svg$": "<rootDir>/jestSvgStub.js",
+        "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.ts",
+    },
+    testPathIgnorePatterns: [",/node_modules/", "/dist/", "/src/_defunct/"],
+    globals: {
+        "ts-jest": {
+            packageJson: "<rootDir>/package.json",
+        },
+    },
+};
---
+++ libs/sdk-ui-tests/jestSvgStub.js
@@ -0,0 +1,2 @@
+// jest doesn't need to load real svg files during tests
+module.exports = "";
