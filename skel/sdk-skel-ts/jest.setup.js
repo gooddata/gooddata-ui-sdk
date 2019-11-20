2d2ef81e6ae2f4726bd18462b2bd9f1000c19f3d
Petr Benes <petr.benes@gooddata.com>
2019-11-20 14:58:01 +0100

    Unify jest config for skeletons
    JIRA ID: RAIL-1850

--- skel/sdk-skel-ts/jest.setup.js
+++ skel/sdk-skel-ts/jest.setup.js
@@ -1,8 +1,6 @@
 // (C) 2019 GoodData Corporation
-// tslint:disable:no-console
-
 const consoleError = console.error;
-console.error = (err: any, ...args: any) => {
+console.error = (err, ...args) => {
     consoleError(err, ...args);
     throw new Error(err);
 };
