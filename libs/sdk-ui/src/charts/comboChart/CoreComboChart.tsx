3d61c7d0888c833a92e3ef233c2805160c301b91
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-11-26 11:10:47 +0100

    Remove beta warning from ComboChart

--- libs/sdk-ui/src/charts/comboChart/CoreComboChart.tsx
+++ libs/sdk-ui/src/charts/comboChart/CoreComboChart.tsx
@@ -1,13 +1,11 @@
 // (C) 2007-2018 GoodData Corporation
 import * as React from "react";
-import { visualizationIsBetaWarning } from "../../base/helpers/utils";
 import { ICoreChartProps } from "../chartProps";
 import { BaseChart } from "../_base/BaseChart";
 
 export class CoreComboChart extends React.PureComponent<ICoreChartProps, null> {
     constructor(props: ICoreChartProps) {
         super(props);
-        visualizationIsBetaWarning();
     }
 
     public render() {
