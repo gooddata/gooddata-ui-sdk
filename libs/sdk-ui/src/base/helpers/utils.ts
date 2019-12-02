119915df9bdddb36490fff602b03001de8591c1d
Lubo Slivka <lubomir.slivka@gooddata.com>
2019-12-02 19:32:37 +0100

    Remove unused utils
    -  These functions are no longer used
    -  Telemetry headers now encapsulated in backend-spi
    -  obj id from URI now in bear backend impl

--- libs/sdk-ui/src/base/helpers/utils.ts
+++ libs/sdk-ui/src/base/helpers/utils.ts
@@ -1,22 +1,5 @@
 // (C) 2007-2019 GoodData Corporation
-import isObject = require("lodash/isObject");
-import { SDK } from "@gooddata/gd-bear-client";
 import isNil = require("lodash/isNil");
-import { name as pkgName, version as pkgVersion } from "../../../package.json";
-
-export function setTelemetryHeaders(sdk: SDK, componentName: string, props: object) {
-    sdk.config.setJsPackage(pkgName, pkgVersion);
-
-    sdk.config.setRequestHeader("X-GDC-JS-SDK-COMP", componentName);
-    if (isObject(props)) {
-        sdk.config.setRequestHeader("X-GDC-JS-SDK-COMP-PROPS", Object.keys(props).join(","));
-    }
-}
-
-export function getObjectIdFromUri(uri: string): string {
-    const match = /\/obj\/([^$\/\?]*)/.exec(uri);
-    return match ? match[1] : null;
-}
 
 export function visualizationIsBetaWarning() {
     // tslint:disable-next-line no-console
