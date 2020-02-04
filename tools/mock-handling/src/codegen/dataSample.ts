b8b1a495a29d3050f26e15216e2a620b253d247c
tulean <letuananh1996@gmail.com>
2020-02-04 14:29:13 +0100

    Add data sample codegen for display forms
    JIRA: RAIL-2013

---
+++ tools/mock-handling/src/codegen/dataSample.ts
@@ -0,0 +1,80 @@
+// (C) 2020 GoodData Corporation
+import { OptionalKind, VariableDeclarationKind, VariableStatementStructure } from "ts-morph";
+import * as path from "path";
+import { createUniqueVariableName } from "../base/variableNaming";
+import { DisplayFormRecording } from "../recordings/displayForms";
+import groupBy = require("lodash/groupBy");
+
+const DataSampleConstName = "DataSamples";
+
+type DataSampleRecording = [string, DisplayFormRecording];
+
+function generateRecordingForDataSample(entries: DataSampleRecording[]): string {
+    return `{ ${entries
+        .map(([_, entryRecording]) => {
+            return `${entryRecording.getAttributeElements().map((element, index) => {
+                return `${createUniqueVariableName(
+                    element.title,
+                )} : ${entryRecording.getRecordingName()}[${index}]`;
+            })}`;
+        })
+        .join(",")} }`;
+}
+
+function comparatorDataSample(a: [string, DataSampleRecording[]], b: [string, DataSampleRecording[]]) {
+    if (a[0] < b[0]) return -1;
+    if (a[0] > b[0]) return 1;
+    return 0;
+}
+
+function generateDataSampleConst(
+    recordings: DisplayFormRecording[],
+): OptionalKind<VariableStatementStructure> {
+    const recsWithDataSample: DataSampleRecording[] = recordings.map(rec => [
+        createUniqueVariableName(rec.getDisplayFormTitle()),
+        rec,
+    ]);
+
+    const entriesDataSample = Object.entries(groupBy(recsWithDataSample, ([title]) => title)).sort(
+        comparatorDataSample,
+    );
+    return {
+        declarationKind: VariableDeclarationKind.Const,
+        isExported: true,
+        declarations: [
+            {
+                name: DataSampleConstName,
+                initializer: `{ ${entriesDataSample
+                    .map(([title, rec]) => `${title}: ${generateRecordingForDataSample(rec)}`)
+                    .join(",")} }`,
+            },
+        ],
+    };
+}
+
+/**
+ * Generate constants for the display form recordings and data sample. This function will return non-exported constant per recording
+ * and then also an exported 'DataSamples' constant that is a map from data sample => display form => recording.
+ *
+ * @param recordings - recordings to generate constants for
+ * @param targetDir - absolute path to directory where index will be stored, this is needed so that paths can be
+ *   made relative for require()
+ */
+export function generateConstantsForDataSamples(
+    recordings: DisplayFormRecording[],
+    targetDir: string,
+): Array<OptionalKind<VariableStatementStructure>> {
+    const recConsts = recordings.map(rec => {
+        return {
+            declarationKind: VariableDeclarationKind.Const,
+            isExported: false,
+            declarations: [
+                {
+                    name: rec.getRecordingName(),
+                    initializer: `require('./${path.relative(targetDir, rec.elementFile)}')`,
+                },
+            ],
+        };
+    });
+    return [...recConsts, generateDataSampleConst(recordings)];
+}
---
+++ tools/reference-workspace/src/recordings/dataSample.ts
@@ -0,0 +1,21 @@
+// (C) 2020 GoodData Corporation
+/* tslint:disable:file-header */
+/* tslint:disable:variable-name */
+/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-02-05T14:13:35.546Z; */
+
+const df_label_product_id_name = require("./metadata/displayForms/label.product.id.name/elements.json");
+const df_label_owner_department = require("./metadata/displayForms/label.owner.department/elements.json");
+const df_label_owner_region = require("./metadata/displayForms/label.owner.region/elements.json");
+export const DataSamples = {
+    Department: { DirectSales: df_label_owner_department[0], InsideSales: df_label_owner_department[1] },
+    ProductName: {
+        CompuSci: df_label_product_id_name[0],
+        Educationly: df_label_product_id_name[1],
+        Explorer: df_label_product_id_name[2],
+        GrammarPlus: df_label_product_id_name[3],
+        PhoenixSoft: df_label_product_id_name[4],
+        TouchAll: df_label_product_id_name[5],
+        WonderKid: df_label_product_id_name[6],
+    },
+    Region: { EastCoast: df_label_owner_region[0], WestCoast: df_label_owner_region[1] },
+};
