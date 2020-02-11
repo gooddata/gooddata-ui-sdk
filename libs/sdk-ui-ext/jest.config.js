03a7ab16a11ecaec2d8aa00eeb616f275feda2de
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-02-11 09:32:49 +0100

    Split off sdk-ui-ext and sdk-ui-pivot from sdk-ui

---
+++ libs/sdk-ui-ext/jest.config.js
@@ -0,0 +1,19 @@
+// (C) 2019 GoodData Corporation
+process.env = Object.assign(process.env, { NODE_ICU_DATA: "node_modules/full-icu" });
+const base = require("../../common/config/jest/jest.config.base.js");
+module.exports = {
+    ...base,
+    /*
+     * cannot use jest-environment-enzyme nor latest jsdom from Jest 25.
+     *
+     * see: https://github.com/ag-grid/ag-grid/issues/3488
+     *
+     * TODO: split pivot table to separate package asap and keep this dirt only in that other project.
+     *  using jsdom 14 we are losing the Jest-advertised performance boost that comes with jsdom 15
+     */
+    testEnvironment: "jest-environment-jsdom-fourteen",
+    testEnvironmentOptions: {
+        enzymeAdapter: "react16",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts", "jest-enzyme"],
+};
