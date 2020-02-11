64fdf371effb5e942215e61f800c5215362556fc
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-02-11 17:27:06 +0100

    Cleanup sdk-ui deps & Jest setup
    -  Removed jsdom-fourteen & gd-bear-* stufff from dependencies
    -  Reconfigured Jest to use jsdom

--- libs/sdk-ui/jest.ci.js
+++ libs/sdk-ui/jest.ci.js
@@ -5,17 +5,6 @@ const ciBase = require("../../common/config/jest/jest.config.ci.base.js");
 module.exports = {
     ...ciBase,
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-    /*
-     * cannot use jest-environment-enzyme nor latest jsdom from Jest 25.
-     *
-     * see: https://github.com/ag-grid/ag-grid/issues/3488
-     *
-     * TODO: split pivot table to separate package asap and keep this dirt only in that other project.
-     *  using jsdom 14 we are losing the Jest-advertised performance boost that comes with jsdom 15
-     */
-    testEnvironment: "jest-environment-jsdom-fourteen",
-    testEnvironmentOptions: {
-        enzymeAdapter: "react16",
-    },
+    testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts", "jest-enzyme"],
 };
