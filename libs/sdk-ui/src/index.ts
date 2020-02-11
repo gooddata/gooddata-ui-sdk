00ea84ce3063f8e75765231ad8dac9b270aad46a
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-02-11 13:58:18 +0100

    Make sdk-ui build successfully

--- libs/sdk-ui/src/index.ts
+++ libs/sdk-ui/src/index.ts
@@ -5,7 +5,6 @@ import "./polyfills";
 export * from "./base";
 export * from "./charts";
 export * from "./highcharts";
-export * from "./pivotTable";
 export * from "./filters";
 export * from "./execution";
 export * from "./kpi";
