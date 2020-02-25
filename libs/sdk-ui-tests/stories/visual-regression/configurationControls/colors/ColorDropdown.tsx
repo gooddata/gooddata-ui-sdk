356a0fb0cbb6e7c7dbab5cda48eda9012758b26d
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-02-25 14:35:07 +0100

    Sanitize color dropdown test scenarios
    -  Too many irrelevant / duplicate scenarios
    -  Flaky tests verifying what happens when color is clicked
       we know what happens: popup with color disappears
       fast run: its gone at screenshot time
       slow run: its still there :)
       additionally, these scenarios do not verify much, because
       there is no plumbing in the stories that would change the color of the
       button-icon-thing that shows currently selected color
    -  Sanitized test scenario definitions.. reduced unnecessary duplication

--- libs/sdk-ui-tests/stories/visual-regression/configurationControls/colors/ColorDropdown.tsx
+++ libs/sdk-ui-tests/stories/visual-regression/configurationControls/colors/ColorDropdown.tsx
@@ -17,66 +17,27 @@ const wrapperStyle = { width: 400, height: 800, padding: "1em 1em" };
 const mobileViewport = [{ label: "mobile-view", height: 800, width: 480 }];
 const german = "de-DE";
 
-const scenario1 = {
+const defaultScenarios = {
     closed: {},
     opened: {
-        clickSelector: ".s-colored-items-list-item",
-        postInteractionWait: 200,
-    },
-    "dropdown-opened": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list"],
-        postInteractionWait: 200,
-    },
-    "select-color": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list", ".s-color-list-item-7"],
+        clickSelector: ".icon-navigatedown",
         postInteractionWait: 200,
     },
     "dropdown-opened-mobile": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list"],
-        postInteractionWait: 200,
-        viewports: mobileViewport,
-    },
-    "select-color-mobile": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list", ".s-color-list-item-7"],
+        clickSelectors: [".icon-navigatedown"],
         postInteractionWait: 200,
         viewports: mobileViewport,
     },
 };
 
-const scenario2 = {
-    closed: {},
-    opened: {
-        clickSelector: ".s-colored-items-list-item",
-        postInteractionWait: 200,
-    },
-    "dropdown-opened": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list"],
-        postInteractionWait: 200,
-    },
-    "select-color": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list", ".s-color-list-item-12_13"],
-        postInteractionWait: 200,
-    },
-    "dropdown-opened-mobile": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list"],
-        postInteractionWait: 200,
-        viewports: mobileViewport,
-    },
-    "select-color-mobile": {
-        clickSelectors: [".s-colored-items-list-item", ".s-color-drop-down-list", ".s-color-list-item-12_13"],
-        postInteractionWait: 200,
-        viewports: mobileViewport,
-    },
-};
-
-const scenario3 = {
-    scenario2,
+const customPicker = {
+    ...defaultScenarios,
     "select-custom-color": {
-        clickSelectors: [".s-colored-items-list-item", ".s-custom_color"],
+        clickSelectors: [".s-colored-items-list-item", ".s-custom-section-button"],
         postInteractionWait: 200,
     },
     "select-custom-color-mobile": {
-        clickSelectors: [".s-colored-items-list-item", ".s-custom_color"],
+        clickSelectors: [".s-colored-items-list-item", ".s-custom-section-button"],
         postInteractionWait: 200,
         viewports: mobileViewport,
     },
@@ -92,31 +53,17 @@ storiesOf(`${ConfigurationControls}/Colors/ColorDropdown`, module)
                         onColorSelected={action("onColorSelect")}
                         showCustomPicker={false}
                     >
-                        <ColoredItemContent text="aaa" color={{ r: 20, g: 178, b: 226 }} />
+                        <ColoredItemContent
+                            text="<<< this color hardcoded for tests"
+                            color={{ r: 20, g: 178, b: 226 }}
+                        />
                     </ColorDropdown>
                 </InternalIntlWrapper>
             </div>,
-            scenario1,
+            defaultScenarios,
         );
     })
-    .add("ColorDropdown selected guid:4 color", () => {
-        return withMultipleScreenshots(
-            <div style={wrapperStyle} className="screenshot-target">
-                <InternalIntlWrapper>
-                    <ColorDropdown
-                        selectedColorItem={{ type: "guid", value: "4" }}
-                        colorPalette={DefaultColorPalette}
-                        onColorSelected={action("onColorSelect")}
-                        showCustomPicker={false}
-                    >
-                        <ColoredItemContent text="aaa" color={{ r: 20, g: 178, b: 226 }} />
-                    </ColorDropdown>
-                </InternalIntlWrapper>
-            </div>,
-            scenario1,
-        );
-    })
-    .add("ColorDropdown selected guid 11_12 with large palette", () => {
+    .add("ColorDropdown pre-selected with large palette", () => {
         return withMultipleScreenshots(
             <div style={wrapperStyle} className="screenshot-target">
                 <InternalIntlWrapper>
@@ -126,11 +73,14 @@ storiesOf(`${ConfigurationControls}/Colors/ColorDropdown`, module)
                         onColorSelected={action("onColorSelect")}
                         showCustomPicker={false}
                     >
-                        <ColoredItemContent text="aaa" color={{ r: 20, g: 178, b: 226 }} />
+                        <ColoredItemContent
+                            text="<<< this color hardcoded for tests"
+                            color={{ r: 20, g: 178, b: 226 }}
+                        />
                     </ColorDropdown>
                 </InternalIntlWrapper>
             </div>,
-            scenario2,
+            defaultScenarios,
         );
     })
     .add("ColorDropdown with custom picker", () => {
@@ -142,11 +92,14 @@ storiesOf(`${ConfigurationControls}/Colors/ColorDropdown`, module)
                         onColorSelected={action("onColorSelect")}
                         showCustomPicker={true}
                     >
-                        <ColoredItemContent text="aaa" color={{ r: 20, g: 178, b: 226 }} />
+                        <ColoredItemContent
+                            text="<<< this color hardcoded for tests"
+                            color={{ r: 20, g: 178, b: 226 }}
+                        />
                     </ColorDropdown>
                 </InternalIntlWrapper>
             </div>,
-            scenario3,
+            customPicker,
         );
     })
     .add("ColorDropdown with custom picker - localized", () => {
@@ -158,21 +111,13 @@ storiesOf(`${ConfigurationControls}/Colors/ColorDropdown`, module)
                         onColorSelected={action("onColorSelect")}
                         showCustomPicker={true}
                     >
-                        <ColoredItemContent text="aaa" color={{ r: 20, g: 178, b: 226 }} />
+                        <ColoredItemContent
+                            text="<<< this color hardcoded for tests"
+                            color={{ r: 20, g: 178, b: 226 }}
+                        />
                     </ColorDropdown>
                 </InternalIntlWrapper>
             </div>,
-            {
-                scenario2,
-                "select-custom-color": {
-                    clickSelectors: [".s-colored-items-list-item", ".s-custom-section-button"],
-                    postInteractionWait: 200,
-                },
-                "select-custom-color-mobile": {
-                    clickSelectors: [".s-colored-items-list-item", ".s-custom-section-button"],
-                    postInteractionWait: 200,
-                    viewports: mobileViewport,
-                },
-            },
+            customPicker,
         );
     });
