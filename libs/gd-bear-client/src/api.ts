3c9622a3bd76a2b911c846138e28d9036301f709
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-03-26 13:29:01 +0100

    Short circuit identifierToUri if empty input
    -  Resolve immediately to empty output

---
+++ libs/gd-bear-client/src/api.ts
@@ -0,0 +1,9 @@
+// (C) 2007-2020 GoodData Corporation
+
+import { IUriIdentifierPair } from "./metadata";
+import { ApiResponse, ApiResponseError, ApiNetworkError } from "./xhr";
+import { ApiExecutionResponseError } from "./execution/execute-afm";
+export { ApiResponse, ApiResponseError, ApiNetworkError, ApiExecutionResponseError, IUriIdentifierPair };
+export { isUri } from "./util";
+
+export * from "./interfaces";
