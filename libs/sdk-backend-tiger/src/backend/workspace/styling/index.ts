404da11614498a5f981124b3e201e452ffed3d2f
Matyas Kandl <matyas.kandl@gooddata.com>
2020-03-30 15:04:36 +0200

    Add default color palette to sdk-backend-tiger

--- libs/sdk-backend-tiger/src/backend/workspace/styling/index.ts
+++ libs/sdk-backend-tiger/src/backend/workspace/styling/index.ts
@@ -3,11 +3,12 @@ import { IWorkspaceStylingService } from "@gooddata/sdk-backend-spi";
 import { IColorPaletteItem } from "@gooddata/sdk-model";
 
 import { TigerAuthenticatedCallGuard } from "../../../types";
+import { DefaultColorPalette } from "./mocks/colorPalette";
 
 export class TigerWorkspaceStyling implements IWorkspaceStylingService {
     constructor(private readonly authCall: TigerAuthenticatedCallGuard, public readonly workspace: string) {}
 
     public colorPalette = async (): Promise<IColorPaletteItem[]> => {
-        return this.authCall(async () => []);
+        return this.authCall(async () => DefaultColorPalette);
     };
 }
