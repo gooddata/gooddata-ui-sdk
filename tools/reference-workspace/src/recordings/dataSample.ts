d8d4031b0bbefa87bd692928156bd5bce732a162
Dan Homola <dan.homola@gooddata.com>
2020-04-16 17:38:26 +0200

    Refresh recordings
    JIRA: RAIL-1951

--- tools/reference-workspace/src/recordings/dataSample.ts
+++ tools/reference-workspace/src/recordings/dataSample.ts
@@ -2,7 +2,7 @@
 /* tslint:disable:file-header */
 /* tslint:disable:variable-name */
 /* tslint:disable:no-var-requires */
-/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-04-16T11:43:31.187Z; */
+/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-04-16T15:38:05.791Z; */
 
 const df_label_product_id_name = require("./metadata/displayForms/label.product.id.name/elements.json");
 const df_label_owner_department = require("./metadata/displayForms/label.owner.department/elements.json");
