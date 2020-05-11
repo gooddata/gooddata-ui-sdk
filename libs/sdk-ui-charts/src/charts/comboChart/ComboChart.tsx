d25305e0948f94302e712033264fc8358d179c1b
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-05-11 22:05:29 +0200

    Add API docs for ComboChart

--- libs/sdk-ui-charts/src/charts/comboChart/ComboChart.tsx
+++ libs/sdk-ui-charts/src/charts/comboChart/ComboChart.tsx
@@ -79,15 +79,37 @@ function getConfiguration(props: IComboChartProps): IChartConfig {
 //
 
 /**
- * TODO: SDK8: add docs
- *
  * @public
  */
 export interface IComboChartBucketProps {
+    /**
+     * Optionally specify primary measures to render using the primary chart type.
+     */
     primaryMeasures?: IMeasure[];
+
+    /**
+     * Optionally specify secondary measures to render using the secondary chart type.
+     */
     secondaryMeasures?: IMeasure[];
+
+    /**
+     * Optionally specify one or two attributes to use for slicing the measure values along the
+     * X axis.
+     *
+     * If you specify two attributes, the values of these attributes will appear on the X axis as grouped. For each
+     * value of the first attribute there will be all applicable values of the second attribute. For each value of the
+     * second attribute there will be a point/column/area indicating the respective slice's value.
+     */
     viewBy?: IAttribute | IAttribute[];
+
+    /**
+     * Optionally specify filters to apply on the data to chart.
+     */
     filters?: IFilter[];
+
+    /**
+     * Optionally specify how to sort the data to chart.
+     */
     sortBy?: ISortItem[];
 }
 
@@ -100,7 +122,14 @@ export interface IComboChartProps extends IBucketChartProps, IComboChartBucketPr
 
 /**
  * [ComboChart](https://sdk.gooddata.com/gooddata-ui/docs/combo_chart_component.html)
- * is a component with bucket props primaryMeasures, secondaryMeasures, viewBy, filters
+ *
+ * Combo chart combines two types of visualizations, for example, a column chart and a line chart. A combo chart can
+ * have one or two axes. If a combo chart has two axes, it is often referred to as a dual axis chart.
+ *
+ * By default, a combo chart is displayed as a combination of a column chart and a line chart, with the secondary axis
+ * enabled (you can [disable it](https://sdk.gooddata.com/gooddata-ui/docs/combo_chart_component.html#disable-the-secondary-axis)).
+ *
+ * The chart types used to display primary and secondary measures can be customized in {@link IChartConfig}.
  *
  * @public
  */
