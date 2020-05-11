a9f6fcd6c3f27e317bbc9fcacf4639ba681f4785
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-05-11 22:20:44 +0200

    Unify Donut API with pie, add docs
    -  Donut does not have sortBy - which is available for Pie
    -  A hole in the middle probably does not mean that sortBy should not
       be present

--- libs/sdk-ui-charts/src/charts/donutChart/DonutChart.tsx
+++ libs/sdk-ui-charts/src/charts/donutChart/DonutChart.tsx
@@ -1,5 +1,5 @@
 // (C) 2007-2018 GoodData Corporation
-import { IAttributeOrMeasure, IAttribute, IFilter, newBucket } from "@gooddata/sdk-model";
+import { IAttributeOrMeasure, IAttribute, IFilter, newBucket, ISortItem } from "@gooddata/sdk-model";
 import { BucketNames } from "@gooddata/sdk-ui";
 import { roundChartDimensions } from "../_commons/dimensions";
 import { IBucketChartProps } from "../../interfaces";
@@ -12,12 +12,13 @@ import { withChart } from "../_base/withChart";
 //
 
 const donutChartDefinition: IChartDefinition<IDonutChartBucketProps, IDonutChartProps> = {
-    bucketPropsKeys: ["measures", "viewBy", "filters"],
+    bucketPropsKeys: ["measures", "viewBy", "filters", "sortBy"],
     bucketsFactory: props => {
-        return [
-            newBucket(BucketNames.MEASURES, ...props.measures),
-            newBucket(BucketNames.VIEW, props.viewBy),
-        ];
+        const measures: IAttributeOrMeasure[] = Array.isArray(props.measures)
+            ? props.measures
+            : [props.measures];
+
+        return [newBucket(BucketNames.MEASURES, ...measures), newBucket(BucketNames.VIEW, props.viewBy)];
     },
     executionFactory: (props, buckets) => {
         const { backend, workspace } = props;
@@ -27,6 +28,7 @@ const donutChartDefinition: IChartDefinition<IDonutChartBucketProps, IDonutChart
             .workspace(workspace)
             .execution()
             .forBuckets(buckets, props.filters)
+            .withSorting(...props.sortBy)
             .withDimensions(roundChartDimensions);
     },
 };
@@ -36,26 +38,51 @@ const donutChartDefinition: IChartDefinition<IDonutChartBucketProps, IDonutChart
 //
 
 /**
- * TODO: SDK8: add docs
- *
  * @public
  */
 export interface IDonutChartBucketProps {
-    measures: IAttributeOrMeasure[];
+    /**
+     * Specify one or more measures to segment the donut chart.
+     *
+     * If you specify a single measure, then you may further specify the viewBy attribute - there will be
+     * donut slice per attribute value.
+     *
+     * If you specify multiple measures, then there will be a donut slice for each measure value. You may not
+     * specify the viewBy in this case.
+     */
+    measures: IAttributeOrMeasure | IAttributeOrMeasure[];
+
+    /**
+     * Optionally specify viewBy attribute that will be used to create the donut slices. There will be a slice
+     * for each value of the attribute.
+     */
     viewBy?: IAttribute;
+
+    /**
+     * Optionally specify filters to apply on the data to chart.
+     */
     filters?: IFilter[];
+
+    /**
+     * Optionally specify how to sort the data to chart.
+     */
+    sortBy?: ISortItem[];
 }
 
 /**
- * TODO: SDK8: add docs
- *
  * @public
  */
 export interface IDonutChartProps extends IBucketChartProps, IDonutChartBucketProps {}
 
 /**
  * [DonutChart](http://sdk.gooddata.com/gooddata-ui/docs/donut_chart_component.html)
- * is a component with bucket props measures, viewBy, filters
+ *
+ * Donut chart shows data as proportional segments of a disc and has a hollowed out center.
+ * Donut charts can be segmented by either multiple measures or an attribute, and allow viewers to visualize
+ * component parts of a whole.
+ *
+ * Note: the donut chart slices are by default sorted from largest to smallest. There is also a limit on the
+ * number of slices that will be charted.
  *
  * @public
  */
