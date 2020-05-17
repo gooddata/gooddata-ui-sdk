9f1b9ccd4c1573a07684f495bfbb736720a56a50
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-05-17 20:27:28 +0200

    Add infrastructure for integrated tests into bear backend
    -  Integrated tests use Wiremock as a proxy server handing out recorded
       API call responses
    -  Infrastructure uses Wiremock standalone, running inside docker
    -  Scripts & infra in place to capture / refresh recordings & to run tests
       against recordings

---
+++ libs/sdk-backend-bear/tests/integrated/backend.ts
@@ -0,0 +1,63 @@
+// (C) 2020 GoodData Corporation
+
+import {
+    AuthenticatedPrincipal,
+    AuthenticationContext,
+    IAnalyticalBackend,
+    IAuthenticationProvider,
+} from "@gooddata/sdk-backend-spi";
+import bearFactory, { BearAuthProviderBase, FixedLoginAndPasswordAuthProvider } from "../../src";
+import { config } from "dotenv";
+import { InvariantError } from "ts-invariant";
+
+let GlobalBackend: IAnalyticalBackend | undefined;
+
+/*
+ * This implementation of authentication provider is used when integrated tests run against mock data.
+ *
+ * The login itself is not needed in this context: recordings are done against live server while authenticated,
+ * the authentication information is not saved anywhere and does not figure in the recordings.
+ *
+ * The recordings are matched against requests purely based on payload & no auth headers or cookies.
+ * There is no state on the wiremock server.
+ */
+class NoLoginAuthProvider extends BearAuthProviderBase implements IAuthenticationProvider {
+    public async authenticate(context: AuthenticationContext): Promise<AuthenticatedPrincipal> {
+        await this.obtainCurrentPrincipal(context);
+
+        return this.principal!;
+    }
+}
+
+function createBackend(): IAnalyticalBackend {
+    const backend = bearFactory({ hostname: "localhost:8443" });
+
+    if (process.env.GD_BEAR_REC) {
+        const credentials = config();
+
+        if (!credentials.parsed?.GD_USERNAME || !credentials.parsed?.GD_PASSWORD) {
+            throw new InvariantError(
+                "You have started integrated tests in recording mode - this mode requires " +
+                    "credentials in order to log into platform. The credentials must be stored in .env file located " +
+                    "in sdk-backend-bear directory. This a dotenv file and should contain GD_USERNAME and GD_PASSWORD.",
+            );
+        }
+
+        const authProvider = new FixedLoginAndPasswordAuthProvider(
+            credentials.parsed!.GD_USERNAME,
+            credentials.parsed!.GD_PASSWORD,
+        );
+
+        return backend.withAuthentication(authProvider);
+    }
+
+    return backend.withAuthentication(new NoLoginAuthProvider());
+}
+
+export function testBackend(): IAnalyticalBackend {
+    if (!GlobalBackend) {
+        GlobalBackend = createBackend();
+    }
+
+    return GlobalBackend;
+}
