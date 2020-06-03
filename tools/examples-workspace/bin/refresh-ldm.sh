d9ef372ac7382a79e1ad8aefa0a04fc2025ca505
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-06-03 15:47:25 +0200

    Initialize new SDK tool examples-workspace

---
+++ tools/examples-workspace/bin/refresh-ldm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPTDIR=`dirname $0`
+ROOTDIR="${SCRIPTDIR}/.."
+
+EXPORTER="${ROOTDIR}/node_modules/.bin/gdc-catalog-export"
+OUTPUT="${ROOTDIR}/src/ldm/full.ts"
+
+$EXPORTER \
+  --hostname "developer.na.gooddata.com" \
+  --project-id "xms7ga4tf3g3nzucd8380o2bev8oeknp" \
+  --output "${OUTPUT}"
---
+++ tools/examples-workspace/src/index.ts
@@ -0,0 +1,5 @@
+// (C) 2019-2020 GoodData Corporation
+
+import * as ExamplesLdm from "./ldm/full";
+
+export { ExamplesLdm };
