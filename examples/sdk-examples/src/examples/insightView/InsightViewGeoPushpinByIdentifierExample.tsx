4767958744bae78b8d0916e26a65dfff058088f1
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-06-09 17:15:25 +0200

    Add live example for GeoPushpin embedding using InsightView

---
+++ examples/sdk-examples/src/examples/insightView/InsightViewGeoPushpinByIdentifierExample.tsx
@@ -0,0 +1,20 @@
+// (C) 2007-2018 GoodData Corporation
+import React from "react";
+
+import { InsightView } from "@gooddata/sdk-ui-ext";
+
+import { Ldm } from "../../ldm";
+import { MAPBOX_TOKEN } from "../../constants/fixtures";
+
+export const InsightViewGeoPushpinByIdentifierExample: React.FC = () => {
+    return (
+        <div style={{ height: 600, position: "relative" }} className="s-insightView-geo">
+            <InsightView
+                insight={Ldm.Insights.GeoPushpinChart}
+                config={{
+                    mapboxToken: MAPBOX_TOKEN,
+                }}
+            />
+        </div>
+    );
+};
