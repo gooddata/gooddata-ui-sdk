7780e1a430c7c7e0480d5addb5845034036e0264
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-06-15 14:00:42 +0200

    Switch integrated tests to authoritative mode

--- libs/sdk-backend-bear/scripts/test-ci.sh
+++ libs/sdk-backend-bear/scripts/test-ci.sh
@@ -11,12 +11,7 @@ JEST_JUNIT_OUTPUT=./ci/results/test-results.xml jest --ci --config jest.ci.js
 UNIT_RC=$?
 
 JEST_JUNIT_OUTPUT=./ci/results/integrated-test-results.xml NODE_TLS_REJECT_UNAUTHORIZED=0 jest --config "integrated-test.config.js"
-
-#
-# Note: integrated tests on CI fail now and this ignores that failure. Its some infra stuff with docker & ports, need
-# to troubleshoot.
-#
-INTEGRATED_RC=0
+INTEGRATED_RC=$?
 
 if [[ $UNIT_RC -ne 0 || $INTEGRATED_RC -ne 0 ]]; then
   exit 1
