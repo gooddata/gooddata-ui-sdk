1ad55241847c9b97b73f0d5700798b6c2702a724
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-06-18 09:18:15 +0200

    Fix the insight view dual-axis-bar-chart example
    -  Config was not used (was not supported until last week)

--- examples/sdk-examples/src/examples/advanced/chartConfiguration/InsightViewDualAxisBarChartExample.tsx
+++ examples/sdk-examples/src/examples/advanced/chartConfiguration/InsightViewDualAxisBarChartExample.tsx
@@ -3,34 +3,32 @@ import React, { Component } from "react";
 
 import { InsightView } from "@gooddata/sdk-ui-ext";
 import { Ldm } from "../../../ldm";
+import { totalCostsLocalId, totalSalesLocalIdentifier } from "../../../ldm/ext";
 
 export class InsightViewDualAxisBarChartExample extends Component {
     public render() {
-        // const config = {
-        //     secondary_xaxis: {
-        //         visible: true,
-        //         labelsEnabled: true,
-        //         rotation: "auto",
-        //         min: "-75000000",
-        //         max: "75000000",
-        //         measures: [totalSalesLocalIdentifier],
-        //     },
-        //     xaxis: {
-        //         visible: true,
-        //         labelsEnabled: true,
-        //         rotation: "auto",
-        //         min: "-75000000",
-        //         max: "75000000",
-        //         measures: [totalCostsLocalIdentifier],
-        //     },
-        // };
+        const config = {
+            secondary_xaxis: {
+                visible: true,
+                labelsEnabled: true,
+                rotation: "auto",
+                min: "-75000000",
+                max: "75000000",
+                measures: [totalSalesLocalIdentifier],
+            },
+            xaxis: {
+                visible: true,
+                labelsEnabled: true,
+                rotation: "auto",
+                min: "-75000000",
+                max: "75000000",
+                measures: [totalCostsLocalId],
+            },
+        };
 
         return (
             <div style={{ height: 300 }} className="s-insightView-dual-axis-bar">
-                <InsightView
-                    insight={Ldm.Insights.DualAxisBarChart}
-                    // config={config}
-                />
+                <InsightView insight={Ldm.Insights.DualAxisBarChart} config={config} />
             </div>
         );
     }
