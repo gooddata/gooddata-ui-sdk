3801390d623e41c533b86ce384fdd3b9968bc390
Dan Homola <dan.homola@gooddata.com>
2020-07-01 14:27:34 +0200

    Add storybook tests for MeasureNumberFormat
    This envelopes all the cases tested in the original with much less code.
    
    Also a README was added.
    
    JIRA: RAIL-2107

--- libs/sdk-ui-tests/stories/visual-regression/_infra/storyGroups.ts
+++ libs/sdk-ui-tests/stories/visual-regression/_infra/storyGroups.ts
@@ -6,3 +6,4 @@ export const PlugVizStories = "04 Stories For Pluggable Vis";
 export const EmbeddingStories = "05 Stories For Embedding using InsightView";
 export const FilterStories = "10 Filters";
 export const ConfigurationControls = "11 Configuration Controls";
+export const UiKit = "12 UI Kit";
---
+++ libs/sdk-ui-tests/stories/visual-regression/kit/MeasureNumberFormat/MeasureNumberFormat.tsx
@@ -0,0 +1,134 @@
+// (C) 2007-2019 GoodData Corporation
+import {
+    MeasureNumberFormat,
+    IFormatPreset,
+    IToggleButtonProps,
+    IFormatTemplate,
+} from "@gooddata/sdk-ui-kit";
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withMultipleScreenshots } from "../../_infra/backstopWrapper";
+import { UiKit } from "../../_infra/storyGroups";
+
+import "@gooddata/sdk-ui-kit/styles/css/main.css";
+import { ISeparators } from "@gooddata/sdk-ui";
+
+const wrapperStyle = { width: 400, height: 800, padding: "1em 1em" };
+
+const documentationLink =
+    "https://help.gooddata.com/doc/en/reporting-and-dashboards/reports/working-with-reports/formatting-numbers-in-reports";
+
+const presets: IFormatPreset[] = [
+    {
+        name: "Whole number",
+        localIdentifier: "whole-number",
+        format: "#,##0",
+        previewNumber: 1000.12,
+    },
+    {
+        name: "Decimal number",
+        localIdentifier: "decimal-number",
+        format: "[=null]--;\n" + "[<.3][red]#,##0.00;\n" + "[>.8][green]#,##0.00;\n" + "#,##0.00",
+        previewNumber: 1000.12,
+    },
+    {
+        name: "Percentage",
+        localIdentifier: "percentage",
+        format: "#,##0.0%",
+        previewNumber: 1000.12,
+    },
+    {
+        name: "Currency",
+        localIdentifier: "currency",
+        format: "€ #,##0.0",
+        previewNumber: 1000.12,
+    },
+];
+
+const templates: IFormatTemplate[] = [
+    {
+        name: "Whole number",
+        localIdentifier: "whole-number",
+        format: "#,##0",
+    },
+    {
+        name: "Decimal number",
+        localIdentifier: "decimal-number",
+        format: "[=null]--;\n" + "[<.3][red]#,##0.00;\n" + "[>.8][green]#,##0.00;\n" + "#,##0.00",
+    },
+    {
+        name: "Percentage",
+        localIdentifier: "percentage",
+        format: "#,##0.0%",
+    },
+    {
+        name: "Currency",
+        localIdentifier: "currency",
+        format: "€ #,##0.0",
+    },
+];
+
+const separators: ISeparators = {
+    thousand: " ",
+    decimal: ".",
+};
+
+storiesOf(`${UiKit}/MeasureNumberFormat`, module).add("full-featured", () => {
+    const ToggleButton: React.FC<IToggleButtonProps> = ({ toggleDropdown, text }) => (
+        <button
+            type="button"
+            className="s-measure-number-format-button gd-button gd-button-secondary gd-button-small"
+            onClick={toggleDropdown}
+        >
+            {text}
+        </button>
+    );
+
+    return withMultipleScreenshots(
+        <div style={wrapperStyle} className="screenshot-target">
+            <MeasureNumberFormat
+                toggleButton={ToggleButton}
+                presets={presets}
+                separators={separators}
+                templates={templates}
+                selectedFormat={null}
+                setFormat={action(`selected format`)}
+                documentationLink={documentationLink}
+            />
+        </div>,
+        {
+            closed: {},
+            opened: { clickSelector: ".s-measure-number-format-button", postInteractionWait: 200 },
+            "custom-editor": {
+                clickSelectors: [".s-measure-number-format-button", ".s-format-preset-customFormat"],
+                postInteractionWait: 200,
+            },
+            "custom-editor-extended-preview": {
+                clickSelectors: [
+                    ".s-measure-number-format-button",
+                    ".s-format-preset-customFormat",
+                    ".s-custom-format-dialog-extended-preview-button",
+                ],
+                postInteractionWait: 200,
+            },
+            "custom-editor-templates": {
+                clickSelectors: [
+                    ".s-measure-number-format-button",
+                    ".s-format-preset-customFormat",
+                    ".s-measure-format-templates-toggle-button",
+                ],
+                postInteractionWait: 200,
+            },
+            "custom-editor-templates-selected": {
+                clickSelectors: [
+                    ".s-measure-number-format-button",
+                    ".s-format-preset-customFormat",
+                    ".s-measure-format-templates-toggle-button",
+                    ".s-measure-format-template-decimal_number",
+                ],
+                postInteractionWait: 200,
+            },
+        },
+    );
+});
