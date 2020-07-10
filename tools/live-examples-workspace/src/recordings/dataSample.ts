fe66a9bf5b6ac48c98a1a59abff48dabed1f8e49
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-07-10 10:39:17 +0200

    Post rebase recording refresh

--- tools/live-examples-workspace/src/recordings/dataSample.ts
+++ tools/live-examples-workspace/src/recordings/dataSample.ts
@@ -2,6 +2,6 @@
 /* tslint:disable:file-header */
 /* tslint:disable:variable-name */
 /* tslint:disable:no-var-requires */
-/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-07-09T06:42:45.365Z; */
+/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-07-10T08:44:07.811Z; */
 
 export const DataSamples = {};
