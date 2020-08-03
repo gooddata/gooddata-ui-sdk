2ea938623af4d5fe9f4f2b7f5388dcce8a6363cb
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-08-03 14:10:17 +0200

    Cleanup sort mocks to use sort item factories

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pivotTable/tests/sortMocks.ts
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pivotTable/tests/sortMocks.ts
@@ -1,107 +1,31 @@
 // (C) 2020 GoodData Corporation
 
 // sortItems based on referencePointMocks.simpleStackedReferencePoint
-import { IAttributeSortItem, IMeasureSortItem } from "@gooddata/sdk-model";
+import {
+    IAttributeSortItem,
+    IMeasureSortItem,
+    newAttributeLocator,
+    newAttributeSort,
+    newMeasureSort,
+} from "@gooddata/sdk-model";
 
-export const validMeasureSort: IMeasureSortItem = {
-    measureSortItem: {
-        direction: "asc",
-        locators: [
-            {
-                attributeLocatorItem: {
-                    attributeIdentifier: "a2",
-                    element: "/gdc/md/PROJECTID/obj/2210/elements?id=1234",
-                },
-            },
-            {
-                measureLocatorItem: {
-                    measureIdentifier: "m1",
-                },
-            },
-        ],
-    },
-};
-export const validAttributeSort: IAttributeSortItem = {
-    attributeSortItem: {
-        attributeIdentifier: "a1",
-        direction: "desc",
-    },
-};
-export const invalidAttributeSort: IAttributeSortItem = {
-    attributeSortItem: {
-        attributeIdentifier: "invalid",
-        direction: "desc",
-    },
-};
-export const invalidMeasureSortInvalidMeasure: IMeasureSortItem = {
-    measureSortItem: {
-        direction: "asc",
-        locators: [
-            {
-                attributeLocatorItem: {
-                    attributeIdentifier: "a2",
-                    element: "/gdc/md/PROJECTID/obj/2210/elements?id=1234",
-                },
-            },
-            {
-                measureLocatorItem: {
-                    measureIdentifier: "invalid", // this measure is not in buckets
-                },
-            },
-        ],
-    },
-};
-export const invalidMeasureSortInvalidAttribute: IMeasureSortItem = {
-    measureSortItem: {
-        direction: "asc",
-        locators: [
-            {
-                attributeLocatorItem: {
-                    attributeIdentifier: "a1", // this identifier doesn't exist on second dimension
-                    element: "/gdc/md/PROJECTID/obj/2210/elements?id=1234",
-                },
-            },
-            {
-                measureLocatorItem: {
-                    measureIdentifier: "m1",
-                },
-            },
-        ],
-    },
-};
-export const invalidMeasureSortTooManyLocators: IMeasureSortItem = {
-    measureSortItem: {
-        direction: "asc",
-        locators: [
-            {
-                attributeLocatorItem: {
-                    attributeIdentifier: "a2",
-                    element: "/gdc/md/PROJECTID/obj/2210/elements?id=1234",
-                },
-            },
-            {
-                attributeLocatorItem: {
-                    attributeIdentifier: "a2",
-                    element: "/gdc/md/PROJECTID/obj/2210/elements?id=1234",
-                },
-            },
-            {
-                measureLocatorItem: {
-                    measureIdentifier: "m1",
-                },
-            },
-        ],
-    },
-};
-export const invalidMeasureSortLocatorsTooShort: IMeasureSortItem = {
-    measureSortItem: {
-        direction: "asc",
-        locators: [
-            {
-                measureLocatorItem: {
-                    measureIdentifier: "m1",
-                },
-            },
-        ],
-    },
-};
+export const validMeasureSort: IMeasureSortItem = newMeasureSort("m1", "asc", [
+    newAttributeLocator("a2", "/gdc/md/PROJECTID/obj/2210/elements?id=1234"),
+]);
+export const validAttributeSort: IAttributeSortItem = newAttributeSort("a1", "desc");
+export const invalidAttributeSort: IAttributeSortItem = newAttributeSort("invalid", "desc");
+
+export const invalidMeasureSortInvalidMeasure: IMeasureSortItem = newMeasureSort("invalid", "asc", [
+    newAttributeLocator("a2", "/gdc/md/PROJECTID/obj/2210/elements?id=1234"),
+]);
+
+export const invalidMeasureSortInvalidAttribute: IMeasureSortItem = newMeasureSort("m1", "asc", [
+    newAttributeLocator("a1", "/gdc/md/PROJECTID/obj/2210/elements?id=1234"),
+]);
+
+export const invalidMeasureSortTooManyLocators: IMeasureSortItem = newMeasureSort("m1", "asc", [
+    newAttributeLocator("a2", "/gdc/md/PROJECTID/obj/2210/elements?id=1234"),
+    newAttributeLocator("a2", "/gdc/md/PROJECTID/obj/2210/elements?id=1234"),
+]);
+
+export const invalidMeasureSortLocatorsTooShort: IMeasureSortItem = newMeasureSort("m1", "asc");
