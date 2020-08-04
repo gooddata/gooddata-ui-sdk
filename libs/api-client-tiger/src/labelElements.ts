282d0b37f4451ff757d29d766682c990a4251a35
Dan Homola <dan.homola@gooddata.com>
2020-08-04 17:27:55 +0200

    Fix api-extractor issues in api-client-tiger
    JIRA: RAIL-1815

--- libs/api-client-tiger/src/labelElements.ts
+++ libs/api-client-tiger/src/labelElements.ts
@@ -1,6 +1,19 @@
 // (C) 2019-2020 GoodData Corporation
 import { AxiosInstance } from "axios";
-import { ElementsControllerApi, ElementsControllerApiInterface } from "./generated/afm-rest-api";
+import {
+    ElementsControllerApi,
+    ElementsControllerApiInterface,
+    Configuration,
+    ConfigurationParameters,
+} from "./generated/afm-rest-api";
+import { BaseAPI, RequestArgs } from "./generated/afm-rest-api/base";
+
+export {
+    Configuration as LabelElementsConfiguration,
+    ConfigurationParameters as LabelElementsConfigurationParameters,
+    BaseAPI as LabelElementsBaseApi,
+    RequestArgs as LabelElementsRequestArgs,
+};
 
 export const tigerLabelElementsClientFactory = (axios: AxiosInstance): ElementsControllerApiInterface =>
     new ElementsControllerApi({}, "", axios);
--- libs/api-client-tiger/src/metadata.ts
+++ libs/api-client-tiger/src/metadata.ts
@@ -1,6 +1,19 @@
 // (C) 2019-2020 GoodData Corporation
 import { AxiosInstance } from "axios";
-import { DefaultApi, DefaultApiInterface } from "./generated/metadata-json-api";
+import {
+    DefaultApi,
+    DefaultApiInterface,
+    Configuration,
+    ConfigurationParameters,
+} from "./generated/metadata-json-api";
+import { BaseAPI, RequestArgs } from "./generated/metadata-json-api/base";
+
+export {
+    Configuration as MetadataConfiguration,
+    ConfigurationParameters as MetadataConfigurationParameters,
+    BaseAPI as MetadataBaseApi,
+    RequestArgs as MetadataRequestArgs,
+};
 
 export const tigerMetadataClientFactory = (axios: AxiosInstance): DefaultApiInterface =>
     new DefaultApi({}, "/api/metadata", axios);
