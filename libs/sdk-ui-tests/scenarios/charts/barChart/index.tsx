2f08a58794432bfc996ca1e895511a8fbeb137a1
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-09-04 09:50:57 +0200

    Fix import naming for BarChart test scenarios

--- libs/sdk-ui-tests/scenarios/charts/barChart/index.tsx
+++ libs/sdk-ui-tests/scenarios/charts/barChart/index.tsx
@@ -4,7 +4,7 @@ import axisCustomization from "./axisCustomization";
 import base from "./base";
 import coloring from "./coloring";
 import customization from "./customization";
-import drilling from "./stacking";
-import stacking from "./drilling";
+import stacking from "./stacking";
+import drilling from "./drilling";
 
 export default [base, ...axisCustomization, ...coloring, ...customization, drilling, stacking];
