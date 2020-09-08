def2cbb1c7ec3e5b4c5d800c0acf8f77c178f4af
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-09-08 15:11:40 +0200

    Change recordings so that fixed project id appears in URIs
    -  Using name of the workspace without dash. This is to prevent possible
       false negatives / errors in code that relies on parsing URIs using regex

--- tools/experimental-workspace/bin/refresh-recordings.sh
+++ tools/experimental-workspace/bin/refresh-recordings.sh
@@ -9,5 +9,6 @@ RECORDING_DIR="${ROOTDIR}/src/recordings"
 $RECORDER \
   --hostname "staging3.intgdc.com" \
   --project-id "d79dpgty2b4ydewi6kbzqm4fq1be2ltm" \
+  --replace-project-id "experimentalworkspace" \
   --accept-untrusted-ssl \
   --recordingDir "${RECORDING_DIR}"
