ea385f02124156cc45dbce99fb7cab77d0933599
Dan Homola <dan.homola@gooddata.com>
2020-09-10 13:28:46 +0200

    Fix styles in ComboChart InsightView example
    JIRA: RAIL-2652

--- examples/sdk-examples/src/examples/insightView/InsightViewComboChartByIdentifierExample.tsx
+++ examples/sdk-examples/src/examples/insightView/InsightViewComboChartByIdentifierExample.tsx
@@ -4,9 +4,11 @@ import { InsightView } from "@gooddata/sdk-ui-ext";
 
 import { Ldm } from "../../ldm";
 
+const style = { height: 300 };
+
 export const InsightViewComboChartByIdentifierExample: React.FC = () => {
     return (
-        <div className="s-insightView-chart">
+        <div style={style} className="s-insightView-chart">
             <InsightView insight={Ldm.Insights.JZACombo} />
         </div>
     );
