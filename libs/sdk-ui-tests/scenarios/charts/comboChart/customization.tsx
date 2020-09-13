b3de1b3ee419c08da14b2b75a058ef769bf074b3
fiedlr <info@fiedlr.sk>
2020-09-13 23:55:25 +0200

    tests for combocharts done

--- libs/sdk-ui-tests/scenarios/charts/comboChart/customization.tsx
+++ libs/sdk-ui-tests/scenarios/charts/comboChart/customization.tsx
@@ -2,9 +2,27 @@
 import { ComboChart, IComboChartProps } from "@gooddata/sdk-ui-charts";
 import { scenariosFor } from "../../../src";
 import { dataLabelCustomizer } from "../_infra/dataLabelVariants";
+import { dataPointCustomizer } from "../_infra/dataPointVariants";
 import { legendCustomizer } from "../_infra/legendVariants";
 import { ComboChartWithTwoMeasuresAndViewBy } from "./base";
 import { ScenarioGroupNames } from "../_infra/groupNames";
+
+export function dataPointCustomizerForComboCharts<T extends VisProps>(
+    baseName: string,
+    baseProps: UnboundVisProps<T>,
+): Array<CustomizedScenario<T>> {
+    return dataPointCustomizer(baseName, baseProps).map((c) => [
+        c[0],
+        {
+            ...c[1],
+            config: {
+                ...c[1].config,
+                primaryChartType: "line",
+                secondaryChartType: "area",
+            },
+        },
+    ]);
+}
 
 const legendScenarios = scenariosFor<IComboChartProps>("ComboChart", ComboChart)
     .withGroupNames(ScenarioGroupNames.ConfigurationCustomization)
@@ -18,4 +36,10 @@ const dataLabelScenarios = scenariosFor<IComboChartProps>("ComboChart", ComboCha
     .withDefaultTags("vis-config-only", "mock-no-scenario-meta")
     .addScenarios("data labels", ComboChartWithTwoMeasuresAndViewBy, dataLabelCustomizer);
 
-export default [legendScenarios, dataLabelScenarios];
+const dataPointScenarios = scenariosFor<IComboChartProps>("ComboChart", ComboChart)
+    .withGroupNames(ScenarioGroupNames.ConfigurationCustomization)
+    .withVisualTestConfig({ groupUnder: "data points" })
+    .withDefaultTags("vis-config-only", "mock-no-scenario-meta")
+    .addScenarios("data points", ComboChartWithTwoMeasuresAndViewBy, dataPointCustomizerForComboCharts);
+
+export default [legendScenarios, dataLabelScenarios, dataPointScenarios];
