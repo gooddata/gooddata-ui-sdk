5066f1bd33195332c9420ca6db869a36419d8189
fiedlr <info@fiedlr.sk>
2020-09-16 14:14:22 +0200

    remove prepublish

--- libs/sdk-ui-charts/prepublish.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-fswatch -r src/ | (while read;
-do
- echo "Running prepublish.."
- npm run prepublishOnly
- cp -r ./dist ~/gd/gdc-analytical-designer/node_modules/\@gooddata/sdk-ui-charts
- echo "Done!\n"
-done)
