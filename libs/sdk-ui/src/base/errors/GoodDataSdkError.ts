9635de5e788dd11ac835fd1a44a2d89e4b730841
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-09-21 09:39:12 +0200

    Refactor SDK error codes and exceptions
    -  Added exception per code (can be expanded in the future to contain additional information about the error)
    -  Added type guards
    
    JIRA: RAIL-1815

--- libs/sdk-ui/src/base/errors/GoodDataSdkError.ts
+++ libs/sdk-ui/src/base/errors/GoodDataSdkError.ts
@@ -3,16 +3,43 @@
 import isEmpty from "lodash/isEmpty";
 
 /**
- * This is common SDK-land exception.
+ * Error codes recognized by the SDK.
  *
  * @public
  */
-export class GoodDataSdkError extends Error {
-    public readonly sdkError: boolean = true;
+export const ErrorCodes = {
+    BAD_REQUEST: "BAD_REQUEST",
+    UNAUTHORIZED: "UNAUTHORIZED",
+    GEO_LOCATION_MISSING: "GEO_LOCATION_MISSING",
+    GEO_MAPBOX_TOKEN_MISSING: "GEO_MAPBOX_TOKEN_MISSING",
+    DATA_TOO_LARGE_TO_DISPLAY: "DATA_TOO_LARGE_TO_DISPLAY",
+    DATA_TOO_LARGE_TO_COMPUTE: "DATA_TOO_LARGE_TO_COMPUTE",
+    NEGATIVE_VALUES: "NEGATIVE_VALUES",
+    NO_DATA: "NO_DATA",
+    NOT_FOUND: "NOT_FOUND",
+    PROTECTED_REPORT: "PROTECTED_REPORT",
+    UNKNOWN_ERROR: "UNKNOWN_ERROR",
+    CANCELLED: "CANCELLED",
+};
 
+/**
+ * @public
+ */
+export type SdkErrorType = keyof typeof ErrorCodes;
+
+/**
+ * Base class for all anticipated GoodData.UI SDK errors.
+ *
+ * @public
+ */
+export abstract class GoodDataSdkError extends Error {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    constructor(public message: string, public cause?: any) {
-        super(message);
+    constructor(public readonly seType: SdkErrorType, message?: string, public readonly cause?: any) {
+        /*
+         * This is here to keep exception handling in client code initially backward compatible. Previosly
+         * GoodDataSdkError had the error code inside the message itself. Keeping it that way.
+         */
+        super(message ?? seType);
 
         Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
     }
@@ -27,10 +54,125 @@ export class GoodDataSdkError extends Error {
     /**
      * Underlying cause of this error (if any).
      */
-    public getCause(): any {
+    public getCause(): any | undefined {
         return this.cause;
     }
 }
+
+/**
+ * This error means that server could not understand the request due to invalid syntax.
+ */
+export class BadRequestSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.BAD_REQUEST as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that you are not authorized.
+ */
+export class UnauthorizedSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.UNAUTHORIZED as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that location bucket is missing
+ */
+export class GeoLocationMissingSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.GEO_LOCATION_MISSING as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that mapbox token of GeoChart is missing
+ */
+export class GeoTokenMissingSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.GEO_MAPBOX_TOKEN_MISSING as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that executed data were too large to be displayed by GoodData.UI.
+ */
+export class DataTooLargeToDisplaySdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.DATA_TOO_LARGE_TO_DISPLAY as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that processed request would generate a result too large to be processed
+ * by GoodData platform.
+ */
+export class DataTooLargeToComputeSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.DATA_TOO_LARGE_TO_COMPUTE as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that processed result contains negative values which does not make
+ * sense within the given visualization (e.g. pie chart with negative values).
+ */
+export class NegativeValuesSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.NEGATIVE_VALUES as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that the processed result does not contain any data.
+ */
+export class NoDataSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.NO_DATA as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that requested entity (e.g. a visualization) was not found on the server.
+ */
+export class NotFoundSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.NO_DATA as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that requested visualization is restricted by access rules within the GoodData platform.
+ * Please contact your administrator.
+ */
+export class ProtectedReportSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.PROTECTED_REPORT as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that GoodData.UI does not know how to handle such error.
+ */
+export class UnknownSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.UNKNOWN_ERROR as SdkErrorType, message, cause);
+    }
+}
+
+/**
+ * This error means that request has been cancelled usually after component has been unmounted.
+ */
+export class CancelledSdkError extends GoodDataSdkError {
+    constructor(message?: string, cause?: Error) {
+        super(ErrorCodes.CANCELLED as SdkErrorType, message, cause);
+    }
+}
+
+//
+//
+//
 
 /**
  * Typeguard checking whether input is an instance of {@link GoodDataSdkError};
@@ -38,70 +180,113 @@ export class GoodDataSdkError extends Error {
  * @public
  */
 export function isGoodDataSdkError(obj: unknown): obj is GoodDataSdkError {
-    return !isEmpty(obj) && (obj as GoodDataSdkError).sdkError === true;
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType !== undefined;
 }
 
-export const ErrorCodes = {
-    /**
-     * This error means that server could not understand the request due to invalid syntax.
-     */
-    BAD_REQUEST: "BAD_REQUEST",
+/**
+ * Typeguard checking whether input is an instance of {@link BadRequestSdkError};
+ *
+ * @public
+ */
+export function isBadRequest(obj: unknown): obj is BadRequestSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "BAD_REQUEST";
+}
 
-    /**
-     * This error means that you are not authorized.
-     */
-    UNAUTHORIZED: "UNAUTHORIZED",
+/**
+ * Typeguard checking whether input is an instance of {@link UnauthorizedSdkError};
+ *
+ * @public
+ */
+export function isUnauthorized(obj: unknown): obj is UnauthorizedSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "UNAUTHORIZED";
+}
 
-    /**
-     * This error means that location bucket is missing
-     */
-    GEO_LOCATION_MISSING: "GEO_LOCATION_MISSING",
+/**
+ * Typeguard checking whether input is an instance of {@link GeoLocationMissingSdkError};
+ *
+ * @public
+ */
+export function isGeoLocationMissing(obj: unknown): obj is GeoLocationMissingSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "GEO_LOCATION_MISSING";
+}
 
-    /**
-     * This error means that mapbox token of GeoChart is missing
-     */
-    GEO_MAPBOX_TOKEN_MISSING: "GEO_MAPBOX_TOKEN_MISSING",
+/**
+ * Typeguard checking whether input is an instance of {@link GeoTokenMissingSdkError};
+ *
+ * @public
+ */
+export function isGeoTokenMissing(obj: unknown): obj is GeoTokenMissingSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "GEO_MAPBOX_TOKEN_MISSING";
+}
 
-    /**
-     * This error means that executed data were too large to be displayed by GoodData.UI.
-     */
-    DATA_TOO_LARGE_TO_DISPLAY: "DATA_TOO_LARGE_TO_DISPLAY",
+/**
+ * Typeguard checking whether input is an instance of {@link DataTooLargeToDisplaySdkError};
+ *
+ * @public
+ */
+export function isDataTooLargeToDisplay(obj: unknown): obj is DataTooLargeToDisplaySdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "DATA_TOO_LARGE_TO_DISPLAY";
+}
 
-    /**
-     * This error means that processed request would generate a result too large to be processed
-     * by GoodData platform.
-     */
-    DATA_TOO_LARGE_TO_COMPUTE: "DATA_TOO_LARGE_TO_COMPUTE",
+/**
+ * Typeguard checking whether input is an instance of {@link DataTooLargeToComputeSdkError};
+ *
+ * @public
+ */
+export function isDataTooLargeToCompute(obj: unknown): obj is DataTooLargeToComputeSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "DATA_TOO_LARGE_TO_COMPUTE";
+}
 
-    /**
-     * This error means that processed result contains negative values which does not make
-     * sense within the given visualization (e.g. pie chart with negative values).
-     */
-    NEGATIVE_VALUES: "NEGATIVE_VALUES",
+/**
+ * Typeguard checking whether input is an instance of {@link NegativeValuesSdkError};
+ *
+ * @public
+ */
+export function isNegativeValues(obj: unknown): obj is NegativeValuesSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "NEGATIVE_VALUES";
+}
 
-    /**
-     * This error means that the processed result does not contain any data.
-     */
-    NO_DATA: "NO_DATA",
+/**
+ * Typeguard checking whether input is an instance of {@link NoDataSdkError};
+ *
+ * @public
+ */
+export function isNoDataSdkError(obj: unknown): obj is NoDataSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "NO_DATA";
+}
 
-    /**
-     * This error means that requested entity (e.g. a visualization) was not found on the server.
-     */
-    NOT_FOUND: "NOT_FOUND",
+/**
+ * Typeguard checking whether input is an instance of {@link NotFoundSdkError};
+ *
+ * @public
+ */
+export function isNotFound(obj: unknown): obj is NotFoundSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "NOT_FOUND";
+}
 
-    /**
-     * This error means that requested visualization is restricted by access rules within the GoodData platform.
-     * Please contact your administrator.
-     */
-    PROTECTED_REPORT: "PROTECTED_REPORT",
+/**
+ * Typeguard checking whether input is an instance of {@link ProtectedReportSdkError};
+ *
+ * @public
+ */
+export function isProtectedReport(obj: unknown): obj is ProtectedReportSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "PROTECTED_REPORT";
+}
 
-    /**
-     * This error means that GoodData.UI does not know how to handle such error.
-     */
-    UNKNOWN_ERROR: "UNKNOWN_ERROR",
+/**
+ * Typeguard checking whether input is an instance of {@link UnknownSdkError};
+ *
+ * @public
+ */
+export function isUnknownSdkError(obj: unknown): obj is UnknownSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "UNKNOWN_ERROR";
+}
 
-    /**
-     * This error means that request has been cancelled usually after component has been unmounted.
-     */
-    CANCELLED: "CANCELLED",
-};
+/**
+ * Typeguard checking whether input is an instance of {@link CancelledSdkError};
+ *
+ * @public
+ */
+export function isCancelledSdkError(obj: unknown): obj is CancelledSdkError {
+    return !isEmpty(obj) && (obj as GoodDataSdkError).seType === "CANCELLED";
+}
