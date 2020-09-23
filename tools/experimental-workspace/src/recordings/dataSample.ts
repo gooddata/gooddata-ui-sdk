3b33b43a4247ac79e56ada911c46d0733736af45
Jan Lipnican <yanlipnican@gmail.com>
2020-09-23 09:52:28 +0200

    Fix ranking filter scenarios
    Recordings were not exported properly

--- tools/experimental-workspace/src/recordings/dataSample.ts
+++ tools/experimental-workspace/src/recordings/dataSample.ts
@@ -2,6 +2,6 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable header/header */
-/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-09-17T07:59:17.104Z; */
+/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2020-09-22T14:20:01.002Z; */
 
 export const DataSamples = {};
