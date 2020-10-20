0ab819271c6e80197a85a522e299e4176fa59ee6
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-10-20 18:49:20 +0200

    Expand dep cruiser rules to have stricter checking in highcharts
    JIRA: RAIL-1815

--- libs/sdk-ui-charts/.dependency-cruiser.js
+++ libs/sdk-ui-charts/.dependency-cruiser.js
@@ -7,6 +7,22 @@ options = {
         depCruiser.isolatedSubmodule("chart-interfaces", "src/interfaces"),
         depCruiser.moduleWithDependencies("highcharts", "src/highcharts", ["src/interfaces"]),
         depCruiser.moduleWithDependencies("charts", "src/charts", ["src/highcharts", "src/interfaces"]),
+        depCruiser.moduleWithDependencies(
+            "chart-options-builder",
+            "src/highcharts/chartTypes/_chartOptions",
+            [
+                "src/highcharts/chartTypes/_util/*",
+                "src/interfaces",
+                "src/highcharts/typings/*",
+                "src/highcharts/constants/*",
+            ],
+        ),
+        depCruiser.moduleWithDependencies("chart-creators", "src/highcharts/chartTypes/_chartCreators", [
+            "src/highcharts/chartTypes/_util/*",
+            "src/interfaces",
+            "src/highcharts/typings/*",
+            "src/highcharts/constants/*",
+        ]),
     ],
     options: depCruiser.DefaultOptions,
 };
