bdcbfcac8cec1165234c774a2fefa4b4d291cc96
Dan Homola <dan.homola@gooddata.com>
2020-11-30 10:55:53 +0100

    Simplify DashbaordItem{Kpi,Visualization}
    We keep the components separate in case we need them to differ more later.
    
    JIRA: RAIL-2819

--- libs/sdk-ui-ext/src/internal/dashboardEmbedding/DashboardItem/DashboardItemVisualization.tsx
+++ libs/sdk-ui-ext/src/internal/dashboardEmbedding/DashboardItem/DashboardItemVisualization.tsx
@@ -2,70 +2,13 @@
 import React from "react";
 import cx from "classnames";
 
-import { DashboardItemContent } from "./DashboardItemContent";
-import { DashboardItemContentWrapper } from "./DashboardItemContentWrapper";
+import { DashboardItemBase, IDashboardItemBaseProps } from "./DashboardItemBase";
 
-interface IDashboardItemVisualizationProps {
-    /**
-     * Render prop for the content itself.
-     */
-    children: (params: { clientWidth: number }) => React.ReactNode;
-    /**
-     * Render prop for the item headline.
-     */
-    renderHeadline?: () => React.ReactNode;
-    /**
-     * Render prop for content rendered inside the main content before the visualization container.
-     */
-    renderBeforeVisualization?: () => React.ReactNode;
-    /**
-     * Render prop for content rendered inside the main content after the visualization container.
-     */
-    renderAfterVisualization?: () => React.ReactNode;
-    /**
-     * Render prop for content rendered before the main content.
-     */
-    renderBeforeContent?: () => React.ReactNode;
-    /**
-     * Render prop for content rendered after the main content.
-     */
-    renderAfterContent?: () => React.ReactNode;
-    /**
-     * Class name applied to the main content.
-     */
-    contentClassName?: string;
-    /**
-     * Class name applied to the visualization container.
-     */
-    visualizationClassName?: string;
-}
-
-export const DashboardItemVisualization: React.FC<IDashboardItemVisualizationProps> = ({
-    children,
-    contentClassName,
+export const DashboardItemVisualization: React.FC<IDashboardItemBaseProps> = ({
     visualizationClassName,
-    renderHeadline = () => null,
-    renderBeforeVisualization = () => null,
-    renderAfterVisualization = () => null,
-    renderBeforeContent = () => null,
-    renderAfterContent = () => null,
+    ...props
 }) => {
     return (
-        <DashboardItemContentWrapper>
-            {({ clientWidth }) => (
-                <>
-                    {renderBeforeContent()}
-                    <DashboardItemContent className={contentClassName}>
-                        {renderBeforeVisualization()}
-                        <div className={cx("visualization", visualizationClassName)}>
-                            {renderHeadline()}
-                            {children({ clientWidth })}
-                        </div>
-                        {renderAfterVisualization()}
-                    </DashboardItemContent>
-                    {renderAfterContent()}
-                </>
-            )}
-        </DashboardItemContentWrapper>
+        <DashboardItemBase {...props} visualizationClassName={cx("visualization", visualizationClassName)} />
     );
 };
