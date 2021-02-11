21a94d7baa5e7a0861b29de66600ea31a38c1ce1
patrikbraborec <patrik.braborec@gooddata.com>
2021-02-11 17:14:59 +0100

    Fix date formatting with multiple dates
    - Change logic of findDateAttributeUris function
    - Fix UT related to new logic
    
    JIRA: ONE-4887

--- libs/sdk-backend-bear/src/convertors/dateFormatting/dateFormatter.ts
+++ libs/sdk-backend-bear/src/convertors/dateFormatting/dateFormatter.ts
@@ -1,4 +1,4 @@
-// (C) 2020 GoodData Corporation
+// (C) 2020-2021 GoodData Corporation
 import {
     IAttributeDescriptor,
     IDimensionDescriptor,
@@ -10,37 +10,38 @@ import { createDateValueFormatter } from "./dateValueFormatter";
 import { DateFormat, DEFAULT_DATE_FORMAT } from "./dateValueParser";
 import { createDefaultDateFormatter } from "./defaultDateFormatter";
 
-export function findDateAttributeUri(dimensions: IDimensionDescriptor[]): string | undefined {
-    const dateAttrDescriptor = dimensions
+// TODO: rewrite. move search for date attributes into data view facade / meta() section
+export function findDateAttributeUris(dimensions: IDimensionDescriptor[]): string[] {
+    const dateAttrsDescriptor = dimensions
         .reduce((attrDimensions: IAttributeDescriptor[], dimensionItem: IDimensionDescriptor) => {
             attrDimensions.push(...dimensionItem.headers.filter(isAttributeDescriptor));
             return attrDimensions;
         }, [])
-        .find(
+        .filter(
             (attrDescriptor: IAttributeDescriptor): boolean =>
                 attrDescriptor.attributeHeader.type === "GDC.time.day_us",
         );
-    if (!dateAttrDescriptor) {
-        return undefined;
-    }
-    return dateAttrDescriptor.attributeHeader.formOf.uri;
+    return dateAttrsDescriptor.map((dateAttrDescriptor) => dateAttrDescriptor.attributeHeader.formOf.uri);
 }
 
 export function transformDateFormat(
     resultHeader: IResultHeader,
-    dateAttributeUri?: string,
+    dateAttributeUris?: string[],
     dateFormat?: DateFormat,
 ): IResultHeader {
     if (
         !isResultAttributeHeader(resultHeader) ||
-        !dateAttributeUri ||
+        !dateAttributeUris?.length ||
         !dateFormat ||
         dateFormat === DEFAULT_DATE_FORMAT
     ) {
         return resultHeader;
     }
     const resultHeaderUri = resultHeader.attributeHeaderItem.uri;
-    if (!resultHeaderUri.startsWith(dateAttributeUri)) {
+    const foundUri = dateAttributeUris.some((dateAttributeUri) =>
+        resultHeaderUri.startsWith(dateAttributeUri),
+    );
+    if (!foundUri) {
         return resultHeader;
     }
     try {
