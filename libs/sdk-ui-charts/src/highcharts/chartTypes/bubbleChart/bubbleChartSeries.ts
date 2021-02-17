e95687f7214119196e2b4c44e185a568faa56c85
Honza Strouhal <strouhal.jan@gmail.com>
2021-02-17 10:22:45 +0100

    BUGFIX: BB-2801 bubble chart legend
    null values are not displayed in legend

--- libs/sdk-ui-charts/src/highcharts/chartTypes/bubbleChart/bubbleChartSeries.ts
+++ libs/sdk-ui-charts/src/highcharts/chartTypes/bubbleChart/bubbleChartSeries.ts
@@ -18,32 +18,34 @@ export function getBubbleChartSeries(
 ): any[] {
     const primaryMeasuresBucketEmpty = dv.def().isBucketEmpty(BucketNames.MEASURES);
     const secondaryMeasuresBucketEmpty = dv.def().isBucketEmpty(BucketNames.SECONDARY_MEASURES);
-
+    let legendIndex = 0;
     return dv
         .rawData()
         .twoDimData()
         .map((resData: any, index: number) => {
-            let data: any = [];
-            if (resData[0] !== null && resData[1] !== null && resData[2] !== null) {
-                const emptyBucketsCount = getCountOfEmptyBuckets([
-                    primaryMeasuresBucketEmpty,
-                    secondaryMeasuresBucketEmpty,
-                ]);
-                data = [
-                    {
-                        x: !primaryMeasuresBucketEmpty ? parseValue(resData[0]) : 0,
-                        y: !secondaryMeasuresBucketEmpty ? parseValue(resData[1 - emptyBucketsCount]) : 0,
-                        // we want to allow NaN on z to be able show bubble of default size when Size bucket is empty
-                        z: parseFloat(resData[2 - emptyBucketsCount]),
-                        format: unwrap(last(measureGroup.items)).format, // only for dataLabel format
-                    },
-                ];
+            if (resData[0] === null || resData[1] === null || resData[2] === null) {
+                return null;
             }
+            let data: any = [];
+            const emptyBucketsCount = getCountOfEmptyBuckets([
+                primaryMeasuresBucketEmpty,
+                secondaryMeasuresBucketEmpty,
+            ]);
+            data = [
+                {
+                    x: !primaryMeasuresBucketEmpty ? parseValue(resData[0]) : 0,
+                    y: !secondaryMeasuresBucketEmpty ? parseValue(resData[1 - emptyBucketsCount]) : 0,
+                    // we want to allow NaN on z to be able show bubble of default size when Size bucket is empty
+                    z: parseFloat(resData[2 - emptyBucketsCount]),
+                    format: unwrap(last(measureGroup.items)).format, // only for dataLabel format
+                },
+            ];
             return {
                 name: stackByAttribute ? stackByAttribute.items[index].attributeHeaderItem.name : "",
-                color: colorStrategy.getColorByIndex(index),
-                legendIndex: index,
+                color: colorStrategy.getColorByIndex(legendIndex),
+                legendIndex: legendIndex++,
                 data,
             };
-        });
+        })
+        .filter((serie) => serie !== null);
 }
