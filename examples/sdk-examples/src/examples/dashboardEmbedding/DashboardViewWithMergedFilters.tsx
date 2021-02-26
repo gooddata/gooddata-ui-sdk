782e3c00e2d110e052b1b3e1be30d2aaccca3cfa
Dan Homola <dan.homola@gooddata.com>
2021-02-26 11:22:20 +0100

    Link DashboardView examples together
    Add links between the different examples where it makes sense
    
    JIRA: RAIL-2960

--- examples/sdk-examples/src/examples/dashboardEmbedding/DashboardViewWithMergedFilters.tsx
+++ examples/sdk-examples/src/examples/dashboardEmbedding/DashboardViewWithMergedFilters.tsx
@@ -1,6 +1,7 @@
 // (C) 2007-2019 GoodData Corporation
 /* eslint-disable import/no-unresolved,import/default */
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { ExampleWithSource } from "../../components/ExampleWithSource";
 
@@ -14,9 +15,10 @@ const DashboardView = (): JSX.Element => (
 
         <p>
             Example of how to embed a Dashboard into your application with custom filters combined with the
-            filters already specified on the dashboard itself – the same Dashboard as in the first example
-            with added filter only to California (this also respects the filters set on the Dashboard itself,
-            if you do not want that, see the previous example).
+            filters already specified on the dashboard itself – the same Dashboard as in the{" "}
+            <Link to="/dashboardView/simple">Simple example</Link> with added filter only to California (this
+            also respects the filters set on the Dashboard itself, if you do not want that, see the{" "}
+            <Link to="/dashboardView/with-filters">{'"With filters"'} example</Link>).
         </p>
 
         <ExampleWithSource
