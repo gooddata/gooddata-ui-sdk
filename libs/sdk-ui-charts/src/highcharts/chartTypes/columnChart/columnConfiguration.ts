fe66fd544bc1db6497fcba5f786dc23fc2a9f7da
scavnickyj <jakub.scavnicky@gooddata.com>
2021-03-09 14:36:13 +0100

    Fix column chart label outline
    JIRA: BB-2836

--- libs/sdk-ui-charts/src/highcharts/chartTypes/columnChart/columnConfiguration.ts
+++ libs/sdk-ui-charts/src/highcharts/chartTypes/columnChart/columnConfiguration.ts
@@ -1,39 +1,49 @@
-// (C) 2007-2020 GoodData Corporation
-import cloneDeep from "lodash/cloneDeep";
+// (C) 2007-2021 GoodData Corporation
+import { IExecutionDefinition } from "@gooddata/sdk-model";
+import { IChartOptions } from "../../typings/unsafe";
+import { ITheme } from "@gooddata/sdk-backend-spi";
 
 import { MAX_POINT_WIDTH } from "../_chartCreators/commonConfiguration";
 
-const COLUMN_TEMPLATE = {
-    chart: {
-        type: "column",
-        spacingTop: 20,
-    },
-    plotOptions: {
-        column: {
-            dataLabels: {
-                enabled: true,
-                padding: 2,
-            },
-            maxPointWidth: MAX_POINT_WIDTH,
+export function getColumnConfiguration(
+    _config: IChartOptions,
+    _definition: IExecutionDefinition,
+    theme: ITheme,
+): any {
+    return {
+        chart: {
+            type: "column",
+            spacingTop: 20,
         },
-        series: {
-            states: {
-                hover: {
-                    enabled: false,
+        plotOptions: {
+            column: {
+                dataLabels: {
+                    enabled: true,
+                    padding: 2,
+                },
+                maxPointWidth: MAX_POINT_WIDTH,
+            },
+            series: {
+                states: {
+                    hover: {
+                        enabled: false,
+                    },
                 },
             },
         },
-    },
-    yAxis: [
-        {
-            stackLabels: {
-                enabled: true,
-                allowOverlap: false,
+        yAxis: [
+            {
+                stackLabels: {
+                    enabled: true,
+                    allowOverlap: false,
+                    ...(theme?.palette?.complementary && {
+                        style: {
+                            color: theme?.palette?.complementary?.shade9,
+                            textOutline: "none",
+                        },
+                    }),
+                },
             },
-        },
-    ],
-};
-
-export function getColumnConfiguration(): typeof COLUMN_TEMPLATE {
-    return cloneDeep(COLUMN_TEMPLATE);
+        ],
+    };
 }
