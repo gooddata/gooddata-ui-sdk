9f0f6fa1caa855375e7fa4f4e45669e50a5c8d7b
Milan Lempera <milanlempera@gmail.com>
2021-04-30 08:37:40 +0200

    RAIL-3120 - measure editing API  - add TSDoc

--- libs/sdk-backend-spi/src/workspace/measures/index.ts
+++ libs/sdk-backend-spi/src/workspace/measures/index.ts
@@ -5,7 +5,7 @@ import { IMeasureMetadataObject } from "../fromModel/ldm/metadata";
 import { IMeasureMetadataObjectDefinition } from "../fromModel/ldm/metadata";
 
 /**
- * Service for querying additional measures data.
+ * Service for create, update or delete measures and querying additional measures data.
  * If you want to query measures themselves, use catalog {@link IWorkspaceCatalogFactory}
  *
  * @public
@@ -18,9 +18,27 @@ export interface IWorkspaceMeasuresService {
      */
     getMeasureExpressionTokens(ref: ObjRef): Promise<IMeasureExpressionToken[]>;
 
+    /**
+     * Create and save measure for the provided measure definition
+     *
+     * @param measure
+     * @returns promise of created measure
+     */
     createMeasure(measure: IMeasureMetadataObjectDefinition): Promise<IMeasureMetadataObject>;
 
+    /**
+     * Update provided measure
+     *
+     * @param measure - measure to update
+     * @returns promise of updated measure
+     */
     updateMeasure(measure: IMeasureMetadataObject): Promise<IMeasureMetadataObject>;
 
+    /**
+     * Delete measure with the given reference
+     *
+     * @param measureRef - ref of the measure to delete
+     * @returns promise of undefined
+     */
     deleteMeasure(measureRef: ObjRef): Promise<void>;
 }
