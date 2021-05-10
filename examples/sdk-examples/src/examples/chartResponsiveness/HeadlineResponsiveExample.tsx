06a09d47a8666d13b3b6127502ad258afce33916
Petr Benes <petr.benes@gooddata.com>
2021-05-10 14:44:09 +0200

    Add resonsive legend examples
    - Add examples for responsive legend with pre-defined and custom
      (draggable) size
    - Unify bar chart and headline chart responsive examples with responsive
      legend examples.
    
    JIRA: ONE-5027

--- examples/sdk-examples/src/examples/chartResponsiveness/HeadlineResponsiveExample.tsx
+++ examples/sdk-examples/src/examples/chartResponsiveness/HeadlineResponsiveExample.tsx
@@ -1,5 +1,5 @@
 // (C) 2007-2019 GoodData Corporation
-import React from "react";
+import React, { useState } from "react";
 import { Headline } from "@gooddata/sdk-ui-charts";
 import { LdmExt } from "../../ldm";
 
@@ -7,37 +7,68 @@ const config = {
     enableCompactSize: true,
 };
 
+interface SizeButtonProps {
+    callback: (size: [number, number]) => void;
+    height: number;
+    width: number;
+    currentHeight: number | null;
+    currentWidth: number | null;
+}
+
+const SizeButton: React.FC<SizeButtonProps> = (props) => {
+    const { callback, width, height, currentWidth, currentHeight } = props;
+    return (
+        <button
+            onClick={() => callback([width, height])}
+            className={`gd-button gd-button-secondary ${
+                width === currentWidth && height === currentHeight ? "is-active" : ""
+            }`}
+        >
+            {width}x{height}
+        </button>
+    );
+};
+
 export const HeadlineResponsiveExample: React.FC = () => {
     const primaryMeasure = LdmExt.FranchiseFees;
     const secondaryMeasure = LdmExt.FranchiseFeesAdRoyalty;
 
+    const [[width, height], setSize] = useState<[number, number]>([150, 120]);
+    const resize = (size: [number, number]) => {
+        setSize(size);
+    };
+
+    const divStyle: React.CSSProperties = {
+        width,
+        height,
+    };
+
     return (
         <>
-            <div className="s-headline" style={{ display: "flex" }}>
-                <style jsx>
-                    {`
-                        .column {
-                            flex: "1 1 50%";
-                        }
-                    `}
-                </style>
-                <div style={{ height: 120, width: 150 }} className="column">
-                    <Headline
-                        primaryMeasure={primaryMeasure}
-                        secondaryMeasure={secondaryMeasure}
-                        config={config}
-                    />
-                </div>
-
-                <div style={{ height: 220, width: 150, marginLeft: "30px" }} className="column">
-                    <Headline
-                        primaryMeasure={primaryMeasure}
-                        secondaryMeasure={secondaryMeasure}
-                        config={config}
-                    />
-                </div>
-
-                <div style={{ height: 220, width: 180, marginLeft: "30px" }} className="column">
+            <div className="s-headline">
+                <SizeButton
+                    callback={resize}
+                    width={150}
+                    height={120}
+                    currentWidth={width}
+                    currentHeight={height}
+                />
+                <SizeButton
+                    callback={resize}
+                    width={150}
+                    height={220}
+                    currentWidth={width}
+                    currentHeight={height}
+                />
+                <SizeButton
+                    callback={resize}
+                    width={180}
+                    height={220}
+                    currentWidth={width}
+                    currentHeight={height}
+                />
+                <hr className="separator" />
+                <div style={divStyle}>
                     <Headline
                         primaryMeasure={primaryMeasure}
                         secondaryMeasure={secondaryMeasure}
