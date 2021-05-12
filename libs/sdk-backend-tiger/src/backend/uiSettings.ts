4869fc601f1a0ce13d852cefd94ef383041d30c1
David Kubecka <david.kubecka@gooddata.com>
2021-05-12 16:47:29 +0200

    Add support for approx. count
    The functionality is available only for Tiger. Bear
    has enableApproxCount hardcoded to false.
    
    JIRA: NAS-110

--- libs/sdk-backend-tiger/src/backend/uiSettings.ts
+++ libs/sdk-backend-tiger/src/backend/uiSettings.ts
@@ -39,6 +39,7 @@ export const DefaultUiSettings: ISettings = {
     enableSectionHeaders: true,
     enableKPIDashboardSaveAsNew: true,
     enableEmbedButtonInKD: true,
+    enableApproxCount: true,
 
     // pivot table specific
     enableTableColumnsManualResizing: true,
