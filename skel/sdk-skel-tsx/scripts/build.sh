e5ae58918748b4eddd1f12e988ed8879bf63bc12
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-05-23 15:04:52 +0200

    Fix double execution of api-extractor
    -  Started from both build script and package json script
    
    JIRA: RAIL-3383

--- skel/sdk-skel-tsx/scripts/build.sh
+++ skel/sdk-skel-tsx/scripts/build.sh
@@ -15,7 +15,7 @@ _common-build() {
 
 build() {
     _common-build
-    concurrently "npm run build-cjs" "npm run build-esm" && npm run api-extractor
+    concurrently "npm run build-cjs" "npm run build-esm"
 }
 
 build-dev() {
