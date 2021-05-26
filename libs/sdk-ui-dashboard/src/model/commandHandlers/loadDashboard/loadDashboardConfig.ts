059661e1b8eb069ca1210ac88c02258ff1b5fd69
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-05-26 16:37:00 +0200

    Improve the config loading logic
    -  Previously could lead to incomplete data in state
    -  Now thoroughly checks whether all the config props are already resolved
    -  If not, it loads what it needs from the backend and fills the gaps
    
    JIRA: RAIL-3383

--- libs/sdk-ui-dashboard/src/model/commandHandlers/loadDashboard/loadDashboardConfig.ts
+++ libs/sdk-ui-dashboard/src/model/commandHandlers/loadDashboard/loadDashboardConfig.ts
@@ -1,6 +1,16 @@
 // (C) 2021 GoodData Corporation
-import { DashboardContext, ResolvedDashboardConfig } from "../../types/commonTypes";
-import { IDateFilterConfigsQueryResult, IUserWorkspaceSettings } from "@gooddata/sdk-backend-spi";
+import {
+    DashboardConfig,
+    DashboardContext,
+    isResolvedConfig,
+    ResolvedDashboardConfig,
+} from "../../types/commonTypes";
+import {
+    IDateFilterConfigsQueryResult,
+    ISeparators,
+    ISettings,
+    IUserWorkspaceSettings,
+} from "@gooddata/sdk-backend-spi";
 import { LoadDashboard } from "../../commands/dashboard";
 import { all, call } from "redux-saga/effects";
 import { dateFilterValidationFailed } from "../../events/dashboard";
@@ -38,6 +48,56 @@ function loadColorPalette(ctx: DashboardContext): Promise<IColorPalette> {
     return backend.workspace(workspace).styling().getColorPalette();
 }
 
+function* resolveDateFilterConfig(ctx: DashboardContext, config: DashboardConfig, cmd: LoadDashboard) {
+    if (config.dateFilterConfig !== undefined) {
+        return config.dateFilterConfig;
+    }
+
+    const result: PromiseFnReturnType<typeof loadDateFilterConfig> = yield call(loadDateFilterConfig, ctx);
+
+    if ((result?.totalCount ?? 0) > 1) {
+        yield call(eventDispatcher, dateFilterValidationFailed(ctx, "TOO_MANY_CONFIGS", cmd.correlationId));
+    }
+
+    const firstConfig = result?.items[0];
+
+    if (!firstConfig) {
+        yield call(eventDispatcher, dateFilterValidationFailed(ctx, "NO_CONFIG", cmd.correlationId));
+    }
+
+    return result?.items[0] ?? defaultDateFilterConfig;
+}
+
+type UserSettings = {
+    locale: ILocale;
+    separators: ISeparators;
+    settings: ISettings;
+};
+
+function resolveUserSettings(ctx: DashboardContext, config: DashboardConfig): Promise<UserSettings> {
+    if (config.settings && config.locale && config.separators) {
+        return Promise.resolve({
+            locale: config.locale,
+            separators: config.separators,
+            settings: config.settings,
+        });
+    }
+
+    return loadSettingsForCurrentUser(ctx).then((res) => ({
+        locale: config.locale ?? (res.locale as ILocale),
+        separators: config.separators ?? res.separators,
+        settings: config.settings ?? stripUserAndWorkspaceProps(res),
+    }));
+}
+
+function resolveColorPalette(ctx: DashboardContext, config: DashboardConfig): Promise<IColorPalette> {
+    if (config.colorPalette) {
+        return Promise.resolve(config.colorPalette);
+    }
+
+    return loadColorPalette(ctx);
+}
+
 /**
  * Loads all essential dashboard configuration from the backend if needed. The load command may specify their
  * own inline config - if that is the case the config is bounced back immediately. Otherwise the necessary
@@ -48,37 +108,35 @@ function loadColorPalette(ctx: DashboardContext): Promise<IColorPalette> {
  */
 export function* loadDashboardConfig(ctx: DashboardContext, cmd: LoadDashboard) {
     const {
-        payload: { config },
+        payload: { config = {} },
     } = cmd;
 
-    if (config) {
+    if (isResolvedConfig(config)) {
+        /*
+         * Config coming in props is fully specified. There is nothing to do. Bail out imediately.
+         */
         return config;
     }
 
-    const [dateFilterConfigResult, settings, colorPalette]: [
-        PromiseFnReturnType<typeof loadDateFilterConfig>,
-        PromiseFnReturnType<typeof loadSettingsForCurrentUser>,
-        PromiseFnReturnType<typeof loadColorPalette>,
+    /*
+     * Resolve the config values. The resolve* functions will take value from config if it is defined,
+     * otherwise they will obtain the config from backend.
+     *
+     * Note: the user settings include locale, separators and the ISettings that should be in effect
+     * for the current user in the context of the workspace.
+     */
+
+    const [dateFilterConfig, settings, colorPalette]: [
+        PromiseFnReturnType<typeof resolveDateFilterConfig>,
+        PromiseFnReturnType<typeof resolveUserSettings>,
+        PromiseFnReturnType<typeof resolveColorPalette>,
     ] = yield all([
-        call(loadDateFilterConfig, ctx),
-        call(loadSettingsForCurrentUser, ctx),
-        call(loadColorPalette, ctx),
+        call(resolveDateFilterConfig, ctx, config, cmd),
+        call(resolveUserSettings, ctx, config),
+        call(resolveColorPalette, ctx, config),
     ]);
 
-    if ((dateFilterConfigResult?.totalCount ?? 0) > 1) {
-        yield call(eventDispatcher, dateFilterValidationFailed(ctx, "TOO_MANY_CONFIGS", cmd.correlationId));
-    }
-
-    const firstConfig = dateFilterConfigResult?.items[0];
-
-    if (!firstConfig) {
-        yield call(eventDispatcher, dateFilterValidationFailed(ctx, "NO_CONFIG", cmd.correlationId));
-    }
-
-    const [dateFilterConfig, configValidation] = getValidDateFilterConfig(
-        firstConfig ?? defaultDateFilterConfig,
-        settings,
-    );
+    const [validDateFilterConfig, configValidation] = getValidDateFilterConfig(dateFilterConfig, settings);
 
     if (configValidation !== "Valid") {
         yield call(eventDispatcher, dateFilterValidationFailed(ctx, configValidation, cmd.correlationId));
@@ -87,8 +145,8 @@ export function* loadDashboardConfig(ctx: DashboardContext, cmd: LoadDashboard) 
     return {
         locale: settings.locale as ILocale,
         separators: settings.separators,
-        dateFilterConfig,
-        settings: stripUserAndWorkspaceProps(settings),
+        dateFilterConfig: validDateFilterConfig,
+        settings: settings.settings,
         colorPalette,
     } as ResolvedDashboardConfig;
 }
