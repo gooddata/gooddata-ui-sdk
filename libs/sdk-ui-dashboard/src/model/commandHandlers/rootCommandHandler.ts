84eb0cc92a193ba807a231b6146e0d868ca53ea1
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-05-30 22:57:25 +0200

    Refactor root command handler
    -  Use command to saga lookup table
    -  Add command rejection for unhandled commands
    -  Add error boundary
    
    JIRA: RAIL-3383

--- libs/sdk-ui-dashboard/src/model/commandHandlers/rootCommandHandler.ts
+++ libs/sdk-ui-dashboard/src/model/commandHandlers/rootCommandHandler.ts
@@ -2,7 +2,35 @@
 import { actionChannel, call, getContext, take } from "redux-saga/effects";
 import { loadDashboardCommandHandler } from "./loadDashboard/handler";
 import { DashboardContext } from "../types/commonTypes";
-import { DashboardCommands } from "../commands";
+import { DashboardCommands, IDashboardCommand } from "../commands";
+import { eventDispatcher } from "../eventEmitter/eventDispatcher";
+import { commandRejected, internalErrorOccurred } from "../events/general";
+
+const DefaultCommandHandlers = {
+    "GDC.DASHBOARD.CMD.LOAD": loadDashboardCommandHandler,
+    "GDC.DASHBOARD.CMD.SAVE": unhandledCommand,
+    "GDC.DASHBOARD.CMD.SAVEAS": unhandledCommand,
+    "GDC.DASHBOARD.CMD.RESET": unhandledCommand,
+    "GDC.DASHBOARD.CMD.RENAME": unhandledCommand,
+    "GDC.DASHBOARD.CMD.DF.CHANGE_SELECTION": unhandledCommand,
+    "GDC.DASHBOARD.CMD.AF.ADD": unhandledCommand,
+    "GDC.DASHBOARD.CMD.AF.REMOVE": unhandledCommand,
+    "GDC.DASHBOARD.CMD.AF.MOVE": unhandledCommand,
+    "GDC.DASHBOARD.CMD.AF.CHANGE_SELECTION": unhandledCommand,
+    "GDC.DASHBOARD.CMD.AF.SET_PARENT": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.ADD_SECTION": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.MOVE_SECTION": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.REMOVE_SECTION": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.CHANGE_SECTION_HEADER": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.ADD_ITEMS": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.MOVE_ITEM": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.REMOVE_ITEM": unhandledCommand,
+    "GDC.DASHBOARD.CMD.FL.UNDO": unhandledCommand,
+};
+
+function* unhandledCommand(ctx: DashboardContext, cmd: IDashboardCommand) {
+    yield call(eventDispatcher, commandRejected(ctx, cmd.correlationId));
+}
 
 /**
  * Root command handler is the central point through which all command processing is done. The handler registers
@@ -20,46 +48,23 @@ export function* rootCommandHandler() {
     while (true) {
         const action: DashboardCommands = yield take(commandChannel);
         const dashboardContext: DashboardContext = yield getContext("dashboardContext");
+        const commandHandler = DefaultCommandHandlers[action.type] ?? unhandledCommand;
 
-        switch (action.type) {
-            case "GDC.DASHBOARD.CMD.LOAD": {
-                yield call(loadDashboardCommandHandler, dashboardContext, action);
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.RENAME": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.RESET": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.SAVE": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.SAVEAS": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.DF.CHANGE_SELECTION": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.AF.ADD": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.AF.REMOVE": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.AF.MOVE": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.AF.CHANGE_SELECTION": {
-                break;
-            }
-            case "GDC.DASHBOARD.CMD.AF.SET_PARENT": {
-                break;
-            }
-            default: {
-                // TODO: emit unhandled command event
-                break;
-            }
+        try {
+            yield call(commandHandler, dashboardContext, action);
+        } catch (e) {
+            // Errors during command handling should be caught and addressed in the handler, possibly with a
+            // more meaningful error message. If the error bubbles up to here then there are holes in error
+            // handling or something is seriously messed up.
+            yield call(
+                eventDispatcher,
+                internalErrorOccurred(
+                    dashboardContext,
+                    `Internal error has occurred while handling ${action.type}`,
+                    e,
+                    action.correlationId,
+                ),
+            );
         }
     }
 }
