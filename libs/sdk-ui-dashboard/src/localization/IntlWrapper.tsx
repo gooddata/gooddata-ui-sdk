98d7ec336188509d6d83be316941131eda59a2d1
Dan Homola <dan.homola@gooddata.com>
2021-06-29 12:10:36 +0200

    Simplify translations in sdk-ui-dashboard
    Import translations from ext and use them everywhere.
    
    JIRA: RAIL-3383

--- libs/sdk-ui-dashboard/src/localization/IntlWrapper.tsx
+++ libs/sdk-ui-dashboard/src/localization/IntlWrapper.tsx
@@ -1,7 +1,9 @@
 // (C) 2007-2019 GoodData Corporation
 import React from "react";
 import { IntlProvider } from "react-intl";
+import merge from "lodash/merge";
 import { DefaultLocale } from "@gooddata/sdk-ui";
+import { translations as sdkUiExtTranslations } from "@gooddata/sdk-ui-ext/dist/internal";
 import { translationUtils } from "@gooddata/util";
 
 import enUS from "./bundles/en-US.json";
@@ -14,17 +16,7 @@ import ptBR from "./bundles/pt-BR.json";
 import ptPT from "./bundles/pt-PT.json";
 import zhHans from "./bundles/zh-Hans.json";
 
-/**
- * @internal
- */
-export interface ITranslations {
-    [key: string]: string;
-}
-
-/**
- * @internal
- */
-export const messagesMap: { [locale: string]: ITranslations } = {
+const sdkUiDashboardTranslations: { [locale: string]: Record<string, string> } = {
     "en-US": translationUtils.removeMetadata(enUS),
     "de-DE": deDE,
     "es-ES": esES,
@@ -35,6 +27,16 @@ export const messagesMap: { [locale: string]: ITranslations } = {
     "pt-PT": ptPT,
     "zh-Hans": zhHans,
 };
+
+/**
+ * @internal
+ */
+export const translations = merge(
+    {},
+    // we also need the sdk-ui-ext translations
+    sdkUiExtTranslations,
+    sdkUiDashboardTranslations,
+);
 
 /**
  * @internal
@@ -54,7 +56,7 @@ export class IntlWrapper extends React.PureComponent<IIntlWrapperProps> {
     public render(): React.ReactNode {
         const { locale } = this.props;
         return (
-            <IntlProvider locale={locale} messages={messagesMap[locale]}>
+            <IntlProvider locale={locale} messages={translations[locale]}>
                 {this.props.children}
             </IntlProvider>
         );
