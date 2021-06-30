252dbff2a23bab16d9ff39ff6521c61c76285309
Dan Homola <dan.homola@gooddata.com>
2021-06-30 14:11:49 +0200

    Expose InsightError and InsightRenderer and use them
    JIRA: RAIL-3383

--- libs/sdk-ui-ext/src/insightView/index.ts
+++ libs/sdk-ui-ext/src/insightView/index.ts
@@ -3,3 +3,7 @@
 export { InsightView } from "./InsightView";
 export { IInsightViewProps } from "./types";
 export { IInsightTitleProps } from "./InsightTitle";
+
+// exports only for internal use by other gooddata packages
+export { IInsightErrorProps, InsightError } from "./InsightError";
+export { IInsightRendererProps, InsightRenderer } from "./InsightRenderer";
