cc15cc9d461e170c524d80da4653a91913c343da
Dan Homola <dan.homola@gooddata.com>
2021-07-09 17:00:54 +0200

    Adopt sonar-eslint in dashboard-plugin-template
    JIRA: FET-781

--- tools/dashboard-plugin-template/src/harness/PluginLoader.tsx
+++ tools/dashboard-plugin-template/src/harness/PluginLoader.tsx
@@ -12,8 +12,7 @@ function loadComponent(scope: string, module: any) {
         // Initialize the container, it may provide shared modules
         await container.init(__webpack_share_scopes__.default);
         const factory = await window[scope].get(module);
-        const Module = factory();
-        return Module;
+        return factory();
     };
 }
 
