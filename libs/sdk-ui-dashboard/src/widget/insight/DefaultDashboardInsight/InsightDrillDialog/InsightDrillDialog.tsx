4f153d54d7d6a7e5e10c18cd4dd4d1bdebf05955
Matyas Kandl <matyas.kandl@gooddata.com>
2021-07-14 10:55:12 +0200

    Fix InsightRenderer insight memoization
    - Update insight on filters change (now, when the insight filters change, they are not updated)
    
    JIRA: RAIL-3533

--- libs/sdk-ui-dashboard/src/widget/insight/DefaultDashboardInsight/InsightDrillDialog/InsightDrillDialog.tsx
+++ libs/sdk-ui-dashboard/src/widget/insight/DefaultDashboardInsight/InsightDrillDialog/InsightDrillDialog.tsx
@@ -1,6 +1,5 @@
 // (C) 2020 GoodData Corporation
 import React from "react";
-import stableStringify from "json-stable-stringify";
 import { IInsightWidget } from "@gooddata/sdk-backend-spi";
 import { IInsight, insightTitle } from "@gooddata/sdk-model";
 import { FullScreenOverlay, Overlay, OverlayPositionType, useMediaQuery } from "@gooddata/sdk-ui-kit";
@@ -81,8 +80,6 @@ export const InsightDrillDialog = (props: InsightDrillDialogProps): JSX.Element 
                     breadcrumbs={breadcrumbs}
                 >
                     <DefaultDashboardInsightWithDrillSelect
-                        // TODO: RAIL-3533 - Fix & remove or elaborate why it's necessary
-                        key={stableStringify(insight)}
                         insight={insight}
                         widget={widget}
                         disableWidgetImplicitDrills
