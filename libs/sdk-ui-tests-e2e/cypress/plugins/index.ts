4fb96ef8227f4fc65eaac6d79d8e58f9940013ad
Martin Plavek <martin.plavek@gooddata.com>
2021-07-14 12:27:58 +0200

    RAIL-3539 Cypress E2E tests infrastructure
    - fix failing build
    
    JIRA: RAIL-3539

--- libs/sdk-ui-tests-e2e/cypress/plugins/index.ts
+++ libs/sdk-ui-tests-e2e/cypress/plugins/index.ts
@@ -19,7 +19,7 @@ import logPlugin from "./log";
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+module.exports = (on: any, config: any) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
     logPlugin(on, config);
