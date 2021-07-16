080426929c4b0deec5d85a98de3c3c92e194e9cb
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-07-16 16:12:02 +0200

    Add command to run tests incrementally in PR
    -  Finds what packages have changed
    -  Concurrently starts `test` for all of them
    
    JIRA: RAIL-3492

---
+++ common/scripts/test-pr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APPLINK="tools/applink/dist/index.js"
+PKGS=`git diff --name-only master...HEAD | node ${APPLINK} findImpacted`
+
+echo ${PKGS}
+
+for pkg in ${PKGS}; do {
+  echo "Starting tests in: ${pkg}"
+
+  { cd ${pkg}; npm run test-ci; } & pid=$!
+  PID_LIST+=" $pid";
+} done
+
+trap "kill $PID_LIST" SIGINT
+wait $PID_LIST
