1449bfc8785eb40d1ab5b032f507ae8d67b09741
Dan Homola <dan.homola@gooddata.com>
2021-07-21 16:05:22 +0200

    Fix Insight Widget Filter query
    Make the date filters resolution the same as before
    
    JIRA: RAIL-3383

--- libs/sdk-ui-dashboard/src/model/queryServices/queryInsightWidgetFilters.ts
+++ libs/sdk-ui-dashboard/src/model/queryServices/queryInsightWidgetFilters.ts
@@ -11,11 +11,15 @@ import {
     IFilter,
     IInsight,
     insightFilters,
+    insightMeasures,
     isAllTimeDateFilter,
     isAttributeFilter,
     isDateFilter,
     isMeasureValueFilter,
     isRankingFilter,
+    isSimpleMeasure,
+    measureFilters,
+    newAllTimeFilter,
     ObjectType,
     ObjRef,
     objRefToString,
@@ -208,15 +212,59 @@ async function getResolvedInsightNonDateFilters(
     return [...resolvedAttributeFilters, ...resolvedMeasureValueFilters, ...resolvedRankingFilters];
 }
 
+function hasDateFilterForDateDataset(filters: IFilter[], dateDataset: ObjRef): boolean {
+    return filters.some((filter) => {
+        if (!isDateFilter(filter)) {
+            return false;
+        }
+
+        return areObjRefsEqual(filterObjRef(filter), dateDataset);
+    });
+}
+
+function addImplicitAllTimeFilter(widget: IWidget, resolvedFilters: IDateFilter[]): IDateFilter[] {
+    // if the widget is connected to a dateDataset and has no date filters for it in the context,
+    // add an implicit All time filter for that dimension - this will cause the InsightView to ignore
+    // any date filters on that dimension - this is how KPI dashboards behave
+    if (widget.dateDataSet && !hasDateFilterForDateDataset(resolvedFilters, widget.dateDataSet)) {
+        return [...resolvedFilters, newAllTimeFilter(widget.dateDataSet)];
+    }
+    return resolvedFilters;
+}
+
+/**
+ * Tests whether dashboard's date filter should not be applied on the insight included in the provided widget.
+ *
+ * This should happen for insights whose simple measures are all already set up with date filters. I guess ignoring
+ * global date filter is desired because otherwise there is a large chance that the intersection of global date filter
+ * and measure's date filters would lead to empty set and no data shown for the insight?
+ */
+export function isDateFilterIgnoredForInsight(insight: IInsight): boolean {
+    const simpleMeasures = insightMeasures(insight, isSimpleMeasure);
+
+    if (simpleMeasures.length === 0) {
+        return false;
+    }
+
+    const simpleMeasuresWithDateFilter = simpleMeasures.filter((m) =>
+        (measureFilters(m) ?? []).some(isDateFilter),
+    );
+
+    return simpleMeasures.length === simpleMeasuresWithDateFilter.length;
+}
+
 async function getResolvedInsightDateFilters(
     ctx: DashboardContext,
-    _widget: IWidget,
-    _insight: IInsight,
+    widget: IWidget,
+    insight: IInsight,
     dashboardDateFilters: IDateFilter[],
     insightDateFilters: IDateFilter[],
 ): Promise<IDateFilter[]> {
-    // TODO implicit all time filter
-    const allDateFilters = [...insightDateFilters, ...dashboardDateFilters];
+    if (isDateFilterIgnoredForInsight(insight)) {
+        return insightDateFilters;
+    }
+
+    const allDateFilters = addImplicitAllTimeFilter(widget, [...insightDateFilters, ...dashboardDateFilters]);
     const allDateFilterDateDatasetPairs = await loadDateDatasetsForDateFilters(ctx, allDateFilters);
 
     // go through the filters in reverse order using the first filter for a given dimension encountered
