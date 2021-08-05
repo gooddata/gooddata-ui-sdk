24eecf792c3a53013c5dc12cc848b968a84b890d
Martin Plavek <martin.plavek@gooddata.com>
2021-08-05 13:42:32 +0200

    RAIL-3594 add scripts for record creation
    - resolve CR issues
    - script for record creation
    
    JIRA: RAIL-3594

--- libs/sdk-ui-tests-e2e/README.md
+++ libs/sdk-ui-tests-e2e/README.md
@@ -18,5 +18,11 @@ Tests in this library verify various go-through scenarios for GoodData.UI compon
 -   In folder `cypres/integration` you can either create new folder for the tests you're writing. You can then add new `*.spec.ts` file into either this new folder or to suitable existing one.
 -   In the `scenarios/src/components/Scenarios`, create new Scenario file and within the new file use the component you are about to test.
 -   In the `scenarios/src/routes/ComponentResolver`, add the `ScenarioComponent` you just created with a new unique hash. Navigation component takes this hash as its parameter and navigates the application to matching component.
+-   Run command `npm run run-isolated-record` to create new recordings for tests.
 
 ### Test results
+
+-   If tests fail, there are 3 ways how to check what happened:
+    -   screenshots in `cypress/screenshots`
+    -   videos in `cypress/videos`
+    -   if you are running tests in record mode, then you can see all calls logged in `recording/mappings`
--- libs/sdk-ui-tests-e2e/scenarios/webpack.config.js
+++ libs/sdk-ui-tests-e2e/scenarios/webpack.config.js
@@ -12,28 +12,12 @@ const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const playgroundDefinitions = {
-    sec: ["https://secure.gooddata.com", "k26dtejorcqlqf11crn6imbeevp2q4kg"],
-    secure: ["https://secure.gooddata.com", "k26dtejorcqlqf11crn6imbeevp2q4kg"],
-    stg: ["https://staging.intgdc.com", "na1q8a0q4efb7cajbgre9mmm776dr1yv"],
-    stg2: ["https://staging2.intgdc.com", "ws7pxsamkx8o0t1s7kfvkj5o41uwcmqg"],
-    stg3: ["https://staging3.intgdc.com", "mbuumy476p78ybcceiru61hcyr8i8lo8"],
-    developer: ["https://developer.na.gooddata.com", "xms7ga4tf3g3nzucd8380o2bev8oeknp"],
-    public: ["https://live-examples-proxy.herokuapp.com", "xms7ga4tf3g3nzucd8380o2bev8oeknp"],
-    reference: ["https://secure.gooddata.com", "l32xdyl4bjuzgf9kkqr2avl55gtuyjwf"],
-};
-
 module.exports = async (env, argv) => {
-    const backendParam = (env && env.backend) || "public";
-    const [backendUrl, workspace] = playgroundDefinitions[backendParam] || playgroundDefinitions.stg3;
+    const backendParam = "stg3";
+    const [backendUrl, workspace] = ["https://staging3.intgdc.com", "mbuumy476p78ybcceiru61hcyr8i8lo8"];
 
     // eslint-disable-next-line no-mixed-operators
     const basePath = (env && env.basePath) || "";
-
-    console.log(`BasePath: ${basePath}`);
-
-    // eslint-disable-next-line no-console
-    console.log("Backend URI:", backendUrl, "Workspace to use:", workspace);
 
     const isProduction = argv.mode === "production";
 
@@ -138,26 +122,6 @@ module.exports = async (env, argv) => {
                     test: /\.[jt]sx?$/,
                     include: path.resolve(__dirname, "src"),
                     use: ["babel-loader"],
-                },
-                {
-                    test: /\.js$/,
-                    include: (rawModulePath) => {
-                        // Some npm modules no longer transpiled to ES5, which
-                        // causes errors such in IE11.
-                        const inclusionReg =
-                            /node_modules\/.*((lru-cache)|(react-intl)|(intl-messageformat))/;
-                        // On Windows, mPath use backslashes for folder separators. We need
-                        // to convert these to forward slashes because our
-                        // test regex, inclusionReg, contains one.
-                        const modulePath = rawModulePath.replace(/\\/g, "/");
-                        return inclusionReg.test(modulePath);
-                    },
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                        },
-                    },
                 },
                 {
                     test: /\.(jpe?g|gif|png|svg|ico|eot|woff2?|ttf|wav|mp3)$/,
