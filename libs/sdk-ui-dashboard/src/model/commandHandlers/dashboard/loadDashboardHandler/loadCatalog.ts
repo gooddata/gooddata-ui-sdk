3b10ebfc427c2109fc4763b3ff9a6c2a11ccb3d1
ivanmjartan <ivan.mjartan@gooddata.com>
2021-08-25 15:05:43 +0200

    Change catalog load and add includeDateGranularities definitions
     - fix drill down form generic date attributes
    
    JIRA: RAIL-3640

--- libs/sdk-ui-dashboard/src/model/commandHandlers/dashboard/loadDashboardHandler/loadCatalog.ts
+++ libs/sdk-ui-dashboard/src/model/commandHandlers/dashboard/loadDashboardHandler/loadCatalog.ts
@@ -1,9 +1,29 @@
 // (C) 2021 GoodData Corporation
 import { DashboardContext } from "../../../types/commonTypes";
-import { IWorkspaceCatalog } from "@gooddata/sdk-backend-spi";
+import { IWorkspaceCatalog, IWorkspaceCatalogFactoryOptions } from "@gooddata/sdk-backend-spi";
+import { DateAttributeGranularity } from "@gooddata/sdk-model";
+
+const SupportedCatalogGranularity: DateAttributeGranularity[] = [
+    "GDC.time.day_in_week",
+    "GDC.time.day_in_month",
+    "GDC.time.day_in_quarter",
+    "GDC.time.day_in_year",
+    "GDC.time.week_in_quarter",
+    "GDC.time.week_in_year",
+    "GDC.time.month_in_quarter",
+    "GDC.time.month_in_year",
+    "GDC.time.quarter_in_year",
+];
 
 export function loadCatalog(ctx: DashboardContext): Promise<IWorkspaceCatalog> {
     const { backend, workspace } = ctx;
 
-    return backend.workspace(workspace).catalog().load();
+    const options: IWorkspaceCatalogFactoryOptions = {
+        excludeTags: [],
+        includeTags: [],
+        types: ["attribute", "fact", "measure", "dateDataset"],
+        includeDateGranularities: SupportedCatalogGranularity,
+    };
+
+    return backend.workspace(workspace).catalog().withOptions(options).load();
 }
