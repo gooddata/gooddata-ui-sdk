55e6d0467b48dfcf926145438b0d4e9d607baea6
Martin Plavek <martin.plavek@gooddata.com>
2021-10-11 13:54:43 +0200

    Introduce why-did-you-render into playground app
    - declare require variable to avoid type error
    - disable eslint for require statement
    - add moduleResolution to tsConfig
    - create new npm script to run playground with WDYR
    - Update Readme
    
    JIRA: FET-828

--- examples/playground/scripts/run-playground.sh
+++ examples/playground/scripts/run-playground.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+if test -z "$WDYR"
+then
+    WDYR="false"
+fi
 
 PARAMS=$@
 if [ ! -z "$1" ]; then # if first argument is not empty
-    PARAMS="--env backend=$1 ${@:2}"
+    PARAMS="--env backend=$1 ${@:2} wdyr=$WDYR"
 fi
 
+echo "why-did-you-render enabled: $WDYR"
 
 echo "$ webpack serve --https $PARAMS"
 webpack serve --hot --https $PARAMS
