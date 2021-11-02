f48bed5a04f864eb3d1fce710eee39811f7c8eea
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-11-02 23:21:38 +0100

    Refactor and sanitize input validations
    -  Added validators for all inputs
    -  Reshuffled code so that all input on CLI is validated first, then any necessary prompting is done
    -  Simplified how backend type is passed around (backend type passed as is instead of isBear indicator)
    
    JIRA: RAIL-3428

--- tools/plugin-toolkit/src/initCmd/index.ts
+++ tools/plugin-toolkit/src/initCmd/index.ts
@@ -1,6 +1,6 @@
 // (C) 2021 GoodData Corporation
 import { ActionOptions, TargetAppFlavor, TargetBackendType } from "../_base/types";
-import { logInfo } from "../_base/cli/loggers";
+import { logError, logInfo } from "../_base/cli/loggers";
 import { promptBackend, promptFlavor, promptHostname, promptName } from "../_base/cli/prompts";
 import kebabCase from "lodash/kebabCase";
 import * as path from "path";
@@ -8,6 +8,14 @@ import mkdirp from "mkdirp";
 import tar from "tar";
 import { getDashboardPluginTemplateArchive } from "../dashboard-plugin-template";
 import { readJsonSync, writeAsJsonSync } from "../_base/utils";
+import {
+    backendTypeValidator,
+    flavorValidator,
+    hostnameValidatorFactory,
+    isInputValidationError,
+    pluginNameValidator,
+    validOrDie,
+} from "../_base/cli/validators";
 
 type InitCmdActionConfig = {
     backend: TargetBackendType;
@@ -17,50 +25,81 @@ type InitCmdActionConfig = {
     skipInstall: boolean;
 };
 
-function getHostname(options: ActionOptions): string | undefined {
-    return options.programOpts.hostname;
+function getHostname(backend: TargetBackendType | undefined, options: ActionOptions): string | undefined {
+    const { hostname } = options.programOpts;
+
+    if (!hostname) {
+        return undefined;
+    }
+
+    // do the hostname validation only if the backend type is known at this point
+    if (backend !== undefined) {
+        validOrDie("hostname", hostname, hostnameValidatorFactory(backend));
+    }
+
+    return hostname;
 }
 
 function getBackend(options: ActionOptions): TargetBackendType | undefined {
     const { backend } = options.commandOpts;
 
-    if (backend === "bear" || backend === "tiger") {
-        return backend;
+    if (!backend) {
+        return undefined;
     }
 
-    return undefined;
+    validOrDie("backend", backend, backendTypeValidator);
+
+    return backend as TargetBackendType;
 }
 
 function getFlavor(options: ActionOptions): TargetAppFlavor | undefined {
     const { flavor } = options.commandOpts;
 
-    if (flavor === "ts" || flavor === "js") {
-        return flavor;
+    if (!flavor) {
+        return undefined;
     }
 
-    return undefined;
+    validOrDie("flavor", flavor, flavorValidator);
+
+    return flavor as TargetAppFlavor;
 }
 
-async function getEffectivePluginName(pluginName: string | undefined): Promise<string> {
+async function getPluginNameAndConfig(
+    pluginName: string | undefined,
+    options: ActionOptions,
+): Promise<{ name: string; config: InitCmdActionConfig }> {
+    if (pluginName) {
+        validOrDie("plugin-name", pluginName, pluginNameValidator);
+    }
+
+    const backendFromOptions = getBackend(options);
+    const hostnameFromOptions = getHostname(backendFromOptions, options);
+    const flavorFromOptions = getFlavor(options);
+    const backend = backendFromOptions ?? (await promptBackend());
+    const hostname = hostnameFromOptions ?? (await promptHostname(backend));
+    const flavor = flavorFromOptions ?? (await promptFlavor());
     const name = pluginName ?? (await promptName());
-    // TODO: add validation; don't want invalid characters that cannot be used for plugin/package name
 
-    return kebabCase(name);
-}
-
-async function getEffectiveConfig(options: ActionOptions): Promise<InitCmdActionConfig> {
-    const backend = getBackend(options) ?? (await promptBackend());
-    const hostname = getHostname(options) ?? (await promptHostname(backend === "bear"));
-    const flavor = getFlavor(options) ?? (await promptFlavor());
+    // validate hostname once again; this is to catch the case when hostname is provided as
+    // option but the backend is not and user is prompted for it. the user may select backend
+    // for which the protocol used in the hostname is not valid (http on bear)
+    validOrDie("hostname", hostname, hostnameValidatorFactory(backend));
 
     return {
-        backend,
-        hostname,
-        flavor,
-        targetDir: options.commandOpts.targetDir,
-        skipInstall: options.commandOpts.skipInstall ?? false,
+        name: kebabCase(name),
+        config: {
+            backend,
+            hostname,
+            flavor,
+            targetDir: options.commandOpts.targetDir,
+            skipInstall: options.commandOpts.skipInstall ?? false,
+        },
     };
 }
+
+//
+//
+//
 
 function unpackProject(target: string, flavor: TargetAppFlavor) {
     return mkdirp(target).then((_) => {
@@ -88,9 +127,15 @@ async function prepareProject(name: string, config: InitCmdActionConfig) {
 }
 
 export async function initCmdAction(pluginName: string | undefined, options: ActionOptions): Promise<void> {
-    const config = await getEffectiveConfig(options);
-    const name = await getEffectivePluginName(pluginName);
+    try {
+        const { name, config } = await getPluginNameAndConfig(pluginName, options);
 
-    logInfo(`initCmdAction ${name} ${JSON.stringify(config, null, 4)}`);
-    await prepareProject(name, config);
+        logInfo(`initCmdAction ${name} ${JSON.stringify(config, null, 4)}`);
+        await prepareProject(name, config);
+    } catch (e) {
+        if (isInputValidationError(e)) {
+            logError(e.message);
+            process.exit(1);
+        }
+    }
 }
