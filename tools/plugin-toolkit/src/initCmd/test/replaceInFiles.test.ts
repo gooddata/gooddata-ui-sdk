fa96f7e536363ac48d78d41dc64c194af621296b
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-11-03 11:15:07 +0100

    Port file replacement code from boilerplate
    -  Only mild changes stemming from typings
    -  Renamed some methods, made code more verbose
    
    JIRA: RAIL-3428

---
+++ tools/plugin-toolkit/src/initCmd/test/replaceInFiles.test.ts
@@ -0,0 +1,30 @@
+// (C) 2019-2021 GoodData Corporation
+import path from "path";
+import { replaceInFiles } from "../replaceInFiles";
+
+describe("replaceInFiles", () => {
+    describe("replaceInFiles", () => {
+        it("should replace values according to the spec", async () => {
+            const readFileMock = async () => "foo bar baz buz";
+            const writeFileMock = jest.fn();
+
+            const spec = {
+                directory: {
+                    "b.js": [
+                        { regex: /bar/g, value: "replaced", apply: true },
+                        { regex: /baz/g, value: "not replaced", apply: false },
+                        { regex: /buz/g, value: "buf" },
+                    ],
+                },
+            };
+
+            await replaceInFiles(".", spec, readFileMock as any, writeFileMock);
+
+            expect(writeFileMock).toHaveBeenLastCalledWith(
+                path.join(".", "directory", "b.js"),
+                "foo replaced baz buf",
+                { encoding: "utf8", flag: "w" },
+            );
+        });
+    });
+});
