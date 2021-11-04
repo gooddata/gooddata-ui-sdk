8058dfa6b0e4338809fc80541268cb93d3d49e32
Dan Homola <dan.homola@gooddata.com>
2021-11-04 17:46:57 +0100

    Optimize IntlWrapper
    JIRA: RAIL-3696

--- libs/sdk-ui-dashboard/src/presentation/localization/IntlWrapper.tsx
+++ libs/sdk-ui-dashboard/src/presentation/localization/IntlWrapper.tsx
@@ -1,7 +1,11 @@
 // (C) 2007-2019 GoodData Corporation
-import React from "react";
+import React, { useCallback } from "react";
 import { IntlProvider } from "react-intl";
-import { DefaultLocale, TranslationsCustomizationProvider } from "@gooddata/sdk-ui";
+import {
+    DefaultLocale,
+    ITranslationsCustomizationProviderProps,
+    TranslationsCustomizationProvider,
+} from "@gooddata/sdk-ui";
 
 import { translations } from "./translations";
 
@@ -9,28 +13,20 @@ import { translations } from "./translations";
  * @internal
  */
 export interface IIntlWrapperProps {
-    locale: string;
+    locale?: string;
 }
 
 /**
  * @internal
  */
-export class IntlWrapper extends React.PureComponent<IIntlWrapperProps> {
-    public static defaultProps: IIntlWrapperProps = {
-        locale: DefaultLocale,
-    };
-
-    public render(): React.ReactNode {
-        const { children, locale } = this.props;
-        return (
-            <TranslationsCustomizationProvider
-                translations={translations[locale]}
-                render={(translations) => (
-                    <IntlProvider key={locale} locale={locale} messages={translations}>
-                        {children}
-                    </IntlProvider>
-                )}
-            />
-        );
-    }
-}
+export const IntlWrapper: React.FC<IIntlWrapperProps> = ({ children, locale = DefaultLocale }) => {
+    const render = useCallback<ITranslationsCustomizationProviderProps["render"]>(
+        (modifiedTranslations) => (
+            <IntlProvider locale={locale} messages={modifiedTranslations}>
+                {children}
+            </IntlProvider>
+        ),
+        [locale, children],
+    );
+    return <TranslationsCustomizationProvider translations={translations[locale]} render={render} />;
+};
