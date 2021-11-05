ea550142c61e6c147e8f71d1372821739d440511
Dan Homola <dan.homola@gooddata.com>
2021-11-05 16:34:15 +0100

    Optimize DashboardItemHeadline
    Prevent surprisingly expensive Truncate re-renders
    
    JIRA: RAIL-3696

--- libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemHeadline.tsx
+++ libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemHeadline.tsx
@@ -1,5 +1,5 @@
 // (C) 2020 GoodData Corporation
-import React from "react";
+import React, { memo } from "react";
 import Truncate from "react-truncate";
 
 import { DashboardItemHeadlineContainer } from "./DashboardItemHeadlineContainer";
@@ -9,12 +9,14 @@ interface IDashboardItemHeadlineProps {
     clientHeight?: number;
 }
 
-export const DashboardItemHeadline: React.FC<IDashboardItemHeadlineProps> = ({ title, clientHeight }) => {
-    return (
-        <DashboardItemHeadlineContainer clientHeight={clientHeight}>
-            <Truncate lines={2} ellipsis={"..."} className="item-headline-inner s-headline">
-                {title}
-            </Truncate>
-        </DashboardItemHeadlineContainer>
-    );
-};
+export const DashboardItemHeadline: React.FC<IDashboardItemHeadlineProps> = memo(
+    function DashboardItemHeadline({ title, clientHeight }) {
+        return (
+            <DashboardItemHeadlineContainer clientHeight={clientHeight}>
+                <Truncate lines={2} ellipsis="..." className="item-headline-inner s-headline">
+                    {title}
+                </Truncate>
+            </DashboardItemHeadlineContainer>
+        );
+    },
+);
