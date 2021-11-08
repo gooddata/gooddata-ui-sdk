0b13e30f78de25d49faa3e84e9a2b4c96e6603eb
Dan Homola <dan.homola@gooddata.com>
2021-11-08 16:29:35 +0100

    Optimize/memoize pickCorrectInsightWording
    Make sure to return referentially equal results if possible.
    This alleviates pressure on the react-intl context and prevents
    quite a lot of unnecessary re-renders.
    
    JIRA: FET-855

--- libs/sdk-ui/src/base/localization/TranslationsCustomizationProvider/utils.ts
+++ libs/sdk-ui/src/base/localization/TranslationsCustomizationProvider/utils.ts
@@ -1,16 +1,13 @@
 // (C) 2007-2021 GoodData Corporation
+import memoizeOne from "memoize-one";
 import { IWorkspaceSettings } from "@gooddata/sdk-backend-spi";
 
 const getNewKey = (key: string, stringToRemove: string) => key.replace(stringToRemove, "");
 
-/**
- * @beta
- */
-export const pickCorrectInsightWording = (
+const pickCorrectInsightWordingInner = (
     translations: Record<string, string>,
-    settings?: IWorkspaceSettings,
+    isEnabledInsightToReport: boolean,
 ): Record<string, string> => {
-    const isEnabledInsightToReport = !!settings?.enableInsightToReport;
     const modifiedTranslations = {};
     Object.keys(translations).forEach((key) => {
         if (key.includes("|report") || key.includes("|insight")) {
@@ -22,6 +19,24 @@ export const pickCorrectInsightWording = (
         ...translations,
         ...modifiedTranslations,
     };
+};
+
+/**
+ * Even this simple translations-reference-based cache is very effective as most of the time the 'translations'
+ * objects come from some static constant.
+ */
+const memoizedPickCorrectInsightWordingInner = memoizeOne(pickCorrectInsightWordingInner);
+
+/**
+ * @beta
+ */
+export const pickCorrectInsightWording = (
+    translations: Record<string, string>,
+    settings?: IWorkspaceSettings,
+): Record<string, string> => {
+    const isEnabledInsightToReport = !!settings?.enableInsightToReport;
+
+    return memoizedPickCorrectInsightWordingInner(translations, isEnabledInsightToReport);
 };
 
 /**
