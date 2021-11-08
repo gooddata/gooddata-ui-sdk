db6761cbdb1a6fb4772c24b4d41b2219c5a8595c
Lubo Slivka <lubomir.slivka@gooddata.com>
2021-11-08 09:49:55 +0100

    Move refactor option extractors
    -  getBackend, getHostname, getWorkspace and getDashboard are things that will be common for multiple commands
    -  move them to base/cli
    
    JIRA: RAIL-3428

---
+++ tools/plugin-toolkit/src/_base/cli/extractors.ts
@@ -0,0 +1,87 @@
+// (C) 2021 GoodData Corporation
+
+import { ActionOptions, TargetBackendType } from "../types";
+import { backendTypeValidator, createHostnameValidator, validOrDie } from "./validators";
+
+/**
+ * Gets a valid hostname from CLI options or dies with validation error.
+ *
+ * @param backend - type of backend for which user is entering hostname
+ * @param options - program & command options
+ * @returns undefined if no hostname specified.
+ */
+export function getHostname(
+    backend: TargetBackendType | undefined,
+    options: ActionOptions,
+): string | undefined {
+    const { hostname } = options.programOpts;
+
+    if (!hostname) {
+        return undefined;
+    }
+
+    // do the hostname validation only if the backend type is known at this point
+    if (backend !== undefined) {
+        validOrDie("hostname", hostname, createHostnameValidator(backend));
+    }
+
+    return hostname;
+}
+
+/**
+ * Gets a valid backend type from CLI options or dies with validation error.
+ *
+ * @param options - program & command options
+ * @returns undefined if no backend type specified.
+ */
+export function getBackend(options: ActionOptions): TargetBackendType | undefined {
+    const { backend } = options.commandOpts;
+
+    if (!backend) {
+        return undefined;
+    }
+
+    validOrDie("backend", backend, backendTypeValidator);
+
+    return backend as TargetBackendType;
+}
+
+/**
+ * Gets a valid workspace from CLI options or dies with validation error.
+ *
+ * Note: does not validate that workspace actually exists. Just that it is in the options.
+ *
+ * @param options - program & command options
+ * @returns undefined if no workspace specified.
+ */
+export function getWorkspace(options: ActionOptions): string | undefined {
+    const { workspaceId } = options.commandOpts;
+
+    if (!workspaceId) {
+        return undefined;
+    }
+
+    validOrDie("workspace", workspaceId, () => true);
+
+    return workspaceId;
+}
+
+/**
+ * Gets a valid dashboard from CLI options or dies with validation error.
+ *
+ * Note: does not validate that dashboard actually exists. Just that it is in the options.
+ *
+ * @param options - program & command options
+ * @returns undefined if no dashboard specified.
+ */
+export function getDashboard(options: ActionOptions): string | undefined {
+    const { dashboardId } = options.commandOpts;
+
+    if (!dashboardId) {
+        return undefined;
+    }
+
+    validOrDie("dashboard", dashboardId, () => true);
+
+    return dashboardId;
+}
