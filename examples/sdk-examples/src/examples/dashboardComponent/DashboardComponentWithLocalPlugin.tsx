43214c691a0a7b0a0af9692d62a93a72174d573b
Dan Homola <dan.homola@gooddata.com>
2021-11-24 10:57:05 +0100

    Add basic examples of the Dashboard component
    JIRA: RAIL-3794

---
+++ examples/sdk-examples/src/examples/dashboardComponent/DashboardComponentWithLocalPlugin.tsx
@@ -0,0 +1,28 @@
+// (C) 2007-2021 GoodData Corporation
+/* eslint-disable import/no-unresolved,import/default */
+import React from "react";
+
+import { ExampleWithSource } from "../../components/ExampleWithSource";
+
+import DashboardComponentWithLocalPlugin from "./DashboardComponentWithLocalPluginSrc";
+import DashboardComponentWithLocalPluginSRC from "./DashboardComponentWithLocalPluginSrc?raw";
+import DashboardComponentWithLocalPluginSRCJS from "./DashboardComponentWithLocalPluginSrc?rawJS";
+
+const DashboardWithLocalPlugin = (): JSX.Element => (
+    <div>
+        <h1>Dashboard with local plugin</h1>
+
+        <p>
+            Example on how to embed a dashboard with a local plugin. The example plugin adds a new section to
+            the Dashboard and renders a text there.
+        </p>
+
+        <ExampleWithSource
+            for={DashboardComponentWithLocalPlugin}
+            source={DashboardComponentWithLocalPluginSRC}
+            sourceJS={DashboardComponentWithLocalPluginSRCJS}
+        />
+    </div>
+);
+
+export default DashboardWithLocalPlugin;
