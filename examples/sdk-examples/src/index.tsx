b98ab95a8f87f39949d46e7d120e71ba94de9315
Dan Homola <dan.homola@gooddata.com>
2021-11-24 10:57:05 +0100

    Add basic examples of the Dashboard component
    JIRA: RAIL-3794

--- examples/sdk-examples/src/index.tsx
+++ examples/sdk-examples/src/index.tsx
@@ -6,6 +6,7 @@ import ReactDOM from "react-dom";
 import "react-datepicker/dist/react-datepicker.css";
 import "@gooddata/sdk-ui-filters/styles/css/main.css";
 import "@gooddata/sdk-ui-charts/styles/css/main.css";
+import "@gooddata/sdk-ui-dashboard/styles/css/main.css";
 import "@gooddata/sdk-ui-pivot/styles/css/main.css";
 import "@gooddata/sdk-ui-kit/styles/css/main.css";
 import "@gooddata/sdk-ui-ext/styles/css/main.css";
