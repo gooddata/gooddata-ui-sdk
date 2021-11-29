c1a07513414e8335ae1c1fab5771ea2a94e48e2f
Dan Homola <dan.homola@gooddata.com>
2021-11-29 16:28:25 +0100

    Remove react-router-use-location-state
    This package was not React 17-compatible and actually not needed.
    
    JIRA: FET-529

--- libs/sdk-ui-tests-e2e/scenarios/src/contexts/Workspace.tsx
+++ libs/sdk-ui-tests-e2e/scenarios/src/contexts/Workspace.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-import { useQueryState } from "react-router-use-location-state";
 import { WorkspaceProvider as DefaultWorkspaceProvider } from "@gooddata/sdk-ui";
 import identity from "lodash/identity";
 
@@ -19,15 +18,7 @@ const WorkspaceContext = createContext<IWorkspaceContext>({
 
 export const WorkspaceProvider: React.FC = ({ children }) => {
     const workspaceList = useWorkspaceList();
-    const [queryWorkspace, setQueryWorkspace] = useQueryState<string>("workspace", defaultWorkspace);
-    const [workspace, setWorkspace] = useState<string>(queryWorkspace);
-
-    // update query string with actual workspace
-    useEffect(() => {
-        setQueryWorkspace(workspace);
-        // Do not include setQueryWorkspace into effect dependencies
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [workspace, queryWorkspace]);
+    const [workspace, setWorkspace] = useState<string>(defaultWorkspace);
 
     // if workspace was not set yet then try to use first workspace available
     useEffect(() => {
