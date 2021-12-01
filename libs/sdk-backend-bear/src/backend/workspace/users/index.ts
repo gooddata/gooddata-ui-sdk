f84f212e0364451b6701723cb72a755fad4c3ad0
Matyas Kandl <matyas.kandl@gooddata.com>
2021-12-01 17:42:13 +0100

    Use ServerPaging in users query
    JIRA: FET-847

--- libs/sdk-backend-bear/src/backend/workspace/users/index.ts
+++ libs/sdk-backend-bear/src/backend/workspace/users/index.ts
@@ -8,7 +8,7 @@ import {
 
 import { BearAuthenticatedCallGuard } from "../../../types/auth";
 import { convertWorkspaceUser } from "../../../convertors/fromBackend/UsersConverter";
-import { enhanceWithAll } from "@gooddata/sdk-backend-base";
+import { ServerPaging } from "@gooddata/sdk-backend-base";
 
 export class BearWorkspaceUsersQuery implements IWorkspaceUsersQuery {
     private options: IWorkspaceUsersQueryOptions = {};
@@ -21,59 +21,40 @@ export class BearWorkspaceUsersQuery implements IWorkspaceUsersQuery {
     }
 
     public async queryAll(): Promise<IWorkspaceUser[]> {
-        const { search } = this.options;
-        return this.authCall((sdk) =>
-            sdk.project
-                .getUserList(this.workspace, { prefixSearch: search, userState: "ACTIVE" })
-                .then((users) => users.map(convertWorkspaceUser)),
-        );
+        const usersQuery = await this.query();
+        return usersQuery.all();
     }
 
-    private async queryWorker(
-        offset: number,
-        limit: number,
-        options: IWorkspaceUsersQueryOptions,
-    ): Promise<IWorkspaceUsersQueryResult> {
+    private async queryWorker(options: IWorkspaceUsersQueryOptions): Promise<IWorkspaceUsersQueryResult> {
         const { search } = options;
-        const data = await this.authCall((sdk) =>
-            sdk.project.getUserListWithPaging(this.workspace, {
-                prefixSearch: search,
-                userState: "ACTIVE",
-                offset,
-                limit,
-            }),
+
+        return ServerPaging.for(
+            async ({ limit, offset }) => {
+                const data = await this.authCall((sdk) =>
+                    sdk.project.getUserListWithPaging(this.workspace, {
+                        prefixSearch: search,
+                        userState: "ACTIVE",
+                        offset,
+                        limit,
+                    }),
+                );
+
+                const {
+                    items,
+                    paging: { totalCount },
+                } = data.userList;
+
+                return {
+                    items: items.map(convertWorkspaceUser),
+                    totalCount,
+                };
+            },
+            this.options.limit ?? 1000,
+            this.options.offset,
         );
-        const { items, paging } = data.userList;
-        const total = paging.totalCount;
-        const serverOffset = paging.offset ?? 0;
-        const { count } = paging;
-
-        const hasNextPage = serverOffset + count < total;
-        const goTo = (pageIndex: number) => this.queryWorker(pageIndex * count, limit, options);
-
-        const emptyResult: IWorkspaceUsersQueryResult = enhanceWithAll({
-            items: [],
-            limit: count,
-            offset: total,
-            totalCount: total,
-            next: () => Promise.resolve(emptyResult),
-            goTo,
-        });
-
-        return enhanceWithAll({
-            items: items.map(convertWorkspaceUser),
-            limit: count,
-            offset: serverOffset,
-            totalCount: total,
-            next: hasNextPage
-                ? () => this.queryWorker(offset + count, limit, options)
-                : () => Promise.resolve(emptyResult),
-            goTo,
-        });
     }
 
     public async query(): Promise<IWorkspaceUsersQueryResult> {
-        const { offset = 0, limit = 1000 } = this.options;
-        return this.queryWorker(offset, limit, this.options);
+        return this.queryWorker(this.options);
     }
 }
