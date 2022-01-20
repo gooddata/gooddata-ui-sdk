517c959e4576fd560d06432720d63f1036d77cc6
Martin Plavek <martin.plavek@gooddata.com>
2022-01-20 15:31:00 +0100

    Remove duplicate call of useDashboardComponentsContext
    JIRA: RAIL-3979

--- libs/sdk-ui-dashboard/src/presentation/widget/insight/DefaultDashboardInsight/InsightDrillDialog/DrillDialogInsight.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/insight/DefaultDashboardInsight/InsightDrillDialog/DrillDialogInsight.tsx
@@ -12,7 +12,7 @@ import {
     useWorkspaceStrict,
 } from "@gooddata/sdk-ui";
 import { InsightRenderer } from "@gooddata/sdk-ui-ext";
-import { useDashboardComponentsContext } from "../../../../dashboardContexts";
+
 import {
     useDashboardSelector,
     selectColorPalette,
@@ -28,7 +28,6 @@ import { useWidgetFilters } from "../../../common";
 import { useResolveDashboardInsightProperties } from "../useResolveDashboardInsightProperties";
 import { useDrillDialogInsightDrills } from "./useDrillDialogInsightDrills";
 import { CustomError } from "../CustomError/CustomError";
-import { ThemedLoadingEqualizer } from "../../../../presentationComponents";
 import { IntlWrapper } from "../../../../localization";
 
 const insightStyle: CSSProperties = { width: "100%", height: "100%", position: "relative", flex: "1 1 auto" };
@@ -66,19 +65,9 @@ export const DrillDialogInsight = (props: IDashboardInsightProps): JSX.Element =
         onExportReady,
         onLoadingChanged,
         pushData,
-        ErrorComponent: CustomErrorComponent,
-        LoadingComponent: CustomLoadingComponent,
+        ErrorComponent,
+        LoadingComponent,
     } = props;
-
-    // Custom components
-    const { ErrorComponent, LoadingComponent } = useDashboardComponentsContext({
-        ErrorComponent: CustomErrorComponent,
-        /**
-         * There is a need to use Loading spinner instead of "Running three dots" loader while drill is loading.
-         * If no custom loading component is provided, LoadingComponent defaults to Loading spinner.
-         */
-        LoadingComponent: CustomLoadingComponent ?? ThemedLoadingEqualizer,
-    });
 
     // Context
     const effectiveBackend = useBackendStrict(backend);
