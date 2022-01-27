77efb716c41185ef5c8300ad6684f672636903dc
Dan Homola <dan.homola@gooddata.com>
2022-01-27 09:26:52 +0100

    Fix TSDoc issues in sdk-ui-tests-e2e
    JIRA: FET-966

--- libs/sdk-ui-tests-e2e/cypress/plugins/index.ts
+++ libs/sdk-ui-tests-e2e/cypress/plugins/index.ts
@@ -15,9 +15,6 @@
 
 import logPlugin from "./log";
 
-/**
- * @type {Cypress.PluginConfig}
- */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on: any, config: any) => {
     // `on` is used to hook into various events Cypress emits
