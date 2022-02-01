d8d228c64b2bdcb90a0b4721a972da46d1196b2f
oleg-morozov <oleg.morozov@gooddata.com>
2022-02-01 13:38:40 +0100

    KPI alert close button is hidden with small screen
    JIRA: TNT-429

--- libs/sdk-ui-dashboard/src/presentation/widget/kpi/DefaultDashboardKpi/KpiAlerts/KpiAlertDialog/alignPoints.ts
+++ libs/sdk-ui-dashboard/src/presentation/widget/kpi/DefaultDashboardKpi/KpiAlerts/KpiAlertDialog/alignPoints.ts
@@ -1,4 +1,4 @@
-// (C) 2020-2021 GoodData Corporation
+// (C) 2020-2022 GoodData Corporation
 import { IAlignPoint } from "@gooddata/sdk-ui-kit";
 
 export const kpiAlertDialogAlignPoints: IAlignPoint[] = [
@@ -17,20 +17,6 @@ export const kpiAlertDialogAlignPoints: IAlignPoint[] = [
         },
     },
     {
-        align: "br bl",
-        offset: {
-            x: 18,
-            y: 0,
-        },
-    },
-    {
-        align: "bl br",
-        offset: {
-            x: -18,
-            y: 0,
-        },
-    },
-    {
         align: "bc tc",
         offset: {
             x: 0,
@@ -38,10 +24,31 @@ export const kpiAlertDialogAlignPoints: IAlignPoint[] = [
         },
     },
     {
-        align: "cc cr",
+        align: "tc cr",
         offset: {
-            x: -200,
-            y: 0,
+            x: -100,
+            y: 90,
+        },
+    },
+    {
+        align: "tc cl",
+        offset: {
+            x: 100,
+            y: 90,
+        },
+    },
+    {
+        align: "tc cr",
+        offset: {
+            x: -50,
+            y: 90,
+        },
+    },
+    {
+        align: "tc cl",
+        offset: {
+            x: 50,
+            y: 90,
         },
     },
 ];
