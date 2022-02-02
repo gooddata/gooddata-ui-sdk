3ea6e714fcc46dcd21a63f54a5d0676c8370c0da
Martin Plavek <martin.plavek@gooddata.com>
2022-02-02 14:37:30 +0100

    DashboardStateAccessors docs and refactoring
    - refactor name of the accessor class
    - code documentation
    
    JIRA: RAIL-3989

--- libs/sdk-ui-dashboard/src/model/store/storeAccessors/StoreAccessors.ts
+++ libs/sdk-ui-dashboard/src/model/store/storeAccessors/StoreAccessors.ts
@@ -3,26 +3,58 @@
 import { DashboardDispatch, DashboardSelectorEvaluator } from "../types";
 
 /**
- * This singleton class servers the selector and the dispatcher properties of the dashboard component state.
+ * This singleton class serves the selector and the dispatcher properties of the dashboard component state.
  *
- * To get latest properties, use static member function {@link StoreAccessors#getInstance}. If there is already an instance
- * created, it will return this instance and will return new instance of the {@link StoreAccessors} otherwise.
+ * The {@link Dashboard} component has an optional property {@link IDashboardProps#onStateChange} through which
+ * you can handle set the values for {@link DashboardDispatch} and {@link DashboardSelectorEvaluator}.
+ *
+ *
+ * In your component using {@link Dashboard} you can create and instance of the {@link DashboardStoreAccessor} object
+ * and use it like in the example below. The example shows the accessor's usage as well. There is a need to check
+ * the select and dispatch object existence.
+ *
+ * @example
+ * ```
+ *  const stateAccessors = StoreAccessors.getInstance();
+ *
+ *  const onStateChange = (state: DashboardState, dispatch: DashboardDispatch) => {
+ *      const dashboardSelect: DashboardSelectorEvaluator = (select) => select(state);
+ *
+ *      stateAccessors.setSelector(dashboardSelect);
+ *      stateAccessors.setDispatch(dispatch);
+ *  }
+ *
+ *  // in the code where needed
+ *  stateAccessors.getSelector()?.(selectEffectiveDateFilterOptions);
+ *  stateAccessors.getDispatch()?.(saveDashboardAs("Saved from exposed dispatch", false, false));
+ *
+ *  return (
+ *      <Dashboard dashboard={"<dashboardId>"} onStateChange={onStateChange}/>
+ *  )
+ * ```
+ *
+ * To get latest properties, use static member function {@link DashboardStoreAccessor#getInstance}. If there is already an instance
+ * created, it will return this instance and will return new instance of the {@link DashboardStoreAccessor} otherwise.
  *
  * @public
  */
-export class StoreAccessors {
-    static stateAccessor: StoreAccessors;
+export class DashboardStoreAccessor {
+    static stateAccessor: DashboardStoreAccessor;
 
     selectorEvaluator: DashboardSelectorEvaluator | undefined;
     dispatch: DashboardDispatch | undefined;
+
+    private constructor() {
+        // constructor empty on purpose to prevent incorrect StoreAccessors object initialization
+    }
 
     /**
      * @returns stateAccessor - an existing instance of StoreAccessors class. If no instance is available,
      * the new one is created.
      */
     static getInstance() {
-        if (!StoreAccessors.stateAccessor) {
-            StoreAccessors.stateAccessor = new StoreAccessors();
+        if (!DashboardStoreAccessor.stateAccessor) {
+            DashboardStoreAccessor.stateAccessor = new DashboardStoreAccessor();
         }
         return this.stateAccessor;
     }
