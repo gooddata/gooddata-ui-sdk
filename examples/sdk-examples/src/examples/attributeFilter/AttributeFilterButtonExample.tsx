fe3d955aa425ac3b2e47b70d063e3a8391505790
Martin Plavek <martin.plavek@gooddata.com>
2022-02-24 10:53:21 +0100

    Refactor attributeFilter basic examples
    JIRA: RAIL-4003

--- examples/sdk-examples/src/examples/attributeFilter/AttributeFilterButtonExample.tsx
+++ examples/sdk-examples/src/examples/attributeFilter/AttributeFilterButtonExample.tsx
@@ -1,16 +1,8 @@
 // (C) 2007-2022 GoodData Corporation
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { OnError, OnLoadingChanged } from "@gooddata/sdk-ui";
 import { LineChart } from "@gooddata/sdk-ui-charts";
-import {
-    filterIsEmpty,
-    IAttributeFilter,
-    INegativeAttributeFilter,
-    IPositiveAttributeFilter,
-    isPositiveAttributeFilter,
-    modifyMeasure,
-    newNegativeAttributeFilter,
-} from "@gooddata/sdk-model";
+import { IAttributeFilter, modifyMeasure, newNegativeAttributeFilter } from "@gooddata/sdk-model";
 import * as Md from "../../md/full";
 import { AttributeFilterButton } from "@gooddata/sdk-ui-filters";
 
@@ -18,100 +10,35 @@ const TotalSales = modifyMeasure(Md.$TotalSales, (m) =>
     m.format("#,##0").alias("$ Total Sales").title("Total Sales"),
 );
 
-interface IAttributeFilterExampleState {
-    filters: Array<IPositiveAttributeFilter | INegativeAttributeFilter> | undefined;
-    error: string | undefined;
-}
+export const AttributeFilterButtonExample: React.FC = () => {
+    const [filter, setFilter] = useState<IAttributeFilter>(
+        newNegativeAttributeFilter(Md.LocationResort, { uris: [] }),
+    );
 
-export class AttributeFilterButtonExample extends Component<unknown, IAttributeFilterExampleState> {
-    state: IAttributeFilterExampleState = {
-        filters: [],
-        error: undefined,
-    };
-
-    public onLoadingChanged: OnLoadingChanged = (...params) => {
+    const onLoadingChanged: OnLoadingChanged = (...params) => {
         // eslint-disable-next-line no-console
         console.info("AttributeFilterExample onLoadingChanged", ...params);
     };
 
-    public onApply = (filter: IAttributeFilter): void => {
-        // eslint-disable-next-line no-console
-        console.log("AttributeFilterExample onApply", filter);
-        this.setState({ filters: [], error: undefined });
-        if (isPositiveAttributeFilter(filter)) {
-            this.filterPositiveAttribute(filter);
-        } else {
-            this.filterNegativeAttribute(filter);
-        }
-    };
-
-    public onError: OnError = (...params) => {
+    const onError: OnError = (...params) => {
         // eslint-disable-next-line no-console
         console.info("AttributeFilterExample onLoadingChanged", ...params);
     };
 
-    public filterPositiveAttribute(filter: IPositiveAttributeFilter): void {
-        let filters;
-        const {
-            positiveAttributeFilter: { displayForm },
-        } = filter;
-        const inElements = filter.positiveAttributeFilter.in;
-        const checkLengthOfFilter = !filterIsEmpty(filter);
-        if (checkLengthOfFilter) {
-            filters = [
-                {
-                    positiveAttributeFilter: {
-                        displayForm,
-                        in: inElements,
-                    },
-                },
-            ];
-        } else {
-            this.setState({
-                error: "The filter must have at least one item selected",
-            });
-        }
-        this.setState({ filters });
-    }
-
-    public filterNegativeAttribute(filter: INegativeAttributeFilter): void {
-        let filters;
-        const {
-            negativeAttributeFilter: { notIn, displayForm },
-        } = filter;
-        const checkLengthOfFilter = !filterIsEmpty(filter);
-        if (checkLengthOfFilter) {
-            filters = [
-                {
-                    negativeAttributeFilter: {
-                        displayForm,
-                        notIn,
-                    },
-                },
-            ];
-        }
-        this.setState({ filters });
-    }
-
-    public render(): React.ReactNode {
-        const { filters } = this.state;
-        const filter = filters?.[0] ?? newNegativeAttributeFilter(Md.LocationResort, { uris: [] });
-
-        return (
-            <div className="s-attribute-filter">
-                <AttributeFilterButton filter={filter} onApply={this.onApply} onError={this.onError} />
-                <div style={{ height: 300 }} className="s-line-chart">
-                    <LineChart
-                        measures={[TotalSales]}
-                        trendBy={Md.LocationResort}
-                        filters={filters}
-                        onLoadingChanged={this.onLoadingChanged}
-                        onError={this.onError}
-                    />
-                </div>
+    return (
+        <div className="s-attribute-filter">
+            <AttributeFilterButton filter={filter} onApply={setFilter} onError={onError} />
+            <div style={{ height: 300 }} className="s-line-chart">
+                <LineChart
+                    measures={[TotalSales]}
+                    trendBy={Md.LocationResort}
+                    filters={filter ? [filter] : []}
+                    onLoadingChanged={onLoadingChanged}
+                    onError={onError}
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default AttributeFilterButtonExample;
