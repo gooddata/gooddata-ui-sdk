33d63475d3c3f0aec839b2eb21ae5d22d471dd24
Dan Homola <dan.homola@gooddata.com>
2022-02-24 16:20:35 +0100

    Simplify story-extractor
    Avoid the need for async imports.
    
    JIRA: FET-922

---
+++ libs/sdk-ui-tests/backstop/generateStoryExtractorInput.mjs
@@ -0,0 +1,29 @@
+// (C) 2022 GoodData Corporation
+import fg from "fast-glob";
+import path from "path";
+import { writeFile } from "fs/promises";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storiesGlob = "../stories/**/*.@(ts|tsx)";
+const targetFile = path.resolve(__dirname, "generated.stories.js");
+
+/**
+ * Generate a barrel file with imports of all the story files.
+ * This has to be done ahead of time, otherwise jest is much slower and fails on CI.
+ */
+async function main() {
+    const files = await fg(storiesGlob, { cwd: path.resolve(__dirname) });
+
+    const header = `// (C) ${new Date().getFullYear()} GoodData Corporation`;
+
+    const requires = files.map((file) => `import "${file}";`).join("\n");
+
+    const fileContents = `${header}\n${requires}`;
+
+    await writeFile(targetFile, fileContents, { encoding: "utf8" });
+}
+
+main();
--- libs/sdk-ui-tests/backstop/story-extractor.js
+++ libs/sdk-ui-tests/backstop/story-extractor.js
@@ -1,35 +1,19 @@
 // (C) 2022 GoodData Corporation
 /* eslint-disable no-console */
-import fg from "fast-glob";
 import path from "path";
-import { writeFile } from "fs/promises";
-import { existsSync } from "fs";
+import { existsSync, writeFileSync } from "fs";
 
 import { toBackstopJson } from "../stories/_infra/storyRepository";
 
-const storiesGlob = path.resolve(__dirname, "../stories/**/*.@(ts|tsx)");
+import "./generated.stories";
+
 const targetFile = path.resolve(__dirname, "stories.json");
 
 describe("story-extractor", () => {
-    it("dumps stories into a file", async () => {
-        try {
-            console.log("Going to extract stories for backstop...");
-            const files = await fg(storiesGlob, { cwd: path.resolve(__dirname) });
+    it("dumps stories into a file", () => {
+        const fileContents = toBackstopJson();
 
-            await Promise.all(
-                files.map((file) => {
-                    console.log("Importing story file: ", file);
-                    return import(file);
-                }),
-            );
-
-            const fileContents = toBackstopJson();
-
-            await writeFile(targetFile, fileContents, { encoding: "utf-8" });
-            expect(existsSync(targetFile)).toBe(true);
-        } catch (e) {
-            console.error("Error extracting stories: ", e);
-            throw e;
-        }
+        writeFileSync(targetFile, fileContents, { encoding: "utf-8" });
+        expect(existsSync(targetFile)).toBe(true);
     });
 });
