54e60847e884dce1a0f96e462573d9781e64f7a7
Martin Plavek <martin.plavek@gooddata.com>
2022-02-28 15:48:44 +0100

    Fix the path to the videos
    JIRA: RAIL-4023

--- libs/sdk-ui-tests-e2e/scripts/create_github_report.js
+++ libs/sdk-ui-tests-e2e/scripts/create_github_report.js
@@ -44,7 +44,7 @@ function createSuitesMessage(testResults) {
         const shortFileName = file.split("/").slice(-1)[0];
 
         const videoFile = BUILD_URL
-            ? `${BUILD_URL}/artifact/test_new/cypress/videos/${shortFileName}.mp4`
+            ? `${BUILD_URL}/artifact/libs/sdk-ui-tests-e2e/cypress/videos/${shortFileName}.mp4`
             : `#`;
 
         const formattedFile = isFailing ? `[🎥 ${shortFileName}](${videoFile})` : shortFileName;
