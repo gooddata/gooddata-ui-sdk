4d95fb71fbfea2813eed52a520591cca3f55fb65
Martin Plavek <martin.plavek@gooddata.com>
2022-03-01 14:47:06 +0100

    AttributeFilterButton tests
    JIRA: RAIL-4041

--- libs/sdk-ui-tests-e2e/cypress/integration/02-sdk-ui-filters/attributeFilterButton.spec.ts
+++ libs/sdk-ui-tests-e2e/cypress/integration/02-sdk-ui-filters/attributeFilterButton.spec.ts
@@ -3,6 +3,9 @@ import * as Navigation from "../../tools/navigation";
 import { AttributeFilterButton } from "../../tools/attributeFilterButton";
 import camelCase from "lodash/camelCase";
 
+const SEARCH_QUERY = "101 Financial";
+const INVALID_SEARCH_QUERY = "does not exist";
+
 describe("AttributeFilterButton", () => {
     beforeEach(() => {
         cy.login();
@@ -29,5 +32,94 @@ describe("AttributeFilterButton", () => {
             );
 
         attributeFilter.clickCancel().subtitleHasText("All");
+    });
+
+    it("search", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .isFilteredElementsCount(2);
+    });
+
+    it("select searched elements", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .clearSelection()
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .selectAll()
+            .clickApply()
+            .subtitleHasText("101 Financial > Educationly, 101 Financial > WonderKid");
+    });
+
+    it("deselect search elements", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .clearSelection()
+            .subtitleHasText("All except 101 Financial > Educationly, 101 Financial > WonderKid");
+    });
+
+    it("search does not modify selection", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .clearSelection()
+            .selectElement(camelCase("1000Bulbs.com > Educationly"))
+            .selectElement(camelCase("1000Bulbs.com > PhoenixSoft"))
+            .selectElement(camelCase("101 Financial > Educationly"))
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .subtitleHasText(
+                "1000Bulbs.com > Educationly, 1000Bulbs.com > PhoenixSoft, 101 Financial > Educationly",
+            );
+    });
+
+    it("search elements corresponding with the query", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .elementsCorrespondToQuery(SEARCH_QUERY);
+    });
+
+    it("search out all elements", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .searchElements(INVALID_SEARCH_QUERY)
+            .waitElementsLoaded()
+            .isFilteredElementsCount(0)
+            .isAllElementsFiltered();
+    });
+
+    it("apply not disabled when clear selection on searched out elements", () => {
+        const attributeFilter = new AttributeFilterButton();
+
+        attributeFilter
+            .open()
+            .waitElementsLoaded()
+            .searchElements(SEARCH_QUERY)
+            .waitElementsLoaded()
+            .clearSelection()
+            .applyDisabled(false);
     });
 });
--- libs/sdk-ui-tests-e2e/cypress/tools/attributeFilterButton.ts
+++ libs/sdk-ui-tests-e2e/cypress/tools/attributeFilterButton.ts
@@ -30,8 +30,8 @@ export class AttributeFilterButton {
         return this;
     }
 
-    applyDisabled(): this {
-        cy.get(".s-apply").should("have.class", "disabled");
+    applyDisabled(disabled = true): this {
+        cy.get(".s-apply").should(disabled ? "have.class" : "not.have.class", "disabled");
         return this;
     }
 
@@ -47,6 +47,36 @@ export class AttributeFilterButton {
 
     scrollDown(): this {
         cy.get(".ScrollbarLayout_main").trigger("wheel", {});
+        return this;
+    }
+
+    waitElementsLoaded(): this {
+        // wait until loading is initialized
+        cy.get(".s-isLoading").should("exist");
+        // wait until loading is finished
+        cy.get(".s-isLoading").should("not.exist");
+        return this;
+    }
+
+    searchElements(query: string): this {
+        cy.get(".s-attribute-filter-button-search-field .gd-input-field").focus().type(query);
+        return this;
+    }
+
+    isFilteredElementsCount(elementCount: number): this {
+        cy.get(".s-attribute-filter-list-item").should("have.length", elementCount);
+        return this;
+    }
+
+    elementsCorrespondToQuery(query: string): this {
+        cy.get(".s-attribute-filter-list-item span").each((item) => {
+            cy.wrap(item).should("include.text", query);
+        });
+        return this;
+    }
+
+    isAllElementsFiltered(): this {
+        cy.get(".s-list-no-results").should("exist");
         return this;
     }
 }
