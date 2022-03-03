ab3db367a0f02dc4f15f87718935c1fb625f9d1a
Matyas Kandl <matyas.kandl@gooddata.com>
2022-03-03 17:53:00 +0100

    Add PieChart and DonutChart description
    - Provide high-level description of the following Pluggable charts (buckets, dimesions, sorts):
        * DonutChart
        * PieChart
    
    JIRA: RAIL-4052

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pieChart/PluggablePieChart.tsx
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pieChart/PluggablePieChart.tsx
@@ -45,6 +45,40 @@ import { getReferencePointWithSupportedProperties } from "../../../utils/propert
 import { removeSort } from "../../../utils/sort";
 import { setPieChartUiConfig } from "../../../utils/uiConfigHelpers/pieChartUiConfigHelper";
 
+/**
+ * PluggablePieChart
+ *
+ * ## Buckets
+ *
+ * | Name     | Id       | Accepts             |
+ * |----------|----------|---------------------|
+ * | Measures | measures | measures only       |
+ * | ViewBy   | view     | attribute or date   |
+ *
+ * ### Bucket axioms
+ *
+ * - |ViewBy| ≤ 1
+ * - |Measures| ≥ 1 ∧ ≤ 20
+ * - |ViewBy| = 1 ⇒ |Measures| = 1
+ * - |ViewBy| = 0 ⇒ |Measures| ≥ 1
+ *
+ * ## Dimensions
+ *
+ * The PluggablePieChart always creates two dimensional execution.
+ *
+ * With measures only:
+ * - [[], [MeasureGroupIdentifier]]
+ * With viewBy:
+ * - [[MeasureGroupIdentifier], [ViewBy]]
+ *
+ * ## Default sorts
+ *
+ * When Pie Chart is used with measures only, it's sorted by their order by default.
+ * When Pie Chart chart is used with viewBy attribute or date, it's sorted by the values of the measure by default.
+ *
+ * Default sort behavior can be overriden by sortBy option.
+ *
+ */
 export class PluggablePieChart extends PluggableBaseChart {
     constructor(props: IVisConstruct) {
         super(props);
