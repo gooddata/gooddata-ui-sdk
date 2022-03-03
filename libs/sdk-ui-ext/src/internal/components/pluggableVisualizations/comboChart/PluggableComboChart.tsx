ad9b1b2d0be7799305cdc6d268c3b34688df744a
Dan Homola <dan.homola@gooddata.com>
2022-03-03 16:41:02 +0100

    Add PluggableComboChart description
    JIRA: RAIL-4052

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/comboChart/PluggableComboChart.tsx
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/comboChart/PluggableComboChart.tsx
@@ -58,6 +58,33 @@ import {
 import { setComboChartUiConfig } from "../../../utils/uiConfigHelpers/comboChartUiConfigHelper";
 import LineChartBasedConfigurationPanel from "../../configurationPanels/LineChartBasedConfigurationPanel";
 
+/**
+ * PluggableComboChart
+ *
+ * ## Buckets
+ *
+ * | Name                | Id                 | Accepts             |
+ * |---------------------|--------------------|---------------------|
+ * | Measure (Primary)   | measures           | measures only       |
+ * | Measure (Secondary) | secondary_measures | measures only       |
+ * | ViewBy              | view               | attributes or dates |
+ *
+ * ### Bucket axioms
+ *
+ * - |MeasurePrimary| ≤ 20
+ * - |MeasureSecondary| ≤ 20
+ * - |ViewBy| ≤ 1
+ *
+ * ## Dimensions
+ *
+ * The PluggableComboChart always creates the same two dimensional execution.
+ *
+ * - ⊤ ⇒ [[MeasureGroupIdentifier], [...ViewBy]]
+ *
+ * ## Default sorts
+ *
+ * The PluggableComboChart does not use any sorts.
+ */
 export class PluggableComboChart extends PluggableBaseChart {
     private primaryChartType: string = VisualizationTypes.COLUMN;
     private secondaryChartType: string = VisualizationTypes.COLUMN;
