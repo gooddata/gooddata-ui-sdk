a79ae02192b7a4954d0796bb92e95493638893c5
scavnickyj <jakub.scavnicky@gooddata.com>
2022-03-09 11:53:12 +0100

    Revert "Fix insight title hiding for tiger"
    This reverts commit fef37956f2b59dc9b0062524e1ed150c6bb4a2d8.

--- libs/sdk-backend-tiger/src/convertors/fromBackend/analyticalDashboards/widgetConfiguration.ts
+++
@@ -1,7 +0,0 @@
-// (C) 2020-2022 GoodData Corporation
-
-import { IInsightWidgetConfiguration } from "@gooddata/sdk-backend-spi";
-
-export const defaultWidgetConfiguration: IInsightWidgetConfiguration = {
-    hideTitle: false,
-};
