ecdc8efbbd6486dfbe4cf3900a50c5302345be41
Dan Homola <dan.homola@gooddata.com>
2022-03-10 13:29:50 +0100

    Normalize localIds in embedding code generator
    This will help to make the resulting code nicer in case the original
    insight has ugly/randomized localIds.
    
    TODO: properties
    
    JIRA: RAIL-4069

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/bubbleChart/tests/BubbleChartDescriptor.test.ts
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/bubbleChart/tests/BubbleChartDescriptor.test.ts
@@ -1,6 +1,12 @@
 // (C) 2022 GoodData Corporation
 import { ReferenceMd } from "@gooddata/reference-workspace";
-import { newBucket, newInsightDefinition } from "@gooddata/sdk-model";
+import {
+    modifyAttribute,
+    modifyMeasure,
+    newArithmeticMeasure,
+    newBucket,
+    newInsightDefinition,
+} from "@gooddata/sdk-model";
 import { BucketNames } from "@gooddata/sdk-ui";
 import { BubbleChartDescriptor } from "../BubbleChartDescriptor";
 
@@ -8,8 +14,32 @@ describe("BubbleChartDescriptor", () => {
     // TODO replace by some recording?
     const insight = newInsightDefinition("local:bubble", (i) =>
         i.buckets([
-            newBucket(BucketNames.MEASURES, ReferenceMd.Amount),
-            newBucket(BucketNames.VIEW, ReferenceMd.Region),
+            newBucket(
+                BucketNames.MEASURES,
+                modifyMeasure(ReferenceMd.Amount, (m) =>
+                    m.alias("Custom Amount alias").localId("amount-custom-local-id"),
+                ),
+            ),
+            newBucket(
+                BucketNames.SECONDARY_MEASURES,
+                modifyMeasure(ReferenceMd.Velocity.Sum, (m) =>
+                    m.alias("Custom Velocity alias").localId("velocity-custom-local-id"),
+                ),
+            ),
+            newBucket(
+                BucketNames.TERTIARY_MEASURES,
+                newArithmeticMeasure(
+                    ["amount-custom-local-id", "velocity-custom-local-id"],
+                    "multiplication",
+                    (m) => m.title("Velocity times Amount"),
+                ),
+            ),
+            newBucket(
+                BucketNames.VIEW,
+                modifyAttribute(ReferenceMd.Region, (a) =>
+                    a.alias("Custom Region alias").localId("region-custom-local-id"),
+                ),
+            ),
         ]),
     );
     const descriptor = new BubbleChartDescriptor();
--- libs/sdk-ui-ext/src/internal/utils/embeddingCodeGenerator.ts
+++ libs/sdk-ui-ext/src/internal/utils/embeddingCodeGenerator.ts
@@ -1,12 +1,37 @@
 // (C) 2022 GoodData Corporation
 import compact from "lodash/compact";
+import flow from "lodash/fp/flow";
+import fromPairs from "lodash/fp/fromPairs";
 import isEmpty from "lodash/isEmpty";
 import isFunction from "lodash/isFunction";
 import isString from "lodash/isString";
+import map from "lodash/fp/map";
 import repeat from "lodash/repeat";
-import toPairs from "lodash/toPairs";
-import { factoryNotationFor, IInsightDefinition } from "@gooddata/sdk-model";
+import toPairs from "lodash/fp/toPairs";
+import {
+    attributeAlias,
+    attributeLocalId,
+    bucketItems,
+    bucketTotals,
+    factoryNotationFor,
+    IInsightDefinition,
+    insightBuckets,
+    insightSetBuckets,
+    insightSetFilters,
+    insightSetSorts,
+    isAttribute,
+    measureAlias,
+    measureFormat,
+    measureLocalId,
+    measureTitle,
+    modifyAttribute,
+    modifyMeasure,
+    newBucket,
+    newDefForInsight,
+    newTotal,
+} from "@gooddata/sdk-model";
 import { IEmbeddingCodeConfig } from "../interfaces/VisualizationDescriptor";
+import { Normalizer } from "@gooddata/sdk-backend-base";
 
 export interface IComponentInfo {
     name: string;
@@ -63,12 +88,75 @@ function indent(str: string, tabs: number): string {
         .join("\n");
 }
 
+/**
+ * Creates an insight that has reasonable local ids instead of potentially long illegible ones in the original insight.
+ *
+ * @privateRemarks
+ * Makes use of the {@link @gooddata/sdk-backed-base#Normalizer} to do most of the work, filling back things
+ * that the Normalizer removes (like titles, aliases, etc.).
+ *
+ * @param insight - the insight to "normalize"
+ * @returns always a new instance
+ */
+function normalizeInsight(insight: IInsightDefinition): IInsightDefinition {
+    const execution = newDefForInsight("foo", insight);
+    const { n2oMap, normalized } = Normalizer.normalize(execution);
+
+    const o2nMap = flow(
+        toPairs,
+        map(([normalized, original]) => [original, normalized]),
+        fromPairs,
+    )(n2oMap);
+
+    const processedBuckets = insightBuckets(insight).map((originalBucket) => {
+        // put back stuff deleted by the normalizer
+        const processedItems = bucketItems(originalBucket).map((originalBucketItem) => {
+            if (isAttribute(originalBucketItem)) {
+                const normalizedId = o2nMap[attributeLocalId(originalBucketItem)];
+                const normalizedBucketItem = normalized.attributes.find(
+                    (attr) => attributeLocalId(attr) === normalizedId,
+                );
+                return modifyAttribute(normalizedBucketItem, (a) =>
+                    a.alias(attributeAlias(originalBucketItem)),
+                );
+            } else {
+                const normalizedId = o2nMap[measureLocalId(originalBucketItem)];
+                const normalizedBucketItem = normalized.measures.find(
+                    (measure) => measureLocalId(measure) === normalizedId,
+                );
+                return modifyMeasure(normalizedBucketItem, (m) =>
+                    m
+                        .alias(measureAlias(originalBucketItem))
+                        .format(measureFormat(originalBucketItem))
+                        .title(measureTitle(originalBucketItem)),
+                );
+            }
+        });
+
+        const processedTotals = bucketTotals(originalBucket).map((originalTotal) => {
+            const { attributeIdentifier, measureIdentifier, type, alias } = originalTotal;
+            return newTotal(type, o2nMap[measureIdentifier], o2nMap[attributeIdentifier], alias);
+        });
+
+        return newBucket(originalBucket.localIdentifier, ...processedItems, ...processedTotals);
+    });
+
+    // TODO properties
+
+    return flow(
+        (i) => insightSetBuckets(i, processedBuckets),
+        (i) => insightSetFilters(i, normalized.filters),
+        (i) => insightSetSorts(i, normalized.sortBy),
+    )(insight);
+}
+
 export function getReactEmbeddingCodeGenerator(
     componentInfo: IComponentInfo,
     insightToProps: (insight: IInsightDefinition) => Record<string, any>,
 ): (insight: IInsightDefinition, config?: IEmbeddingCodeConfig) => string {
     return (insight, config) => {
-        const props = insightToProps(insight);
+        const normalizedInsight = normalizeInsight(insight);
+        const props = insightToProps(normalizedInsight);
         const propPairs = toPairs(props)
             // we ignore functions as there is no bullet-proof way to serialize them
             .filter(([_, value]) => !isFunction(value))
