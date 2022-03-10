dfb389094b1b6835b48a371368867ab1c94a3183
Petr Benes <petr.benes@gooddata.com>
2022-03-10 07:56:06 +0100

    Do not mount all recordings when running tests
    Each test loads recordings before run via wiremock api so remove this
    forgotten mounting of all recordings in docker compose.
    
    Finally, enable the (previuosly failing) tests which were affected
    by other tests' mappings which were all mounted during test run.
    
    JIRA: FET-994

--- libs/sdk-ui-tests-e2e/cypress/integration/03-sdk-ui-charts/pivotTableRemoveOneFromMutipleAggregation.spec.ts
+++ libs/sdk-ui-tests-e2e/cypress/integration/03-sdk-ui-charts/pivotTableRemoveOneFromMutipleAggregation.spec.ts
@@ -8,11 +8,7 @@ describe("Pivot Table Aggregations remove all totals", () => {
         Navigation.visit("visualizations/pivot-table/pivot-table-all-total-aggregations-menu");
     });
 
-    // this test is running well on live backend but on recorded backend it failed
-    // table not get correct response after total removed
-
-    // eslint-disable-next-line jest/no-disabled-tests
-    it.skip("should remove totals for one measure and one keep the other total (SEPARATE)", () => {
+    it("should remove totals for one measure and one keep the other total (SEPARATE)", () => {
         const table = new Table(".s-pivot-table-aggregations-menu");
         table.waitLoaded();
 
