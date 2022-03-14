2e93f635a427a1d531320f568b4908fdb618aa2f
Dan Homola <dan.homola@gooddata.com>
2022-03-14 09:48:17 +0100

    Add embedding code to AreaChart descriptor
    JIRA: RAIL-4069

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/areaChart/AreaChartDescriptor.ts
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/areaChart/AreaChartDescriptor.ts
@@ -1,17 +1,30 @@
-// (C) 2021 GoodData Corporation
-import { IDrillEvent } from "@gooddata/sdk-ui";
-import { PluggableVisualizationFactory } from "../../../interfaces/VisualizationDescriptor";
+// (C) 2021-2022 GoodData Corporation
+import { BucketNames, IDrillEvent } from "@gooddata/sdk-ui";
+import {
+    IVisualizationDescriptor,
+    PluggableVisualizationFactory,
+} from "../../../interfaces/VisualizationDescriptor";
 import { PluggableAreaChart } from "./PluggableAreaChart";
 import { BigChartDescriptor } from "../BigChartDescriptor";
-import { IInsight } from "@gooddata/sdk-model";
+import {
+    bucketAttribute,
+    bucketAttributes,
+    bucketMeasures,
+    IInsight,
+    insightBucket,
+    insightFilters,
+    insightSorts,
+} from "@gooddata/sdk-model";
 import {
     modifyBucketsAttributesForDrillDown,
     reverseAndTrimIntersection,
     addIntersectionFiltersToInsight,
 } from "../drillDownUtil";
 import { IDrillDownContext, IDrillDownDefinition } from "../../../interfaces/Visualization";
+import { getReactEmbeddingCodeGenerator } from "../../../utils/embeddingCodeGenerator";
+import { IAreaChartBucketProps } from "@gooddata/sdk-ui-charts";
 
-export class AreaChartDescriptor extends BigChartDescriptor {
+export class AreaChartDescriptor extends BigChartDescriptor implements IVisualizationDescriptor {
     public getFactory(): PluggableVisualizationFactory {
         return (params) => new PluggableAreaChart(params);
     }
@@ -24,6 +37,33 @@ export class AreaChartDescriptor extends BigChartDescriptor {
         );
         return modifyBucketsAttributesForDrillDown(withFilters, drillDownContext.drillDefinition);
     }
+
+    public getEmbeddingCode = getReactEmbeddingCodeGenerator({
+        component: {
+            importType: "named",
+            name: "AreaChart",
+            package: "@gooddata/sdk-ui-charts",
+        },
+        insightToProps(insight): IAreaChartBucketProps {
+            const measuresBucket = insightBucket(insight, BucketNames.MEASURES);
+            const viewBucket = insightBucket(insight, BucketNames.VIEW);
+            const stackBucket = insightBucket(insight, BucketNames.STACK);
+
+            const measures = measuresBucket && bucketMeasures(measuresBucket);
+            const viewBy = viewBucket && bucketAttributes(viewBucket);
+            const stackBy = stackBucket && bucketAttribute(stackBucket);
+            const filters = insightFilters(insight);
+            const sortBy = insightSorts(insight);
+
+            return {
+                measures,
+                viewBy,
+                stackBy,
+                filters,
+                sortBy,
+            };
+        },
+    });
 
     private addFilters(source: IInsight, drillConfig: IDrillDownDefinition, event: IDrillEvent) {
         const cutIntersection = reverseAndTrimIntersection(drillConfig, event.drillContext.intersection);
