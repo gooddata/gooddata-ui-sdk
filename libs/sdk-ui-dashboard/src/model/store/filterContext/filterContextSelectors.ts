e3488b26f0e518576a17a3c291aba22b598d18df
Martin Plavek <martin.plavek@gooddata.com>
2022-03-24 12:45:20 +0100

    Productize filter context selectors
    - make all filterContext related selectors public
    - update tsdoc for api reference
    
    JIRA: RAIL-4091

--- libs/sdk-ui-dashboard/src/model/store/filterContext/filterContextSelectors.ts
+++ libs/sdk-ui-dashboard/src/model/store/filterContext/filterContextSelectors.ts
@@ -20,10 +20,15 @@ const selectSelf = createSelector(
 );
 
 /**
- * This selector returns original (stored) dashboard's filter context definition. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns original (stored) dashboard's filter context definition.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns {@link @gooddata/sdk-backend-spi#IFilterContextDefinition} or `undefined` if original filter context definition is not set.
+ *
+ * @public
  */
 export const selectOriginalFilterContextDefinition = createSelector(selectSelf, (filterContextState) => {
     invariant(
@@ -35,10 +40,15 @@ export const selectOriginalFilterContextDefinition = createSelector(selectSelf, 
 });
 
 /**
- * This selector returns original (stored) dashboard's filter context definition. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns original (stored) dashboard's filter context definition.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns an array of {@link @gooddata/sdk-backend-spi#FilterContextItem} or an empty array.
+ *
+ * @public
  */
 export const selectOriginalFilterContextFilters = createSelector(
     selectOriginalFilterContextDefinition,
@@ -46,10 +56,15 @@ export const selectOriginalFilterContextFilters = createSelector(
 );
 
 /**
- * This selector returns original (stored) dashboard's filter context filters. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns original (stored) dashboard's filter context filters.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns a {@link @gooddata/sdk-backend-spi#IFilterContextDefinition}
+ *
+ * @public
  */
 export const selectFilterContextDefinition = createSelector(selectSelf, (filterContextState) => {
     invariant(
@@ -63,16 +78,22 @@ export const selectFilterContextDefinition = createSelector(selectSelf, (filterC
 /**
  * Selects dashboard's filter context identity.
  *
+ * @remarks
  * The identity may be undefined in two circumstances:
  *
  * -  a new, yet unsaved dashboard; the filter context is saved together with the dashboard and after the
  *    save the identity will be known and added
+ *
  * -  export of an existing, saved dashboard; during the export the dashboard receives a temporary
  *    filter context that represents values of filters at the time the export was initiated - which may
  *    be different from what is saved in the filter context itself. that temporary context is not
  *    persistent and lives only for that particular export operation.
  *
- * @alpha
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns a {@link @gooddata/sdk-backend-spi#IDashboardObjectIdentity} or undefined, if the filter context identity is not set.
+ *
+ * @public
  */
 export const selectFilterContextIdentity = createSelector(selectSelf, (filterContextState) => {
     // this is intentional; want to fail fast when trying to access an optional identity of filter context \
@@ -88,7 +109,12 @@ export const selectFilterContextIdentity = createSelector(selectSelf, (filterCon
 /**
  * Selects list of display form metadata objects referenced by attribute filters.
  *
- * @alpha
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns an array of {@link @gooddata/sdk-backend-spi#IAttributeDisplayFormMetadataObject}
+ *
+ * @public
  */
 export const selectAttributeFilterDisplayForms = createSelector(selectSelf, (filterContextState) => {
     invariant(
@@ -102,7 +128,12 @@ export const selectAttributeFilterDisplayForms = createSelector(selectSelf, (fil
 /**
  * Selects map of display form metadata objects referenced by attribute filters.
  *
- * @alpha
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @returns a {@link ObjRefMap} of {@link @gooddata/sdk-backend-spi#IAttributeDisplayFormMetadataObject}
+ *
+ * @public
  */
 export const selectAttributeFilterDisplayFormsMap = createSelector(selectSelf, (filterContextState) => {
     invariant(
@@ -114,10 +145,13 @@ export const selectAttributeFilterDisplayFormsMap = createSelector(selectSelf, (
 });
 
 /**
- * This selector returns dashboard's filter context filters. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns dashboard's filter context filters.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectFilterContextFilters = createSelector(
     selectFilterContextDefinition,
@@ -125,10 +159,13 @@ export const selectFilterContextFilters = createSelector(
 );
 
 /**
- * This selector returns dashboard's filter context attribute filters. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns dashboard's filter context attribute filters.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectFilterContextAttributeFilters = createSelector(
     selectFilterContextFilters,
@@ -136,10 +173,13 @@ export const selectFilterContextAttributeFilters = createSelector(
 );
 
 /**
- * This selector returns dashboard's filter context date filter. It is expected that the selector is called only after the filter
- * context state is correctly initialized. Invocations before initialization lead to invariant errors.
+ * This selector returns dashboard's filter context date filter.
  *
- * @alpha
+ * @remarks
+ * It is expected that the selector is called only after the filter context state is correctly initialized.
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectFilterContextDateFilter = createSelector(
     selectFilterContextFilters,
@@ -149,7 +189,10 @@ export const selectFilterContextDateFilter = createSelector(
 /**
  * Creates a selector for selecting attribute filter by its displayForm {@link @gooddata/sdk-model#ObjRef}.
  *
- * @alpha
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectFilterContextAttributeFilterByDisplayForm = createMemoizedSelector((displayForm: ObjRef) =>
     createSelector(
@@ -167,7 +210,10 @@ export const selectFilterContextAttributeFilterByDisplayForm = createMemoizedSel
 /**
  * Creates a selector for selecting attribute filter by its localId.
  *
- * @alpha
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectFilterContextAttributeFilterByLocalId = createMemoizedSelector((localId: string) =>
     createSelector(selectFilterContextAttributeFilters, (attributeFilters) =>
@@ -178,8 +224,10 @@ export const selectFilterContextAttributeFilterByLocalId = createMemoizedSelecto
 /**
  * Creates a selector for selecting attribute filter index by its localId.
  *
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
  *
- * @alpha
+ * @public
  */
 export const selectFilterContextAttributeFilterIndexByLocalId = createMemoizedSelector((localId: string) =>
     createSelector(selectFilterContextAttributeFilters, (attributeFilters) =>
@@ -190,7 +238,10 @@ export const selectFilterContextAttributeFilterIndexByLocalId = createMemoizedSe
 /**
  * Creates a selector for selecting all descendants of the attribute filter with given localId.
  *
- * @alpha
+ * @remarks
+ * Invocations before initialization lead to invariant errors.
+ *
+ * @public
  */
 export const selectAttributeFilterDescendants = createMemoizedSelector((localId: string) =>
     createSelector(selectFilterContextAttributeFilters, (attributeFilters) => {
