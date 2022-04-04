50878a83a9ea88784df517af30085211d3badfb6
oleg-morozov <oleg.morozov@gooddata.com>
2022-04-04 14:47:30 +0200

    UI for no insights in export dialog
    JIRA: TNT-622

--- libs/sdk-ui-dashboard/src/presentation/scheduledEmail/DefaultScheduledEmailDialog/ScheduledMailDialogRenderer/Attachments/ScheduleDropdown.tsx
+++ libs/sdk-ui-dashboard/src/presentation/scheduledEmail/DefaultScheduledEmailDialog/ScheduledMailDialogRenderer/Attachments/ScheduleDropdown.tsx
@@ -1,11 +1,16 @@
 // (C) 2022 GoodData Corporation
 import * as React from "react";
 import cx from "classnames";
-import { Dropdown, Button } from "@gooddata/sdk-ui-kit";
-import { injectIntl, WrappedComponentProps } from "react-intl";
+import { Bubble, BubbleHoverTrigger, Dropdown, Button } from "@gooddata/sdk-ui-kit";
+import { FormattedMessage, injectIntl, WrappedComponentProps } from "react-intl";
 import { ITheme } from "@gooddata/sdk-backend-spi";
 import { withTheme } from "@gooddata/sdk-ui-theme-provider";
 import { DEFAULT_DROPDOWN_ALIGN_POINTS, DEFAULT_DROPDOWN_ZINDEX } from "../../constants";
+
+const ALIGN_POINTS = [
+    { align: "bc tc", offset: { x: 0, y: 0 } },
+    { align: "tc bc", offset: { x: 0, y: 0 } },
+];
 
 export interface IScheduleDropdownOwnProps {
     title: string;
@@ -17,6 +22,7 @@ export interface IScheduleDropdownOwnProps {
     contentComponent?: React.ReactNode;
     buttonClassName?: string;
     bodyClassName?: string;
+    buttonDisabled?: boolean;
 }
 
 type IScheduleDropdownProps = IScheduleDropdownOwnProps & WrappedComponentProps;
@@ -32,6 +38,7 @@ export const ScheduleDropdownComponent: React.FC<IScheduleDropdownProps> = (prop
         contentComponent,
         buttonClassName,
         bodyClassName,
+        buttonDisabled,
     } = props;
 
     const renderBody = (closeDropdown: () => void) => {
@@ -91,15 +98,30 @@ export const ScheduleDropdownComponent: React.FC<IScheduleDropdownProps> = (prop
             overlayZIndex={DEFAULT_DROPDOWN_ZINDEX}
             ignoreClicksOnByClass={[".schedule-dropdown-ignore-click"]}
             renderButton={({ toggleDropdown }) => (
-                <a
-                    className={cx("gd-schedule-dropdown-button", buttonClassName)}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={toggleDropdown}
-                >
-                    {iconComponent && <div className="gd-schedule-dropdown-button-icon">{iconComponent}</div>}
-                    {title}
-                </a>
+                <BubbleHoverTrigger showDelay={0} hideDelay={0}>
+                    <a
+                        className={cx("gd-schedule-dropdown-button", buttonClassName, {
+                            "gd-schedule-dropdown-button-disabled": !!buttonDisabled,
+                        })}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => {
+                            !buttonDisabled && toggleDropdown();
+                        }}
+                    >
+                        {iconComponent && (
+                            <div className="gd-schedule-dropdown-button-icon">{iconComponent}</div>
+                        )}
+                        {title}
+                    </a>
+                    {buttonDisabled && (
+                        <Bubble className="bubble-primary" alignPoints={ALIGN_POINTS}>
+                            <FormattedMessage
+                                id={"dialogs.schedule.email.attachment.select.disabled.message"}
+                            />
+                        </Bubble>
+                    )}
+                </BubbleHoverTrigger>
             )}
             renderBody={({ closeDropdown }) => renderBody(closeDropdown)}
         />
