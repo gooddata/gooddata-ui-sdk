35bfdb06eb24e092f9ba05e4573242cb883a8210
Matyas Kandl <matyas.kandl@gooddata.com>
2022-05-10 10:23:23 +0200

    Plugin harness - add GeoPushpin and PivotTable css
    - So PivotTable and GeoPushpin charts are rendered correctly when running plugin locally
    
    JIRA: RAIL-4205

--- tools/dashboard-plugin-template/src/harness/index.tsx
+++ tools/dashboard-plugin-template/src/harness/index.tsx
@@ -1,10 +1,12 @@
-// (C) 2019-2021 GoodData Corporation
+// (C) 2019-2022 GoodData Corporation
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "@gooddata/sdk-ui-charts/styles/css/main.css";
 import "@gooddata/sdk-ui-ext/styles/css/main.css";
 import "@gooddata/sdk-ui-dashboard/styles/css/main.css";
+import "@gooddata/sdk-ui-pivot/styles/css/main.css";
+import "@gooddata/sdk-ui-geo/styles/css/main.css";
 
 import { Root } from "./Root";
 
