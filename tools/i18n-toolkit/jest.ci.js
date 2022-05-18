258132e1daf3ab29ba400fee2490a9dcc9db6c86
hackerstanislav <hacker.stanislav@gmail.com>
2022-05-18 15:20:12 +0200

    FET-1033 - Add localisations validation to SDK-UI
    Repair of bin inside package.json to work with rush
    
    JIRA: FET-1033

--- tools/i18n-toolkit/jest.ci.js
+++ tools/i18n-toolkit/jest.ci.js
@@ -3,4 +3,6 @@ const ciBase = require("../../common/config/jest/jest.config.ci.base.js");
 
 module.exports = {
     ...ciBase,
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
