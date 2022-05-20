7b1261767321d6aba31d92693b6e9ca009041bef
Dan Homola <dan.homola@gooddata.com>
2022-05-20 14:29:11 +0200

    Fix pivot config in codegen
    Now the config logic is reused from PluggablePivotTable.
    This means the menu config is now correct on both bear and tiger.
    
    JIRA: RAIL-4253

--- libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pivotTable/pivotTableConfigFromInsight.ts
+++ libs/sdk-ui-ext/src/internal/components/pluggableVisualizations/pivotTable/pivotTableConfigFromInsight.ts
@@ -1,53 +1,36 @@
 // (C) 2022 GoodData Corporation
 import isEmpty from "lodash/isEmpty";
-import isNil from "lodash/isNil";
 import { IInsightDefinition, insightProperties } from "@gooddata/sdk-model";
-import { IColumnSizing, IPivotTableConfig, pivotTableMenuForCapabilities } from "@gooddata/sdk-ui-pivot";
+import { IPivotTableConfig } from "@gooddata/sdk-ui-pivot";
 import { IEmbeddingCodeContext } from "../../../interfaces/VisualizationDescriptor";
+import { createPivotTableConfig } from "./PluggablePivotTable";
+import { getColumnWidthsFromProperties } from "../../../utils/propertiesHelper";
 
 export function pivotTableConfigFromInsight(
     insight: IInsightDefinition,
     ctx: IEmbeddingCodeContext | undefined,
 ): IPivotTableConfig {
-    const properties = insightProperties(insight);
-    const controls = properties?.controls;
+    const baseConfig =
+        ctx?.settings && ctx.backend
+            ? createPivotTableConfig(
+                  { separators: ctx?.settings?.separators },
+                  "none",
+                  ctx.settings,
+                  ctx.backend.capabilities,
+                  getColumnWidthsFromProperties(insightProperties(insight)) ?? [],
+              )
+            : {};
 
-    const columnSizingFromControls = controls && getColumnSizingFromControls(controls, ctx);
-    const autoResizeAllSizing: IColumnSizing = { defaultWidth: "autoresizeAll" };
-
-    // use sizing from controls if specified, otherwise use autosize if the relevant FF is on
-    const columnSizing =
-        columnSizingFromControls || (ctx.settings?.enableTableColumnsAutoResizing && autoResizeAllSizing);
-
-    const columnSizingProp = !isEmpty(columnSizing) ? { columnSizing } : {};
-
-    const menuConfig = ctx?.backend && pivotTableMenuForCapabilities(ctx.backend.capabilities);
-    const menuProp = !isEmpty(menuConfig) ? { menu: menuConfig } : {};
-
-    const separatorsConfig = ctx?.settings?.separators;
-    const separatorsProp = !isEmpty(separatorsConfig) ? { separators: separatorsConfig } : {};
+    const columnSizingProp = !isEmpty(baseConfig.columnSizing)
+        ? { columnSizing: baseConfig.columnSizing }
+        : {};
+    const menuProp = !isEmpty(baseConfig.menu) ? { menu: baseConfig.menu } : {};
+    const separatorsProp = !isEmpty(baseConfig.separators) ? { separators: baseConfig.separators } : {};
 
     return {
         ...columnSizingProp,
         ...menuProp,
         ...separatorsProp,
-        // the user can fill the rest on their own later
-    };
-}
-
-function getColumnSizingFromControls(
-    controls: Record<string, any>,
-    ctx: IEmbeddingCodeContext | undefined,
-): IColumnSizing | undefined {
-    const { columnWidths } = controls;
-    const columnWidthsProp = !isEmpty(columnWidths) ? { columnWidths } : {};
-
-    const growToFitConfig = ctx?.settings?.enableTableColumnsGrowToFit;
-    const growToFitProp = !isNil(growToFitConfig) ? { growToFit: growToFitConfig } : {};
-
-    return {
-        ...columnWidthsProp,
-        ...growToFitProp,
         // the user can fill the rest on their own later
     };
 }
