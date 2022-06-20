a4eaaaaf77ff5c5cbbc9ff3e245957445c3ec522
Dan Homola <dan.homola@gooddata.com>
2022-06-20 12:18:29 +0200

    Fix the selected widget behavior
    * use proper memoization
    * use adequate ref comparison
    * only make item selected if it is selectable in the first place
    
    JIRA: RAIL-4159

--- libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemContent.tsx
+++ libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemContent.tsx
@@ -1,15 +1,13 @@
 // (C) 2020-2022 GoodData Corporation
-import React, { forwardRef } from "react";
-import isEqual from "lodash/isEqual";
-import noop from "lodash/noop";
+import React, { forwardRef, useCallback } from "react";
 import cx from "classnames";
+import { areObjRefsEqual, ObjRef } from "@gooddata/sdk-model";
 import {
     useDashboardDispatch,
     useDashboardSelector,
     uiActions,
     selectSelectedWidgetRef,
 } from "../../../model";
-import { ObjRef } from "@gooddata/sdk-model";
 
 interface IDashboardItemContentProps {
     className?: string;
@@ -22,14 +20,17 @@ export const DashboardItemContent = forwardRef<HTMLDivElement, IDashboardItemCon
     function DashboardItemContent({ children, className, isSelectable, objRef }, ref) {
         const selectedWidget = useDashboardSelector(selectSelectedWidgetRef);
         const dispatch = useDashboardDispatch();
-
-        const onClick = isSelectable ? () => objRef && dispatch(uiActions.selectWidget(objRef)) : noop;
+        const onClick = useCallback(() => {
+            if (isSelectable && objRef) {
+                dispatch(uiActions.selectWidget(objRef));
+            }
+        }, [isSelectable, objRef, dispatch]);
 
         return (
             <div
                 className={cx("dash-item-content", className, {
                     "is-selectable": isSelectable,
-                    "is-selected": isEqual(selectedWidget, objRef),
+                    "is-selected": isSelectable && selectedWidget && areObjRefsEqual(selectedWidget, objRef),
                 })}
                 ref={ref}
                 onClick={onClick}
