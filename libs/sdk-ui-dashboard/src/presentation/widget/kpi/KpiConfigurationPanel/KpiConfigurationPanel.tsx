8eaae8f29c6ec14b0590263257b90dedcc3c11c0
Dan Homola <dan.homola@gooddata.com>
2022-07-14 11:54:25 +0200

    Migrate Kpi config messages
    JIRA: RAIL-4158

--- libs/sdk-ui-dashboard/src/presentation/widget/kpi/KpiConfigurationPanel/KpiConfigurationPanel.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/kpi/KpiConfigurationPanel/KpiConfigurationPanel.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import cx from "classnames";
-import { IKpiWidget } from "@gooddata/sdk-model";
+import { IKpiWidget, widgetRef } from "@gooddata/sdk-model";
+import { useBackendStrict, useWorkspaceStrict, useCancelablePromise } from "@gooddata/sdk-ui";
 import { Typography } from "@gooddata/sdk-ui-kit";
 import noop from "lodash/noop";
 
@@ -11,6 +12,7 @@ import { KpiComparison } from "./KpiComparison/KpiComparison";
 import { KpiWidgetDateDatasetFilter } from "./KpiWidgetDateDatasetFilter";
 import { KpiMetricDropdown } from "./KpiMetricDropdown/KpiMetricDropdown";
 import { KpiConfigurationPanelHeader } from "./KpiConfigurationPanelHeader";
+import { KpiConfigurationMessages } from "./KpiConfigurationMessages";
 
 interface IKpiConfigurationPanelProps {
     widget: IKpiWidget;
@@ -19,8 +21,30 @@ interface IKpiConfigurationPanelProps {
 export const KpiConfigurationPanel: React.FC<IKpiConfigurationPanelProps> = (props) => {
     const { widget } = props;
     const metric = widget.kpi.metric;
+
+    const backend = useBackendStrict();
+    const workspace = useWorkspaceStrict();
+
+    const { result: numberOfAlerts, status } = useCancelablePromise(
+        {
+            promise: widgetRef(widget)
+                ? async () => {
+                      const res = await backend
+                          .workspace(workspace)
+                          .dashboards()
+                          .getWidgetAlertsCountForWidgets([widgetRef(widget)]);
+
+                      return res[0]?.alertCount;
+                  }
+                : null,
+        },
+        [backend, workspace, widget],
+    );
+
+    const isNumOfAlertsLoaded = status === "success";
+
     const configurationCategoryClasses = cx("configuration-category", {
-        "s-widget-alerts-information-loaded": false, // isNumOfAlertsLoaded, // TODO
+        "s-widget-alerts-information-loaded": isNumOfAlertsLoaded,
     });
 
     const filtersClasses = cx({ "is-disabled": !metric });
@@ -34,7 +58,7 @@ export const KpiConfigurationPanel: React.FC<IKpiConfigurationPanelProps> = (pro
             />
             <div className="configuration-panel">
                 <div className={configurationCategoryClasses}>
-                    {/* {this.renderMessages()} */}
+                    <KpiConfigurationMessages numberOfAlerts={numberOfAlerts} />
 
                     <Typography tagName="h3">
                         <FormattedMessage id="configurationPanel.measure" />
