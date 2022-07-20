cf4910e278cf803db4eea777d657ff91afb8619f
Dan Homola <dan.homola@gooddata.com>
2022-07-20 17:25:10 +0200

    Rework AttributeFilterConfiguration
    Adds optimistic updates to improve UX.
    
    JIRA: RAIL-4158

--- libs/sdk-ui-dashboard/src/presentation/widget/common/configuration/AttributeFilterConfiguration.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/common/configuration/AttributeFilterConfiguration.tsx
@@ -1,23 +1,11 @@
 // (C) 2022 GoodData Corporation
-import React, { useCallback, useMemo } from "react";
-import {
-    isDashboardAttributeFilterReference,
-    isInsightWidget,
-    IWidget,
-    ObjRef,
-    objRefToString,
-    widgetRef,
-} from "@gooddata/sdk-model";
+import React from "react";
+import { IWidget, objRefToString } from "@gooddata/sdk-model";
 import invariant from "ts-invariant";
 import {
-    ignoreFilterOnInsightWidget,
-    ignoreFilterOnKpiWidget,
     selectAllCatalogAttributesMap,
     selectAttributeFilterDisplayFormsMap,
     selectFilterContextAttributeFilters,
-    unignoreFilterOnInsightWidget,
-    unignoreFilterOnKpiWidget,
-    useDashboardDispatch,
     useDashboardSelector,
 } from "../../../../model";
 import { AttributeFilterConfigurationItem } from "./AttributeFilterConfigurationItem";
@@ -32,37 +20,6 @@ export const AttributeFilterConfiguration: React.FC<IAttributeFilterConfiguratio
     const dfMap = useDashboardSelector(selectAttributeFilterDisplayFormsMap);
     const attrMap = useDashboardSelector(selectAllCatalogAttributesMap);
 
-    const notAppliedFiltersRefs = useMemo(() => {
-        return widget.ignoreDashboardFilters.map((ref) => {
-            return isDashboardAttributeFilterReference(ref) ? ref.displayForm : ref.dataSet;
-        });
-    }, [widget]);
-
-    const dispatch = useDashboardDispatch();
-
-    const ignoredDisplayForms = widget.ignoreDashboardFilters
-        .filter(isDashboardAttributeFilterReference)
-        .map((ignored) => dfMap.get(ignored.displayForm));
-
-    const handleIgnoreChanged = useCallback(
-        (filterDisplayFormRef: ObjRef, ignored: boolean) => {
-            if (isInsightWidget(widget)) {
-                if (ignored) {
-                    dispatch(unignoreFilterOnInsightWidget(widgetRef(widget), filterDisplayFormRef));
-                } else {
-                    dispatch(ignoreFilterOnInsightWidget(widgetRef(widget), filterDisplayFormRef));
-                }
-            } else {
-                if (ignored) {
-                    dispatch(unignoreFilterOnKpiWidget(widgetRef(widget), filterDisplayFormRef));
-                } else {
-                    dispatch(ignoreFilterOnKpiWidget(widgetRef(widget), filterDisplayFormRef));
-                }
-            }
-        },
-        [dispatch, widget],
-    );
-
     return (
         <div>
             {attributeFilters.map((filter) => {
@@ -72,18 +29,11 @@ export const AttributeFilterConfiguration: React.FC<IAttributeFilterConfiguratio
                 const attribute = attrMap.get(displayForm.attribute);
                 invariant(attribute, "Inconsistent state in AttributeFilterConfiguration");
 
-                const isIgnored = ignoredDisplayForms.includes(displayForm);
-
                 return (
                     <AttributeFilterConfigurationItem
                         key={objRefToString(displayForm.ref)}
                         displayFormRef={displayForm.ref}
-                        attributeRef={displayForm.attribute}
-                        isIgnored={isIgnored}
                         title={attribute.attribute.title}
-                        notAppliedFiltersRefs={notAppliedFiltersRefs}
-                        recentlyCheckedFilters={[]} // TODO
-                        onIgnoreChange={handleIgnoreChanged}
                         widget={widget}
                     />
                 );
--- libs/sdk-ui-dashboard/src/presentation/widget/common/configuration/AttributeFilterConfigurationItem.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/common/configuration/AttributeFilterConfigurationItem.tsx
@@ -1,44 +1,136 @@
 // (C) 2022 GoodData Corporation
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import cx from "classnames";
 import { stringUtils } from "@gooddata/util";
 import { IAlignPoint, ShortenedText } from "@gooddata/sdk-ui-kit";
-import { areObjRefsEqual, isInsightWidget, IWidget, ObjRef, objRefToString } from "@gooddata/sdk-model";
+import {
+    areObjRefsEqual,
+    isDashboardAttributeFilterReference,
+    isInsightWidget,
+    IWidget,
+    ObjRef,
+    objRefToString,
+    widgetRef,
+} from "@gooddata/sdk-model";
+
+import {
+    ignoreFilterOnInsightWidget,
+    ignoreFilterOnKpiWidget,
+    selectAttributeFilterDisplayFormsMap,
+    unignoreFilterOnInsightWidget,
+    unignoreFilterOnKpiWidget,
+    useDashboardCommandProcessing,
+    useDashboardSelector,
+} from "../../../../model";
+import { safeSerializeObjRef } from "../../../../_staging/metadata/safeSerializeObjRef";
 
 const tooltipAlignPoints: IAlignPoint[] = [{ align: "cl cr", offset: { x: -20, y: 0 } }];
 
 interface IAttributeFilterConfigurationItemProps {
     widget: IWidget;
-    recentlyCheckedFilters: ObjRef[]; // TODO what is this for?
-    notAppliedFiltersRefs: ObjRef[];
-    onIgnoreChange: (filterDisplayFormRef: ObjRef, ignored: boolean) => void;
-    isIgnored: boolean;
-    attributeRef: ObjRef;
     displayFormRef: ObjRef;
     title: string;
 }
 
 export const AttributeFilterConfigurationItem: React.FC<IAttributeFilterConfigurationItemProps> = (props) => {
-    const {
-        widget,
-        notAppliedFiltersRefs,
-        recentlyCheckedFilters,
-        onIgnoreChange,
-        isIgnored,
-        attributeRef,
-        displayFormRef,
-        title,
-    } = props;
+    const { widget, displayFormRef, title } = props;
 
-    const showLoadingIndicator = recentlyCheckedFilters.some((recentRef) =>
-        areObjRefsEqual(recentRef, displayFormRef),
+    const dfMap = useDashboardSelector(selectAttributeFilterDisplayFormsMap);
+
+    const [isApplied, setIsApplied] = useState(
+        () =>
+            !widget.ignoreDashboardFilters.some((reference) => {
+                if (!isDashboardAttributeFilterReference(reference)) {
+                    return false;
+                }
+                const df = dfMap.get(reference.displayForm);
+                return areObjRefsEqual(df?.ref, displayFormRef);
+            }),
+    );
+    const [status, setStatus] = useState<"ok" | "error" | "loading">("ok");
+
+    const ref = widgetRef(widget);
+
+    const { run: ignoreKpiFilter } = useDashboardCommandProcessing({
+        commandCreator: ignoreFilterOnKpiWidget,
+        successEvent: "GDC.DASH/EVT.KPI_WIDGET.FILTER_SETTINGS_CHANGED",
+        errorEvent: "GDC.DASH/EVT.COMMAND.FAILED",
+        onBeforeRun: () => {
+            setIsApplied(false);
+        },
+    });
+
+    const { run: unignoreKpiFilter } = useDashboardCommandProcessing({
+        commandCreator: unignoreFilterOnKpiWidget,
+        successEvent: "GDC.DASH/EVT.KPI_WIDGET.FILTER_SETTINGS_CHANGED",
+        errorEvent: "GDC.DASH/EVT.COMMAND.FAILED",
+        onBeforeRun: () => {
+            setIsApplied(true);
+            setStatus("loading");
+        },
+        onError: () => {
+            setStatus("error");
+        },
+        onSuccess: (_command) => {
+            setStatus("ok");
+        },
+    });
+
+    const { run: ignoreInsightFilter } = useDashboardCommandProcessing({
+        commandCreator: ignoreFilterOnInsightWidget,
+        successEvent: "GDC.DASH/EVT.INSIGHT_WIDGET.FILTER_SETTINGS_CHANGED",
+        errorEvent: "GDC.DASH/EVT.COMMAND.FAILED",
+        onBeforeRun: () => {
+            setIsApplied(false);
+        },
+    });
+
+    const { run: unignoreInsightFilter } = useDashboardCommandProcessing({
+        commandCreator: unignoreFilterOnInsightWidget,
+        successEvent: "GDC.DASH/EVT.INSIGHT_WIDGET.FILTER_SETTINGS_CHANGED",
+        errorEvent: "GDC.DASH/EVT.COMMAND.FAILED",
+        onBeforeRun: () => {
+            setIsApplied(true);
+            setStatus("loading");
+        },
+        onError: () => {
+            setStatus("error");
+        },
+        onSuccess: (_command) => {
+            setStatus("ok");
+        },
+    });
+
+    const handleIgnoreChanged = useCallback(
+        (ignored: boolean) => {
+            if (isInsightWidget(widget)) {
+                if (ignored) {
+                    unignoreInsightFilter(ref, displayFormRef);
+                } else {
+                    ignoreInsightFilter(ref, displayFormRef);
+                }
+            } else {
+                if (ignored) {
+                    unignoreKpiFilter(ref, displayFormRef);
+                } else {
+                    ignoreKpiFilter(ref, displayFormRef);
+                }
+            }
+        },
+        [
+            isInsightWidget(widget),
+            safeSerializeObjRef(displayFormRef),
+            safeSerializeObjRef(ref),
+            ignoreInsightFilter,
+            ignoreKpiFilter,
+            unignoreInsightFilter,
+            unignoreKpiFilter,
+        ],
     );
 
-    const showError =
-        notAppliedFiltersRefs.some((notAppliedRef) => areObjRefsEqual(notAppliedRef, attributeRef)) &&
-        !isIgnored &&
-        !showLoadingIndicator;
+    const isError = status === "error";
+    const isLoading = status === "loading";
 
     const classNames = cx(
         "s-attribute-filter-by-item",
@@ -47,7 +139,7 @@ export const AttributeFilterConfigurationItem: React.FC<IAttributeFilterConfigur
         "filter-by-item",
         "attribute-filter-by-item",
         {
-            "attribute-filter-error": showError,
+            "attribute-filter-error": isError,
         },
     );
 
@@ -60,17 +152,17 @@ export const AttributeFilterConfigurationItem: React.FC<IAttributeFilterConfigur
                     id={uniqueKey}
                     type="checkbox"
                     className="input-checkbox"
-                    checked={!isIgnored}
-                    onChange={(e) => onIgnoreChange(displayFormRef, e.target.checked)}
+                    checked={isApplied}
+                    onChange={(e) => handleIgnoreChanged(e.target.checked)}
                 />
                 <span className="input-label-text">
                     <ShortenedText tooltipAlignPoints={tooltipAlignPoints} tagName="span" className="title">
                         {title}
                     </ShortenedText>
                 </span>
-                {!!showLoadingIndicator && <div className="gd-spinner small" />}
+                {isLoading && <div className="gd-spinner small" />}
             </label>
-            {!!showError && (
+            {!!isError && (
                 <div className="gd-message error s-not-applied-attribute-filter">
                     {isInsightWidget(widget) ? (
                         <FormattedMessage
