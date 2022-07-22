d6b813b3356e1f072b7a14e87a63349bf225a387
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2022-07-22 16:53:01 +0200

    Turn on FF to true for development

--- libs/sdk-backend-tiger/src/backend/uiFeatures.ts
+++ libs/sdk-backend-tiger/src/backend/uiFeatures.ts
@@ -36,7 +36,7 @@ export const DefaultFeatureFlags: ITigerFeatureFlags = {
     enableKPIDashboardDeleteFilterButton: false,
     // disable edit mode in gdc-dashboards during development
     dashboardEditModeDevRollout: false,
-    enablePantherTheming: false,
+    enablePantherTheming: true, // TODO INE just for development
 };
 
 export const FeatureFlagsValues = {
