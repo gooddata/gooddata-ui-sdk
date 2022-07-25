a4f75671521e80f5c7ba9644ee70b05f91f39239
scavnickyj <jakub.scavnicky@gooddata.com>
2022-07-25 11:26:54 +0200

    Regenerate client

--- libs/api-client-tiger/src/generated/auth-json-api/api.ts
+++ libs/api-client-tiger/src/generated/auth-json-api/api.ts
@@ -84,6 +84,19 @@ export interface Features {
 /**
  *
  * @export
+ * @interface Invitation
+ */
+export interface Invitation {
+    /**
+     *
+     * @type {string}
+     * @memberof Invitation
+     */
+    email: string;
+}
+/**
+ *
+ * @export
  * @interface LiveFeatureFlagConfiguration
  */
 export interface LiveFeatureFlagConfiguration {
@@ -274,6 +287,172 @@ export interface Telemetry {
      * @memberof Telemetry
      */
     userHash: string;
+}
+
+/**
+ * ActionsApi - axios parameter creator
+ * @export
+ */
+export const ActionsApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         *
+         * @summary Puts a new invitation requirement into the invitation generator queue.
+         * @param {Invitation} invitation
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        processInvitation: async (
+            invitation: Invitation,
+            options: AxiosRequestConfig = {},
+        ): Promise<RequestArgs> => {
+            // verify required parameter 'invitation' is not null or undefined
+            assertParamExists("processInvitation", "invitation", invitation);
+            const localVarPath = `/api/v1/actions/invite`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+            const localVarRequestOptions = { method: "POST", ...baseOptions, ...options };
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            localVarHeaderParameter["Content-Type"] = "application/json";
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {
+                ...localVarHeaderParameter,
+                ...headersFromBaseOptions,
+                ...options.headers,
+            };
+            const needsSerialization =
+                typeof invitation !== "string" ||
+                localVarRequestOptions.headers["Content-Type"] === "application/json";
+            localVarRequestOptions.data = needsSerialization
+                ? JSON.stringify(invitation !== undefined ? invitation : {})
+                : invitation || "";
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    };
+};
+
+/**
+ * ActionsApi - functional programming interface
+ * @export
+ */
+export const ActionsApiFp = function (configuration?: Configuration) {
+    const localVarAxiosParamCreator = ActionsApiAxiosParamCreator(configuration);
+    return {
+        /**
+         *
+         * @summary Puts a new invitation requirement into the invitation generator queue.
+         * @param {Invitation} invitation
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async processInvitation(
+            invitation: Invitation,
+            options?: AxiosRequestConfig,
+        ): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.processInvitation(invitation, options);
+            return createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration);
+        },
+    };
+};
+
+/**
+ * ActionsApi - factory interface
+ * @export
+ */
+export const ActionsApiFactory = function (
+    configuration?: Configuration,
+    basePath?: string,
+    axios?: AxiosInstance,
+) {
+    const localVarFp = ActionsApiFp(configuration);
+    return {
+        /**
+         *
+         * @summary Puts a new invitation requirement into the invitation generator queue.
+         * @param {ActionsApiProcessInvitationRequest} requestParameters Request parameters.
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        processInvitation(
+            requestParameters: ActionsApiProcessInvitationRequest,
+            options?: AxiosRequestConfig,
+        ): AxiosPromise<void> {
+            return localVarFp
+                .processInvitation(requestParameters.invitation, options)
+                .then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * ActionsApi - interface
+ * @export
+ * @interface ActionsApi
+ */
+export interface ActionsApiInterface {
+    /**
+     *
+     * @summary Puts a new invitation requirement into the invitation generator queue.
+     * @param {ActionsApiProcessInvitationRequest} requestParameters Request parameters.
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ActionsApiInterface
+     */
+    processInvitation(
+        requestParameters: ActionsApiProcessInvitationRequest,
+        options?: AxiosRequestConfig,
+    ): AxiosPromise<void>;
+}
+
+/**
+ * Request parameters for processInvitation operation in ActionsApi.
+ * @export
+ * @interface ActionsApiProcessInvitationRequest
+ */
+export interface ActionsApiProcessInvitationRequest {
+    /**
+     *
+     * @type {Invitation}
+     * @memberof ActionsApiProcessInvitation
+     */
+    readonly invitation: Invitation;
+}
+
+/**
+ * ActionsApi - object-oriented interface
+ * @export
+ * @class ActionsApi
+ * @extends {BaseAPI}
+ */
+export class ActionsApi extends BaseAPI implements ActionsApiInterface {
+    /**
+     *
+     * @summary Puts a new invitation requirement into the invitation generator queue.
+     * @param {ActionsApiProcessInvitationRequest} requestParameters Request parameters.
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ActionsApi
+     */
+    public processInvitation(
+        requestParameters: ActionsApiProcessInvitationRequest,
+        options?: AxiosRequestConfig,
+    ) {
+        return ActionsApiFp(this.configuration)
+            .processInvitation(requestParameters.invitation, options)
+            .then((request) => request(this.axios, this.basePath));
+    }
 }
 
 /**
