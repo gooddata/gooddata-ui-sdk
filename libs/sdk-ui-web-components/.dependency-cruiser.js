6fe900a4aec093db9774f289c00d6008cf517334
Andy Chumak <andrii.chumak@gooddata.com>
2022-07-27 17:16:36 +0200

    Loosen dependency cruiser rules for the sdk-ui-web-components
    JIRA: RAIL-4318

--- libs/sdk-ui-web-components/.dependency-cruiser.js
+++ libs/sdk-ui-web-components/.dependency-cruiser.js
@@ -1,7 +1,11 @@
 const depCruiser = require("../../common/config/dep-cruiser/default.config");
 
 options = {
-    forbidden: [...depCruiser.DefaultRules, ...depCruiser.DefaultSdkRules, ...depCruiser.PublicLibraryRules],
+    // Excluding PublicLibraryRules, as we are importing CSS files to the build
+    forbidden: [
+        ...depCruiser.DefaultRules,
+        ...depCruiser.DefaultSdkRules /*, ...depCruiser.PublicLibraryRules*/,
+    ],
     options: depCruiser.DefaultOptions,
 };
 
