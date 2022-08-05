68b5c04f087bbb4d83a86bd83f1cb42420be8827
Petr Benes <petr.benes@gooddata.com>
2022-08-05 13:40:38 +0200

    finish rewriting of withLoading

--- libs/sdk-ui/src/execution/tests/withLoading.test.tsx
+++ libs/sdk-ui/src/execution/tests/withLoading.test.tsx
@@ -1,7 +1,7 @@
 // (C) 2019-2022 GoodData Corporation
 import React from "react";
 import { withExecutionLoading, IWithExecutionLoading, WithLoadingResult } from "../withExecutionLoading";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { IDummyPromise, createDummyPromise } from "../../base/react/tests/toolkit";
 import { DataViewFacade } from "../../base";
 import { dummyDataView } from "@gooddata/sdk-backend-mockingbird";
@@ -19,10 +19,15 @@ const renderEnhancedComponent = <T, E>(
         const { result, error, reload, isLoading } = props;
         return (
             <div>
-                <button className="Refetch" onClick={reload} />
+                <button onClick={reload}>Refetch</button>
                 {isLoading && <div>Loading</div>}
-                {result && <div>{result}</div>}
-                {error && <div>{error.message}</div>}
+                {result && <div>Result</div>}
+                {error && (
+                    <div>
+                        <span>Error</span>
+                        {error?.cause?.message}
+                    </div>
+                )}
             </div>
         );
     };
@@ -38,88 +43,87 @@ const renderEnhancedComponent = <T, E>(
 
 describe("withLoading", () => {
     const RESULT = EmptyDataViewFacade;
-    //    const ERROR = new Error("ERROR");
+    const ERROR = new Error("ERROR_MESSAGE");
 
-    it.only("should start loading immediately and inject isLoading prop", () => {
+    it("should start loading immediately and inject isLoading prop", () => {
         const { getByText } = renderEnhancedComponent({ result: RESULT, delay: 100 });
         expect(getByText("Loading")).toBeInTheDocument();
     });
 
-    //    it("should not start loading immediately if loadOnMount is set to false", () => {
-    //        const wrapper = renderEnhancedComponent({ result: RESULT, delay: 100 }, { loadOnMount: false });
-    //        expect(wrapper.prop("isLoading")).toBe(false);
-    //    });
-    //
-    //    it("should stop loading when promise is resolved and inject result prop", async () => {
-    //        const wrapper = renderEnhancedComponent({ result: RESULT, delay: 100 });
-    //        await createDummyPromise({ delay: 150 });
-    //        expect(wrapper.prop("isLoading")).toBe(false);
-    //        expect(wrapper.prop("result")).toBe(RESULT);
-    //    });
-    //
-    //    it("should stop loading when promise is rejected and inject error prop", async () => {
-    //        const wrapper = renderEnhancedComponent({ willResolve: false, error: ERROR, delay: 100 });
-    //        await createDummyPromise({ delay: 150 });
-    //        expect(wrapper.prop("isLoading")).toBe(false);
-    //        expect(wrapper.prop("error").cause).toBe(ERROR);
-    //    });
-    //
-    //    it("should inject fetch handler", () => {
-    //        const wrapper = renderEnhancedComponent({ delay: 100 });
-    //        expect(wrapper.prop("reload")).toEqual(expect.any(Function));
-    //    });
-    //
-    //    it("should start loading again after invoking injected fetch function", async () => {
-    //        const wrapper = renderEnhancedComponent({ delay: 100 });
-    //        await createDummyPromise({ delay: 150 });
-    //        wrapper.dive().find(".Refetch").simulate("click");
-    //
-    //        expect(wrapper.prop("isLoading")).toBe(true);
-    //    });
-    //
-    //    it("should invoke onLoadingStart, onLoadingChanged and onLoadingFinish events", async () => {
-    //        const onLoadingStart = jest.fn();
-    //        const onLoadingChanged = jest.fn();
-    //        const onLoadingFinish = jest.fn();
-    //
-    //        renderEnhancedComponent(
-    //            { delay: 100, result: EmptyDataViewFacade },
-    //            {
-    //                events: {
-    //                    onLoadingChanged,
-    //                    onLoadingFinish,
-    //                    onLoadingStart,
-    //                },
-    //            },
-    //        );
-    //
-    //        await createDummyPromise({ delay: 150 });
-    //
-    //        expect(onLoadingStart).toBeCalledTimes(1);
-    //        expect(onLoadingChanged).toBeCalledTimes(2);
-    //        expect(onLoadingFinish).toBeCalledTimes(1);
-    //    });
-    //
-    //    it("should invoke onLoadingStart, onLoadingChanged and onError events", async () => {
-    //        const onLoadingStart = jest.fn();
-    //        const onLoadingChanged = jest.fn();
-    //        const onError = jest.fn();
-    //
-    //        renderEnhancedComponent(
-    //            { willResolve: false, delay: 100 },
-    //            {
-    //                events: {
-    //                    onError,
-    //                    onLoadingChanged,
-    //                    onLoadingStart,
-    //                },
-    //            },
-    //        );
-    //
-    //        await createDummyPromise({ delay: 150 });
-    //
-    //        expect(onLoadingStart).toBeCalledTimes(1);
-    //        expect(onLoadingChanged).toBeCalledTimes(2);
-    //        expect(onError).toBeCalledTimes(1);
-    //    });
+    it("should not start loading immediately if loadOnMount is set to false", () => {
+        const { queryByText } = renderEnhancedComponent(
+            { result: RESULT, delay: 100 },
+            { loadOnMount: false },
+        );
+        expect(queryByText("Loading")).not.toBeInTheDocument();
+    });
+
+    it("should stop loading when promise is resolved and inject result prop", async () => {
+        const { queryByText } = renderEnhancedComponent({ result: RESULT, delay: 100 });
+        await createDummyPromise({ delay: 150 });
+        expect(queryByText("Loading")).not.toBeInTheDocument();
+        expect(queryByText("Result")).toBeInTheDocument();
+    });
+
+    it("should stop loading when promise is rejected and inject error prop", async () => {
+        const { queryByText } = renderEnhancedComponent({ willResolve: false, error: ERROR, delay: 100 });
+        await createDummyPromise({ delay: 150 });
+        expect(queryByText("Loading")).not.toBeInTheDocument();
+        expect(queryByText("ERROR_MESSAGE")).toBeInTheDocument();
+        expect(queryByText("Error")).toBeInTheDocument();
+    });
+
+    it("should start loading again after invoking injected fetch function", async () => {
+        const { getByText } = renderEnhancedComponent({ delay: 100 });
+        await createDummyPromise({ delay: 150 });
+        fireEvent.click(getByText("Refetch"));
+
+        expect(getByText("Loading")).toBeInTheDocument();
+    });
+
+    it("should invoke onLoadingStart, onLoadingChanged and onLoadingFinish events", async () => {
+        const onLoadingStart = jest.fn();
+        const onLoadingChanged = jest.fn();
+        const onLoadingFinish = jest.fn();
+
+        renderEnhancedComponent(
+            { delay: 100, result: EmptyDataViewFacade },
+            {
+                events: {
+                    onLoadingChanged,
+                    onLoadingFinish,
+                    onLoadingStart,
+                },
+            },
+        );
+
+        await createDummyPromise({ delay: 150 });
+
+        expect(onLoadingStart).toBeCalledTimes(1);
+        expect(onLoadingChanged).toBeCalledTimes(2);
+        expect(onLoadingFinish).toBeCalledTimes(1);
+    });
+
+    it("should invoke onLoadingStart, onLoadingChanged and onError events", async () => {
+        const onLoadingStart = jest.fn();
+        const onLoadingChanged = jest.fn();
+        const onError = jest.fn();
+
+        renderEnhancedComponent(
+            { willResolve: false, delay: 100 },
+            {
+                events: {
+                    onError,
+                    onLoadingChanged,
+                    onLoadingStart,
+                },
+            },
+        );
+
+        await createDummyPromise({ delay: 150 });
+
+        expect(onLoadingStart).toBeCalledTimes(1);
+        expect(onLoadingChanged).toBeCalledTimes(2);
+        expect(onError).toBeCalledTimes(1);
+    });
 });
