daa3a822dbf1306d41a469baae18879a9e74bf83
Zdenek Krejci <krejzd@gmail.com>
2022-08-12 09:21:19 +0200

    Fix dropdown icons in drill config panel
    RAIL-4405

---
+++ libs/sdk-ui-dashboard/src/presentation/widget/insight/configuration/DrillOriginItem.tsx
@@ -0,0 +1,46 @@
+// (C) 2019-2022 GoodData Corporation
+import React from "react";
+import { Button } from "@gooddata/sdk-ui-kit";
+import { selectEnableRenamingMeasureToMetric, useDashboardSelector } from "../../../../model";
+
+export interface IDrillOriginItemProps {
+    title: string;
+    type: string;
+    localIdentifier: string;
+    onDelete: (localIdentifier: string) => void;
+    isDateAttribute: boolean;
+}
+
+export const DrillOriginItem: React.FunctionComponent<IDrillOriginItemProps> = ({
+    localIdentifier,
+    onDelete,
+    title,
+    type,
+    isDateAttribute,
+}) => {
+    const onClick = () => {
+        onDelete(localIdentifier);
+    };
+    const shouldRenameMeasureToMetric = useDashboardSelector(selectEnableRenamingMeasureToMetric);
+
+    const iconType = isDateAttribute
+        ? "date"
+        : shouldRenameMeasureToMetric && type === "measure"
+        ? "metric"
+        : type;
+    const iconClassName = `dm-button-icon dm-button-icon-${iconType}`;
+    return (
+        <div className="gd-drill-config-measure-item">
+            <div className={iconClassName} />
+            <div className="dm-button-title s-drill-config-item-title" title={title}>
+                {title}
+            </div>
+            <div className="dm-button-delete-wrapper">
+                <Button
+                    className="drill-config-item-delete gd-button-link gd-button-icon-only gd-icon-cross s-drill-config-item-delete"
+                    onClick={onClick}
+                />
+            </div>
+        </div>
+    );
+};
--- libs/sdk-ui-dashboard/src/presentation/widget/insight/configuration/DrillOriginSelector/DrillAttributeSelectorItem.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/insight/configuration/DrillOriginSelector/DrillAttributeSelectorItem.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import cx from "classnames";
 import { IAvailableDrillTargetAttribute } from "@gooddata/sdk-ui";
+import { useDashboardSelector, selectCatalogDateDatasets } from "../../../../../model";
+import { areObjRefsEqual } from "@gooddata/sdk-model";
 
 export interface IDrillAttributeSelectorItemStateProps {
     isDateAttribute: boolean;
@@ -16,19 +18,26 @@ export interface IFilterDrillAttributeSelectorItemProps {
 export const DrillAttributeSelectorItem: React.FunctionComponent<IFilterDrillAttributeSelectorItemProps> = (
     props,
 ) => {
+    const { item } = props;
     const onClick = () => {
-        props.onClick(props.item);
+        props.onClick(item);
         props.onCloseDropdown();
     };
-    const name = props.item.attribute.attributeHeader.formOf.name;
+    const name = item.attribute.attributeHeader.formOf.name;
+
+    const dateAttributes = useDashboardSelector(selectCatalogDateDatasets);
+    const isFromDateAttribute = dateAttributes.some((attribute) =>
+        attribute.dateAttributes.some((dateAttribute) =>
+            areObjRefsEqual(dateAttribute.attribute.ref, item.attribute.attributeHeader.formOf.ref),
+        ),
+    );
 
     return (
         <a
             onClick={onClick}
             className={cx("s-drill-attribute-selector-item", {
-                // "gd-drill-attribute-selector-list-item": !props.isDateAttribute,
-                "gd-drill-attribute-selector-list-item": true,
-                // "gd-drill-attribute-date-selector-list-item": props.isDateAttribute,
+                "gd-drill-attribute-selector-list-item": !isFromDateAttribute,
+                "gd-drill-attribute-date-selector-list-item": isFromDateAttribute,
             })}
             title={name}
         >
@@ -36,15 +45,3 @@ export const DrillAttributeSelectorItem: React.FunctionComponent<IFilterDrillAtt
         </a>
     );
 };
-
-// const mapStateToProps = (
-//     appState: AppState,
-//     ownProps: IDrillAttributeSelectorItemOwnProps,
-// ): IDrillAttributeSelectorItemStateProps => {
-//     const attributeRef = ownProps?.item?.attribute?.attributeHeader?.formOf?.ref;
-//     return {
-//         isDateAttribute: isDrillAttributeDate(appState, attributeRef),
-//     };
-// };
-//
-// export default connect(mapStateToProps)(DrillAttributeSelectorItem);
