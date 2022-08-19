118b489ba28ac8afb5ae934bbd792e3a1eae7fb3
Dan Homola <dan.homola@gooddata.com>
2022-08-19 14:03:18 +0200

    Prevent deprecation warnings
    JIRA: FET-1121

--- examples/sdk-examples/scripts/run-examples.sh
+++ examples/sdk-examples/scripts/run-examples.sh
@@ -6,5 +6,5 @@ if [ ! -z "$1" ]; then # if first argument is not empty
 fi
 
 
-echo "$ webpack serve --https $PARAMS"
-webpack serve --hot --https $PARAMS
+echo "$ webpack serve --hot --server-type https $PARAMS"
+webpack serve --hot --server-type https $PARAMS
