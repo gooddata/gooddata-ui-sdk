c7f499100aa3a68f9eab7bc19803bf1bae4bb188
oleg-morozov <oleg.morozov@gooddata.com>
2022-09-13 17:11:00 +0200

    Tiger OpenAPI settings resolver without workspace
    JIRA: TNT-1033

--- libs/sdk-backend-tiger/src/backend/user/settings.ts
+++ libs/sdk-backend-tiger/src/backend/user/settings.ts
@@ -4,6 +4,8 @@ import { convertApiError } from "../../utils/errorHandling";
 import { TigerAuthenticatedCallGuard } from "../../types";
 import { TigerFeaturesService } from "../features";
 import { DefaultUserSettings } from "../uiSettings";
+import { ISettings } from "@gooddata/sdk-model";
+import { unwrapSettingContent } from "../../convertors/fromBackend/SettingsConverter";
 
 export class TigerUserSettingsService implements IUserSettingsService {
     constructor(private readonly authCall: TigerAuthenticatedCallGuard) {}
@@ -12,9 +14,19 @@ export class TigerUserSettingsService implements IUserSettingsService {
         return this.authCall(async (client) => {
             const profile = await client.profile.getCurrent();
             const features = await new TigerFeaturesService(this.authCall).getFeatures(profile);
+            const { data } = await client.actions.resolveAllSettingsWithoutWorkspace();
+            const resolvedSettings = data.reduce(
+                (result: ISettings, setting) => ({
+                    ...result,
+                    [setting.id]: unwrapSettingContent(setting.content),
+                }),
+                {},
+            );
+
             return {
                 ...DefaultUserSettings,
                 ...features,
+                ...resolvedSettings,
                 userId: profile.userId,
             };
         });
