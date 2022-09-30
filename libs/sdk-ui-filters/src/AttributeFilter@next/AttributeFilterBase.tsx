96ca61a897cd30b1576fe68e2b60531a3d08f69a
Matyas Kandl <matyas.kandl@gooddata.com>
2022-09-30 16:48:07 +0200

    Improve AttributeFilter error messages
    - Parent filtering / hidden elements are not supported on tiger - provide better error messages
    
    JIRA: RAIL-4452

--- libs/sdk-ui-filters/src/AttributeFilter@next/AttributeFilterBase.tsx
+++ libs/sdk-ui-filters/src/AttributeFilter@next/AttributeFilterBase.tsx
@@ -1,5 +1,6 @@
 // (C) 2021-2022 GoodData Corporation
 import React from "react";
+import { useBackend } from "@gooddata/sdk-ui";
 import { AttributeFilterRenderer } from "./Components/AttributeFilterRenderer";
 import { IAttributeFilterBaseProps } from "./types";
 import { validateAttributeFilterProps } from "./utils";
@@ -9,7 +10,9 @@ import { AttributeFilterProviders } from "./AttributeFilterProviders";
  * @internal
  */
 export const AttributeFilterBase: React.FC<IAttributeFilterBaseProps> = (props) => {
-    validateAttributeFilterProps(props);
+    const backend = useBackend(props.backend);
+
+    validateAttributeFilterProps({ backend, ...props });
 
     return (
         <AttributeFilterProviders {...props}>
