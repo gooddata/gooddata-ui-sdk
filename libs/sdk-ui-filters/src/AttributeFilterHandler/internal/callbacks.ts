ac28c728c0950465c20b5d635a8ffed0268877f2
Dan Homola <dan.homola@gooddata.com>
2022-10-10 10:55:59 +0200

    Log errors from AttributeFilterHandler to console
    This improves DX as the developer can easily see what the error was in
    the console without having to look into redux or registering custom
    callbacks themselves.
    
    JIRA: RAIL-4515

--- libs/sdk-ui-filters/src/AttributeFilterHandler/internal/callbacks.ts
+++ libs/sdk-ui-filters/src/AttributeFilterHandler/internal/callbacks.ts
@@ -33,6 +33,7 @@ import {
     OnSelectionChangedCallbackPayload,
     OnSelectionCommittedCallbackPayload,
 } from "../types";
+import { GoodDataSdkError } from "@gooddata/sdk-ui";
 
 const newCallbackRegistrations = () => {
     return {
@@ -105,6 +106,13 @@ const newCallbackRegistrationsWithGlobalUnsubscribe = () => {
     };
 };
 
+function logError(activity: string, error: GoodDataSdkError): void {
+    const cause = error.getCause();
+    const formattedCause = cause ? `\nInner error: ${cause}` : "";
+    // eslint-disable-next-line no-console
+    console.error(`Error while ${activity}: ${error.getMessage()}${formattedCause}`);
+}
+
 /**
  * @internal
  */
@@ -119,6 +127,7 @@ export const newAttributeFilterCallbacks = () => {
         } else if (actions.initSuccess.match(action)) {
             registrations.initSuccess.invoke(action.payload);
         } else if (actions.initError.match(action)) {
+            logError("initializing", action.payload.error);
             registrations.initError.invoke(action.payload);
         } else if (actions.initCancel.match(action)) {
             registrations.initCancel.invoke(action.payload);
@@ -130,6 +139,7 @@ export const newAttributeFilterCallbacks = () => {
         } else if (actions.loadAttributeSuccess.match(action)) {
             registrations.loadAttributeSuccess.invoke(action.payload);
         } else if (actions.loadAttributeError.match(action)) {
+            logError("loading attribute", action.payload.error);
             registrations.loadAttributeError.invoke(action.payload);
         } else if (actions.loadAttributeCancel.match(action)) {
             registrations.loadAttributeCancel.invoke(action.payload);
@@ -141,6 +151,7 @@ export const newAttributeFilterCallbacks = () => {
         } else if (actions.loadInitialElementsPageSuccess.match(action)) {
             registrations.loadInitialElementsPageSuccess.invoke(action.payload);
         } else if (actions.loadInitialElementsPageError.match(action)) {
+            logError("loading initial elements page", action.payload.error);
             registrations.loadInitialElementsPageError.invoke(action.payload);
         } else if (actions.loadInitialElementsPageCancel.match(action)) {
             registrations.loadInitialElementsPageCancel.invoke(action.payload);
@@ -152,6 +163,7 @@ export const newAttributeFilterCallbacks = () => {
         } else if (actions.loadNextElementsPageSuccess.match(action)) {
             registrations.loadNextElementsPageSuccess.invoke(action.payload);
         } else if (actions.loadNextElementsPageError.match(action)) {
+            logError("loading next elements page", action.payload.error);
             registrations.loadNextElementsPageError.invoke(action.payload);
         } else if (actions.loadNextElementsPageCancel.match(action)) {
             registrations.loadNextElementsPageCancel.invoke(action.payload);
@@ -163,6 +175,7 @@ export const newAttributeFilterCallbacks = () => {
         } else if (actions.loadCustomElementsSuccess.match(action)) {
             registrations.loadCustomElementsSuccess.invoke(action.payload);
         } else if (actions.loadCustomElementsError.match(action)) {
+            logError("loading custom elements", action.payload.error);
             registrations.loadCustomElementsError.invoke(action.payload);
         } else if (actions.loadCustomElementsCancel.match(action)) {
             registrations.loadCustomElementsCancel.invoke(action.payload);
