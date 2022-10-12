ce35f2688a92e1721b91248c6417c297850fd95f
Matyas Kandl <matyas.kandl@gooddata.com>
2022-10-12 17:39:49 +0200

    Enable telemetry for refactored AttributeFilter
    JIRA: RAIL-4408

--- libs/sdk-ui-filters/src/AttributeFilter/AttributeFilterBase.tsx
+++ libs/sdk-ui-filters/src/AttributeFilter/AttributeFilterBase.tsx
@@ -1,21 +1,43 @@
 // (C) 2021-2022 GoodData Corporation
-import React from "react";
-import { useBackend } from "@gooddata/sdk-ui";
+import React, { useMemo } from "react";
+import { useBackendStrict, useWorkspaceStrict } from "@gooddata/sdk-ui";
 import { AttributeFilterRenderer } from "./Components/AttributeFilterRenderer";
 import { IAttributeFilterBaseProps } from "./types";
 import { validateAttributeFilterProps } from "./utils";
 import { AttributeFilterProviders } from "./AttributeFilterProviders";
+import stableStringify from "json-stable-stringify";
 
 /**
  * @internal
  */
 export const AttributeFilterBase: React.FC<IAttributeFilterBaseProps> = (props) => {
-    const backend = useBackend(props.backend);
+    const backend = useBackendStrict(props.backend, "AttributeFilter");
+    const workspace = useWorkspaceStrict(props.workspace, "AttributeFilter");
+    const { filter, locale, hiddenElements, staticElements, fullscreenOnMobile, parentFilters, title } =
+        props;
+
+    const complexTelemetryPropsCheck = useMemo(() => {
+        return stableStringify({ filter, hiddenElements, staticElements, parentFilters });
+    }, [filter, hiddenElements, staticElements, parentFilters]);
+
+    const backendWithTelemetry = useMemo(() => {
+        return backend.withTelemetry("AttributeFilter", {
+            workspace,
+            locale,
+            title,
+            fullscreenOnMobile,
+            filter,
+            hiddenElements,
+            staticElements,
+            parentFilters,
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [backend, workspace, locale, title, fullscreenOnMobile, complexTelemetryPropsCheck]);
 
     validateAttributeFilterProps({ backend, ...props });
 
     return (
-        <AttributeFilterProviders {...props}>
+        <AttributeFilterProviders {...props} backend={backendWithTelemetry}>
             <AttributeFilterRenderer />
         </AttributeFilterProviders>
     );
