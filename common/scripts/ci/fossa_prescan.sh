7ae2ed30242e5c6640f6cc205498c08281d755ee
Binh Pham <binh.pham@gooddata.com>
2022-10-17 16:20:47 +0700

    Use fossa V3 to scan the project Only scan pnpm module under common/config/rush folder It should contain all deps being used for the project
    Create empty package.json inside pre_scan script
    JIRA: RAIL-4412

--- common/scripts/ci/fossa_prescan.sh
+++ common/scripts/ci/fossa_prescan.sh
@@ -2,5 +2,11 @@
 
 DIR=$(echo $(cd $(dirname "${BASH_SOURCE[0]}") && pwd -P))
 
-# install the dependencies using Rush
-node "${DIR}/../install-run-rush.js" install
+# create dummy package.json for fossa-cli is able to detect pnpm module
+# see https://github.com/fossas/fossa-cli/blob/master/docs/references/strategies/languages/nodejs/pnpm.md
+pkg_path=${DIR}/../../config/rush/package.json
+[ -f $pkg_path ] && {
+  echo "ERROR: $pkg_path exists which is not expected"
+  echo "This file supposed to be created only when running fossa scan"
+  exit 1
+} || echo '{}' > $pkg_path
