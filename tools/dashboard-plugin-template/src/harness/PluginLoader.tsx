fc501e9f380dce5bca749db3787ff0ab19d91d55
Dan Homola <dan.homola@gooddata.com>
2022-10-25 13:25:01 +0200

    Add Plugin reload button to plugin toolkit
    JIRA: RAIL-4438

--- tools/dashboard-plugin-template/src/harness/PluginLoader.tsx
+++ tools/dashboard-plugin-template/src/harness/PluginLoader.tsx
@@ -1,23 +1,76 @@
 // (C) 2019-2022 GoodData Corporation
-import React from "react";
-import { idRef } from "@gooddata/sdk-model";
-import { DashboardStub, IEmbeddedPlugin } from "@gooddata/sdk-ui-loaders";
+import React, { useCallback, useMemo, useState } from "react";
+import { IDashboard, idRef, ObjRef } from "@gooddata/sdk-model";
+import { DashboardLoadStatus, IEmbeddedPlugin, useDashboardLoader } from "@gooddata/sdk-ui-loaders";
 import PluginFactory from "../plugin";
 import { DEFAULT_DASHBOARD_ID } from "./constants";
-import { DashboardConfig } from "@gooddata/sdk-ui-dashboard";
+import {
+    DashboardConfig,
+    RenderMode,
+    selectDashboardWorkingDefinition,
+    selectRenderMode,
+    SingleDashboardStoreAccessor,
+} from "@gooddata/sdk-ui-dashboard";
+import { ErrorComponent, LoadingComponent } from "@gooddata/sdk-ui";
+import { Toolbar } from "./Toolbar";
 
 const Plugins: IEmbeddedPlugin[] = [{ factory: PluginFactory }];
 const Config: DashboardConfig = {
     mapboxToken: process.env.MAPBOX_TOKEN,
 };
+const dashboardRef = idRef(process.env.DASHBOARD_ID || DEFAULT_DASHBOARD_ID, "analyticalDashboard");
 
-export const PluginLoader = (): JSX.Element => {
+interface IPluginLoaderBodyProps {
+    loaderStatus: DashboardLoadStatus;
+}
+
+const PluginLoaderBody: React.FC<IPluginLoaderBodyProps> = (props) => {
+    const { status, error, result } = props.loaderStatus;
+
+    if (status === "loading") {
+        return <LoadingComponent />;
+    }
+
+    if (status === "error" || result === undefined) {
+        return <ErrorComponent message={error?.message ?? ""} />;
+    }
+
     return (
-        <DashboardStub
-            dashboard={idRef(process.env.DASHBOARD_ID || DEFAULT_DASHBOARD_ID)}
-            loadingMode="staticOnly"
-            config={Config}
-            extraPlugins={Plugins}
+        <result.DashboardComponent
+            {...result.props}
+            onStateChange={(state, dispatch) => {
+                SingleDashboardStoreAccessor.getOnChangeHandler()(state, dispatch);
+                result.props.onStateChange?.(state, dispatch);
+            }}
         />
+    );
+};
+
+export const PluginLoader = () => {
+    const [dashboard, setDashboard] = useState<IDashboard | ObjRef>(dashboardRef);
+    const [renderMode, setRenderMode] = useState<RenderMode>("view");
+
+    const augmentedConfig = useMemo(() => ({ ...Config, initialRenderMode: renderMode }), [renderMode]);
+
+    const loaderStatus = useDashboardLoader({
+        dashboard,
+        loadingMode: "staticOnly",
+        config: augmentedConfig,
+        extraPlugins: Plugins,
+    });
+
+    const onReload = useCallback(() => {
+        const select = SingleDashboardStoreAccessor.getDashboardSelect();
+        const dashboardObject = select(selectDashboardWorkingDefinition);
+        const renderMode = select(selectRenderMode);
+        setDashboard(dashboardObject as any);
+        setRenderMode(renderMode);
+    }, []);
+
+    return (
+        <>
+            <Toolbar onReloadPlugin={onReload} reloadDisabled={loaderStatus.status !== "success"} />
+            <PluginLoaderBody loaderStatus={loaderStatus} />
+        </>
     );
 };
