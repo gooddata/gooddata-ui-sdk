e75e0e3b4bd37c5c5e0150f0100afbcd9bdcb623
Dan Homola <dan.homola@gooddata.com>
2022-11-03 10:44:45 +0100

    Fix KPI drill in edit mode
    Turns out the KPI should drill even in edit mode when selected and not
    embedded and default drill not disabled.
    
    JIRA: RAIL-4579

---
+++ libs/sdk-ui-dashboard/src/presentation/widget/kpi/common/useKpiDrill.ts
@@ -0,0 +1,38 @@
+// (C) 2022 GoodData Corporation
+import { useCallback } from "react";
+import { IDrillToLegacyDashboard, IKpiWidget } from "@gooddata/sdk-model";
+import { OnFiredDashboardDrillEvent } from "../../../../types";
+import { useDrill, useDrillToLegacyDashboard } from "../../../drill";
+import { useDashboardSelector, selectDisableDefaultDrills } from "../../../../model";
+
+/**
+ * Returns a drill handler for a given KPI.
+ *
+ * @param kpiWidget - widget the drills of which to handle
+ * @internal
+ */
+export function useKpiDrill(kpiWidget: IKpiWidget) {
+    const disableDefaultDrills = useDashboardSelector(selectDisableDefaultDrills);
+
+    const { run: handleDrillToLegacyDashboard } = useDrillToLegacyDashboard();
+    const { run: handleDrill } = useDrill({
+        onSuccess: (event) => {
+            if (disableDefaultDrills || !event.payload.drillEvent.drillDefinitions[0]) {
+                return;
+            }
+
+            handleDrillToLegacyDashboard(
+                event.payload.drillEvent.drillDefinitions[0] as IDrillToLegacyDashboard,
+                event.payload.drillEvent,
+                event.correlationId,
+            );
+        },
+    });
+
+    return useCallback<OnFiredDashboardDrillEvent>(
+        (event) => {
+            handleDrill(event, { widget: kpiWidget });
+        },
+        [handleDrill, kpiWidget],
+    );
+}
