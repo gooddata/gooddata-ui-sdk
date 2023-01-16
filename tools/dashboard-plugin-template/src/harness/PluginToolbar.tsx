6149b8810b0d124f8f0af102cf3e7b3981a3341d
ivanmjartan <ivan.mjartan@gooddata.com>
2023-01-16 13:58:23 +0100

    fix: hide overlays button still shows in view mode in Harness
    JIRA: RAIL-4725

--- tools/dashboard-plugin-template/src/harness/PluginToolbar.tsx
+++ tools/dashboard-plugin-template/src/harness/PluginToolbar.tsx
@@ -1,8 +1,10 @@
-// (C) 2022 GoodData Corporation
+// (C) 2022-2023 GoodData Corporation
 import {
     DefaultDashboardToolbar,
     DefaultDashboardToolbarButton,
     DefaultDashboardToolbarGroup,
+    selectIsInEditMode,
+    useDashboardSelector,
 } from "@gooddata/sdk-ui-dashboard";
 import React from "react";
 
@@ -16,16 +18,21 @@ interface IPluginToolbarProps {
 
 export const PluginToolbar: React.FC<IPluginToolbarProps> = (props) => {
     const { isPluginEnabled, isHideOverlaysEnabled, reloadPlugins, togglePlugin, hideOverlays } = props;
+
+    const isInEditMode = useDashboardSelector(selectIsInEditMode);
+
     return (
         <DefaultDashboardToolbar>
             <DefaultDashboardToolbarGroup title="Plugins">
                 <DefaultDashboardToolbarButton icon="sync" onClick={reloadPlugins} tooltip="Reload" />
-                <DefaultDashboardToolbarButton
-                    icon="invisible"
-                    onClick={hideOverlays}
-                    tooltip="Hide overlays"
-                    disabled={!isHideOverlaysEnabled}
-                />
+                {!!isInEditMode && (
+                    <DefaultDashboardToolbarButton
+                        icon="invisible"
+                        onClick={hideOverlays}
+                        tooltip="Hide overlays"
+                        disabled={!isHideOverlaysEnabled}
+                    />
+                )}
                 <DefaultDashboardToolbarButton
                     icon="circle-cross"
                     onClick={togglePlugin}
