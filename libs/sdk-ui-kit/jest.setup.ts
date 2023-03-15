a89e0855f019eb9d5b98680acd336a34ce5b5100
scavnickyj <jakub.scavnicky@gooddata.com>
2023-03-15 13:15:25 +0100

    refactor: remove enzyme related packages
    JIRA: FET-1054

--- libs/sdk-ui-kit/jest.setup.ts
+++ libs/sdk-ui-kit/jest.setup.ts
@@ -1,13 +1,7 @@
 // (C) 2019 GoodData Corporation
 /* eslint-disable @typescript-eslint/no-var-requires */
-import "jest-enzyme";
 import "@testing-library/jest-dom/extend-expect";
 import * as raf from "raf";
-
-const enzyme = require("enzyme");
-const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
-
-enzyme.configure({ adapter: new Adapter() });
 
 raf.polyfill();
 
