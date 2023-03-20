16cb02c5ce47462945541e80346e6de367c059e6
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2023-03-20 19:53:55 +0100

    feat: add missing test classes

--- libs/sdk-ui-filters/src/AttributeFilter/Components/ElementsSelect/SingleSelectionAttributeFilterElementsSelectItem.tsx
+++ libs/sdk-ui-filters/src/AttributeFilter/Components/ElementsSelect/SingleSelectionAttributeFilterElementsSelectItem.tsx
@@ -1,6 +1,7 @@
 // (C) 2023 GoodData Corporation
 import React, { useCallback, useMemo } from "react";
 import cx from "classnames";
+import camelCase from "lodash/camelCase";
 import { IAttributeFilterElementsSelectItemProps } from "./types";
 import { CustomizableCheckmark, useMediaQuery } from "@gooddata/sdk-ui-kit";
 import { getElementTitle } from "../../utils";
@@ -30,14 +31,19 @@ export const SingleSelectionAttributeFilterElementsSelectItem: React.VFC<
 
     const isMobile = useMediaQuery("mobileDevice");
 
+    const classes = cx(
+        "gd-single-selection-attribute-filter-elements-select-item",
+        "gd-list-item",
+        "s-attribute-filter-list-item",
+        `s-attribute-filter-list-item-${camelCase(item.title)}`,
+        { "is-selected": isSelected },
+        {
+            "s-attribute-filter-list-item-selected": isSelected,
+        },
+    );
+
     return (
-        <div
-            className={cx("gd-single-selection-attribute-filter-elements-select-item", "gd-list-item", {
-                "is-selected": isSelected,
-            })}
-            onClick={onItemClick}
-            title={itemTitle}
-        >
+        <div className={classes} onClick={onItemClick} title={itemTitle}>
             {itemTitle}
             {isSelected && isMobile && fullscreenOnMobile ? (
                 <CustomizableCheckmark className="gd-customizable-checkmark-mobile-navigation" />
