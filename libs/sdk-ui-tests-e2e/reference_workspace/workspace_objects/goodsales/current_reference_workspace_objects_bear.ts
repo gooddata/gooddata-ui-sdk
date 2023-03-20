b34c3ea9b347bfca9ef1006b04f02c3b0649fe8d
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2023-03-20 18:03:35 +0100

    test: single selection e2e tests
    JIRA: TNT-1407

--- libs/sdk-ui-tests-e2e/reference_workspace/workspace_objects/goodsales/current_reference_workspace_objects_bear.ts
+++ libs/sdk-ui-tests-e2e/reference_workspace/workspace_objects/goodsales/current_reference_workspace_objects_bear.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-/* THIS FILE WAS AUTO-GENERATED USING CATALOG EXPORTER; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2023-02-21T11:45:25.538Z; */
+/* THIS FILE WAS AUTO-GENERATED USING CATALOG EXPORTER; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2023-03-20T15:59:32.131Z; */
 // @ts-ignore ignore unused imports here if they happen (e.g. when there is no measure in the workspace)
 import {
     newAttribute,
