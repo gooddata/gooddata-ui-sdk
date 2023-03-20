84e3a695fec10b46ba4aac313b7ad3ffec34e66b
scavnickyj <jakub.scavnicky@gooddata.com>
2023-03-20 17:43:51 +0100

    test: rewrite enzyme tests to RTL in sdk-ui
    - rewrite tests
    - remove enzyme related packages
    
    JIRA: FET-1054

--- libs/sdk-ui/jest.setup.ts
+++ libs/sdk-ui/jest.setup.ts
@@ -1,12 +1,6 @@
 // (C) 2019 GoodData Corporation
-/* eslint-disable @typescript-eslint/no-var-requires */
-import "jest-enzyme";
+/* eslint-disable import/no-unassigned-import */
 import * as raf from "raf";
 import "@testing-library/jest-dom/extend-expect";
-
-const enzyme = require("enzyme");
-const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
-
-enzyme.configure({ adapter: new Adapter() });
 
 raf.polyfill();
