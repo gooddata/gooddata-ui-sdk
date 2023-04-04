36f9d1cb0ebc7fa461c71f77e07408677e302c03
de.do <de.do@gooddata.com>
2023-04-04 12:06:15 +0700

    feat: adding apis to config date format, timezone for Org
    JIRA: SD-2656

--- libs/sdk-backend-spi/src/organization/settings/index.ts
+++ libs/sdk-backend-spi/src/organization/settings/index.ts
@@ -27,6 +27,25 @@ export interface IOrganizationSettingsService {
     setLocale(locale: string): Promise<void>;
 
     /**
+     * Sets timezone for organization.
+     *
+     * @param timezone - the value based on IANA time zone database naming convention.
+     * for example: "America/Los_Angeles", etc.
+     *
+     * @returns promise
+     */
+    setTimezone(timezone: string): Promise<void>;
+
+    /**
+     * Sets date format for organization.
+     *
+     * @param dateFormat - the format based on the ICU standard, for example: "en-US", "cs-CZ", etc.
+     *
+     * @returns promise
+     */
+    setDateFormat(dateFormat: string): Promise<void>;
+
+    /**
      * Get all current organization settings.
      *
      * @remarks
