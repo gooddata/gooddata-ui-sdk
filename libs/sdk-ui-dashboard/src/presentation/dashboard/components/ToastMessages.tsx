7622899588af0115aefca558a34674dda9564e72
scavnickyj <jakub.scavnicky@gooddata.com>
2023-05-22 15:46:20 +0200

    fix: move toast message reset actions to saga
    With React 18, toast message ui actions
    dispatched by components were racing
    with sagas which caused some problems
    in tests. After moving change render
    mode effect actions to the change
    render mode saga, it is fixed.
    
    JIRA: RAIL-4969

--- libs/sdk-ui-dashboard/src/presentation/dashboard/components/ToastMessages.tsx
+++ libs/sdk-ui-dashboard/src/presentation/dashboard/components/ToastMessages.tsx
@@ -1,31 +1,15 @@
 // (C) 2022 GoodData Corporation
-import React, { useCallback, useContext, useEffect, useMemo } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import { Messages, ToastMessageContext } from "@gooddata/sdk-ui-kit";
 import { useDrillValidationMessages } from "./useDrillValidationMessages";
-import { selectRenderMode, useDashboardSelector } from "../../../model";
 
 /**
  * @internal
  */
 export const ToastMessages: React.FC = () => {
-    const {
-        messages: toastMessages,
-        removeMessage: removeToastMessage,
-        removeAllMessages: removeAllToastMessages,
-    } = useContext(ToastMessageContext);
-    const {
-        messages: drillValidationMessages,
-        removeMessage: removeDrillValidationMessage,
-        removeAllMessages: removeAllDrillValidationMessages,
-    } = useDrillValidationMessages();
-
-    const renderMode = useDashboardSelector(selectRenderMode);
-
-    // remove all messages whenever the render mode changes
-    useEffect(() => {
-        removeAllToastMessages();
-        removeAllDrillValidationMessages();
-    }, [removeAllDrillValidationMessages, removeAllToastMessages, renderMode]);
+    const { messages: toastMessages, removeMessage: removeToastMessage } = useContext(ToastMessageContext);
+    const { messages: drillValidationMessages, removeMessage: removeDrillValidationMessage } =
+        useDrillValidationMessages();
 
     const messages = useMemo(
         () => [...toastMessages, ...drillValidationMessages],
