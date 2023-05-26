5a3bcc48842da0e5e9bd29a5d34664a4ba070ca4
Khanh Huynh <khanh.huynh@gooddata.com>
2023-05-26 15:51:12 +0700

    feat: show attribute/geo icon in attribute filter dropdown
    JIRA: EGL-57

--- libs/sdk-ui-dashboard/src/presentation/filterBar/attributeFilter/addAttributeFilter/addAttributeFilterUtils.ts
+++ libs/sdk-ui-dashboard/src/presentation/filterBar/attributeFilter/addAttributeFilter/addAttributeFilterUtils.ts
@@ -1,12 +1,18 @@
 // (C) 2023 GoodData Corporation
 
 import { VisType } from "@gooddata/sdk-ui";
-import { IInsight, insightVisualizationType } from "@gooddata/sdk-model";
+import { IInsight, IInsightWidget, insightVisualizationType } from "@gooddata/sdk-model";
+import { ExtendedDashboardWidget } from "../../../../model";
+import { ObjRefMap } from "../../../../_staging/metadata/objRefMap";
 
 const SUPPORTED_LOCATION_ICON_CHART_TYPES: VisType[] = ["pushpin"];
 
-export const isLocationIconEnabled = (insights: IInsight[]) => {
-    return insights
-        .map((it) => insightVisualizationType(it) as VisType)
+export const isLocationIconEnabled = (
+    insightWidgets: ExtendedDashboardWidget[],
+    insightsMap: ObjRefMap<IInsight>,
+) => {
+    return insightWidgets
+        .map((it) => insightsMap.get((it as IInsightWidget)?.insight))
+        .map((it) => (it && insightVisualizationType(it)) as VisType)
         .some((it) => SUPPORTED_LOCATION_ICON_CHART_TYPES.includes(it));
 };
