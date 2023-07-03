223a108c8249d64c91fc28c62d6ee00e036ddec6
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2023-07-03 14:59:02 +0200

    feat: fix tests
    JIRA: TNT-1589

--- libs/sdk-ui-pivot/src/impl/cell/tests/tableCell.test.ts
+++ libs/sdk-ui-pivot/src/impl/cell/tests/tableCell.test.ts
@@ -55,48 +55,55 @@ describe("Table utils - Cell", () => {
             expect(getMeasureCellStyle(null, "[red]$#,##0.00", undefined, true)).toEqual({
                 color: "var(--gd-table-nullValueColor, var(--gd-palette-complementary-6, #94a1ad))",
                 fontWeight: "bold",
+                textAlign: "right",
             });
         });
 
-        it("should NOT get style when cellContent=''", () => {
-            expect(getMeasureCellStyle("", "[red]$#,##0.00", undefined, true)).toEqual({});
+        it("should get just alignment style when cellContent=''", () => {
+            expect(getMeasureCellStyle("", "[red]$#,##0.00", undefined, true)).toEqual({
+                textAlign: "right",
+            });
         });
 
         it("should get style for number with color in format when applyColor=true", () => {
-            expect(getMeasureCellStyle("123456789", "[red]$#,##0.00", undefined, true)).toEqual({
-                color: "#FF0000",
-            });
+            expect(getMeasureCellStyle("123456789", "[red]$#,##0.00", undefined, true).color).toEqual(
+                "#FF0000",
+            );
         });
 
         it("should get style for number with backgroundColor in format when applyColor=true", () => {
             expect(
-                getMeasureCellStyle("123456789", "[backgroundColor=ffff00]$#,##0.00", undefined, true),
-            ).toEqual({
-                backgroundColor: "#ffff00",
-            });
+                getMeasureCellStyle("123456789", "[backgroundColor=ffff00]$#,##0.00", undefined, true)
+                    .backgroundColor,
+            ).toEqual("#ffff00");
         });
 
         it("should get style for number with color and backgroundColor in format when applyColor=true", () => {
             expect(
                 getMeasureCellStyle("123456789", "[backgroundColor=ffff00][red]$#,##0.00", undefined, true),
-            ).toEqual({
+            ).toMatchObject({
                 backgroundColor: "#ffff00",
                 color: "#FF0000",
             });
         });
 
         it("should NOT get style for number with color in format when applyColor=false", () => {
-            expect(getMeasureCellStyle("123456789", "[red]$#,##0.00", undefined, false)).toEqual({});
+            expect(
+                getMeasureCellStyle("123456789", "[red]$#,##0.00", undefined, false).color,
+            ).toBeUndefined();
         });
 
         it("should NOT get style for number with backgroundColor in format when applyColor=false", () => {
             expect(
-                getMeasureCellStyle("123456789", "[backgroundColor=ffff00]$#,##0.00", undefined, false),
-            ).toEqual({});
+                getMeasureCellStyle("123456789", "[backgroundColor=ffff00]$#,##0.00", undefined, false)
+                    .backgroundColor,
+            ).toBeUndefined();
         });
 
         it("should NOT get style for number without color or backgroundColor in format when applyColor=true", () => {
-            expect(getMeasureCellStyle("123456789", "$#,##0.00", undefined, true)).toEqual({});
+            const style = getMeasureCellStyle("123456789", "$#,##0.00", undefined, true);
+            expect(style.color).toBeUndefined();
+            expect(style.backgroundColor).toBeUndefined();
         });
     });
 });
