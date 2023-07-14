f384b8dfab96c3ccfc2af0385943cc0e94eb1fd1
oleg-morozov <oleg.morozov@gooddata.com>
2023-07-14 10:20:55 +0200

    feat: node 18 and new docker images with c9s
    JIRA: TNT-1599

--- common/scripts/ci/docker_apidocs_build.sh
+++ common/scripts/ci/docker_apidocs_build.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$(echo $(cd $(dirname "${BASH_SOURCE[0]}")/../../.. && pwd -P))
 # go one level up to "see" the gooddata-ui-apidocs too
 ROOT_DIR="$ROOT_DIR/.."
 
-IMAGE="node:16.20.0-bullseye"
+IMAGE="node:18.17.0-bullseye"
 
 echo "Running apidocs build using ${IMAGE} in root directory ${ROOT_DIR}"
 
