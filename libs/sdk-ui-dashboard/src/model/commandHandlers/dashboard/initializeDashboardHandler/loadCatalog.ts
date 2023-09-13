0ed7d5b393334612f35c4b5c5692e97bd2a8f8b8
oleg-morozov <oleg.morozov@gooddata.com>
2023-09-13 16:28:07 +0200

    fix: use include/exclude tags for catalog fetch
    JIRA: TNT-1709

--- libs/sdk-ui-dashboard/src/model/commandHandlers/dashboard/initializeDashboardHandler/loadCatalog.ts
+++ libs/sdk-ui-dashboard/src/model/commandHandlers/dashboard/initializeDashboardHandler/loadCatalog.ts
@@ -1,7 +1,8 @@
 // (C) 2021-2022 GoodData Corporation
 import { DashboardContext } from "../../../types/commonTypes.js";
 import { IWorkspaceCatalog, IWorkspaceCatalogFactoryOptions } from "@gooddata/sdk-backend-spi";
-import { DateAttributeGranularity } from "@gooddata/sdk-model";
+import { DateAttributeGranularity, idRef } from "@gooddata/sdk-model";
+import { InitializeDashboard } from "../../../commands/index.js";
 
 const SupportedCatalogGranularity: DateAttributeGranularity[] = [
     "GDC.time.day_in_week",
@@ -15,12 +16,13 @@ const SupportedCatalogGranularity: DateAttributeGranularity[] = [
     "GDC.time.quarter_in_year",
 ];
 
-export function loadCatalog(ctx: DashboardContext): Promise<IWorkspaceCatalog> {
+export function loadCatalog(ctx: DashboardContext, cmd: InitializeDashboard): Promise<IWorkspaceCatalog> {
     const { backend, workspace } = ctx;
+    const availability = cmd.payload.config?.objectAvailability;
 
     const options: IWorkspaceCatalogFactoryOptions = {
-        excludeTags: [],
-        includeTags: [],
+        excludeTags: (availability?.excludeObjectsWithTags ?? []).map((tag) => idRef(tag)),
+        includeTags: (availability?.includeObjectsWithTags ?? []).map((tag) => idRef(tag)),
         types: ["attribute", "fact", "measure", "dateDataset", "attributeHierarchy"],
         includeDateGranularities: SupportedCatalogGranularity,
         loadGroups: false,
