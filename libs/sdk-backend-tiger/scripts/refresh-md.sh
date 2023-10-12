0983793ec9acd9e9632aebc19c756d7881beeb25
Thuong Nguyen <hoaithuong4792@gmail.com>
2023-10-12 14:38:26 +0700

    feat: update test-fixture version for Snapshot filtering e2e
    JIRA: QA-20270

--- libs/sdk-backend-tiger/scripts/refresh-md.sh
+++ libs/sdk-backend-tiger/scripts/refresh-md.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=`dirname $0`
 ROOTDIR="${SCRIPTDIR}/.."
 EXPORTER="${ROOTDIR}/node_modules/.bin/gdc-catalog-export"
 OUTPUT="${ROOTDIR}/src/fixtures/full.ts"
-PROJECTID="00d48e50c5e0442abc2181a7afd0f66b"
+PROJECTID="b51b89a10e5845189a6fea8e96dba226"
 
 $EXPORTER \
   --hostname "https://staging.anywhere.gooddata.com" \
