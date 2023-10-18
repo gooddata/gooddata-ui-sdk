f9f897de14d1fd78ec80f024cc63b56670300b20
huyenthanh09 <huyen.nguyen@gooddata.com>
2023-10-18 17:01:12 +0700

    ci: add delete mode for cypress daily checklist
    JIRA: QA-20092

--- common/scripts/ci/run_cypress_integrated_tests.sh
+++ common/scripts/ci/run_cypress_integrated_tests.sh
@@ -45,11 +45,13 @@ fi
 $_RUSH install
 $_RUSH build -t sdk-ui-tests-e2e
 $_RUSHX libs/sdk-ui-tests-e2e create-ref-workspace
+WORKSPACE_CREATED=true
+DELETE_MODE="${DELETE_MODE:-delete_always}"
 
 $_RUSHX libs/sdk-ui-tests-e2e build-scenarios
 
 export IMAGE_ID=${sdk_backend}-gooddata-ui-sdk-scenarios-${EXECUTOR_NUMBER}
-trap "$_RUSHX libs/sdk-ui-tests-e2e delete-ref-workspace; rm -f $E2E_TEST_DIR/.env; docker rmi --force $IMAGE_ID || true" EXIT
+trap cleanup EXIT
 
 # Use Dockerfile_local as scenarios have been build in previous steps
 docker build --no-cache --file Dockerfile_local -t $IMAGE_ID . || exit 1
@@ -59,4 +61,16 @@ NO_COLOR=1 docker-compose -f docker-compose-integrated.yaml -p "$PROJECT_NAME" u
   --abort-on-container-exit --exit-code-from integrated-tests \
   --force-recreate --always-recreate-deps --renew-anon-volumes --no-color
 
-$_RUSHX libs/sdk-ui-tests-e2e delete-ref-workspace
+cleanup() {
+    echo "Executing cleanup before exiting..."
+    # remove previously created workspace
+    if [ -n "$WORKSPACE_CREATED" ]; then
+      if [ $DELETE_MODE = "delete_never" ]; then
+        echo "DELETE_MODE is delete_never, skip deleting the created workspace"
+      else
+        $_RUSHX libs/sdk-ui-tests-e2e delete-ref-workspace
+      fi
+    fi
+    rm -f $E2E_TEST_DIR/.env
+    docker rmi --force $IMAGE_ID || true
+}
