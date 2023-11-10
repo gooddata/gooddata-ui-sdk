cb206fc2d7b7329df5f295021b4a186b7a1e3700
Khanh Huynh <khanh.huynh@gooddata.com>
2023-11-10 17:08:51 +0700

    feat: remove enableAttributeHierarchies in disabled unfinished feature
    JIRA: EGL-400

--- libs/sdk-ui-dashboard/src/model/disabledUnfinishedFeatureSettings.ts
+++ libs/sdk-ui-dashboard/src/model/disabledUnfinishedFeatureSettings.ts
@@ -16,6 +16,4 @@ import { ISettings } from "@gooddata/sdk-model";
  * Add disabled FF for your feature, when it is not completed yet, but UI SDK needs to be released.
  * It will prevent this feature from being accidentally turned on in WIP state by its platform setting when used in this version of UI SDK as part of old plugin.
  */
-export const disabledUnfinishedFeatureSettings: ISettings = {
-    enableAttributeHierarchies: false,
-};
+export const disabledUnfinishedFeatureSettings: ISettings = {};
