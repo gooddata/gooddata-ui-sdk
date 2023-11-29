d2f96c7b59ef0e25b7c349d3eb6e6ae8246905ca
phong-nguyen-duy <phong.nguyen-duy@gooddata.com>
2023-11-29 15:47:28 +0700

    fix: filter out the deleted attributes in hierarchies
    JIRA: EGL-429

--- libs/sdk-ui-dashboard/src/model/store/catalog/catalogSelectors.ts
+++ libs/sdk-ui-dashboard/src/model/store/catalog/catalogSelectors.ts
@@ -363,7 +363,10 @@ function getAttributesWithHierarchyDescendants(
                 const ignoredIndex = ignoredDrillDownHierarchies.findIndex((reference) =>
                     existBlacklistHierarchyPredicate(reference, hierarchyRef, attrRef),
                 );
-                return ignoredIndex < 0;
+                const catalogIndex = allCatalogAttributes.findIndex((catalogAttributeRef) => {
+                    areObjRefsEqual(catalogAttributeRef.attribute.ref, attrRef);
+                });
+                return ignoredIndex < 0 && catalogIndex >= 0;
             });
             const foundAttributeIndex = hierarchyAttributes.findIndex((ref) =>
                 areObjRefsEqual(ref, attributeRef),
