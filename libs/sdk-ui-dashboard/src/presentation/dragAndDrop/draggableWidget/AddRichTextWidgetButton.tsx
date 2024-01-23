7eaaa9b2903014d7357047a7cc49fb09aec23f4a
scavnickyj <jakub.scavnicky@gooddata.com>
2024-01-23 11:56:01 +0100

    fix: update rich text styles
    JIRA: F1-56

--- libs/sdk-ui-dashboard/src/presentation/dragAndDrop/draggableWidget/AddRichTextWidgetButton.tsx
+++ libs/sdk-ui-dashboard/src/presentation/dragAndDrop/draggableWidget/AddRichTextWidgetButton.tsx
@@ -2,17 +2,10 @@
 import { Icon } from "@gooddata/sdk-ui-kit";
 import React from "react";
 import { FormattedMessage } from "react-intl";
-import cx from "classnames";
 
 export const AddRichTextWidgetButton: React.FC = () => {
     return (
-        <div
-            className={cx(
-                "add-item-placeholder add-item-placeholder-rich-text",
-                "add-richText-placeholder",
-                "s-add-rich-text",
-            )}
-        >
+        <div className="add-item-placeholder add-item-placeholder-rich-text s-add-rich-text">
             <Icon.RichText />
             <FormattedMessage id="addPanel.richText" />
         </div>
