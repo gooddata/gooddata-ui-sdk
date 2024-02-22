5be6a2eed0287e16f15df1fb23bc736927ced463
Petr Benes <petr.benes@gooddata.com>
2024-02-22 14:28:02 +0100

    fix: update also index.redir for hugo
    JIRA: STL-105

--- .github/actions/hugo-update-param-action/action-build/index.js
+++ .github/actions/hugo-update-param-action/action-build/index.js
@@ -32197,10 +32197,43 @@ ${pendingInterceptorsFormatter.format(pending)}
                 .join("\n") +
             "\n";
 
+        console.log(`Updating ${filePath}:`);
         console.log(result);
-
-        // Write to file
         fs.writeFileSync(filePath, result);
+
+        // transform index.redir file
+
+        const redirFilePath = core.getInput("path-to-redir-config");
+        const redir = fs.readFileSync(redirFilePath, "utf-8");
+        const split = redir.split("\n");
+        const redirLines = split.filter((line) => !line.trim().startsWith("#"));
+        const redirComment = split.filter((line) => line.trim().startsWith("#"));
+        const [base, docs, actualSort, actualLong, ...remainingVersions] = redirLines;
+
+        const shortVersion = newVersion.substring(0, newVersion.lastIndexOf("."));
+
+        // extract version between slashes /x.y.z/...
+        const parsedActualVersion = actualLong.match(/^\/(.*?)\//)[1];
+        const parsedActualVersionShort = parsedActualVersion.substring(
+            0,
+            parsedActualVersion.lastIndexOf("."),
+        );
+        const redirResult = [
+            ...redirComment,
+            base,
+            docs,
+            `/${shortVersion}/ {{ .Site.BaseURL }}/latest/ 301!`,
+            `/${newVersion}/ {{ .Site.BaseURL }}/latest/ 301!`,
+            `/${parsedActualVersion}/ {{ .Site.BaseURL }}/${parsedActualVersionShort}/ 301!`,
+            ...remainingVersions,
+            "\n",
+        ];
+
+        const redirResultJoined = redirResult.join("\n");
+
+        console.log(`Updating ${redirFilePath}:`);
+        console.log(redirResultJoined);
+        fs.writeFileSync(redirFilePath, redirResultJoined);
     })();
 
     module.exports = __webpack_exports__;
