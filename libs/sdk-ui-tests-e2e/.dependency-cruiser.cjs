9babff353346a142a06d349664fcf4062ff8ae50
ivanmjartan <ivan.mjartan@gooddata.com>
2024-03-07 17:23:01 +0100

    chore: move tools dependencies to dev dependencies
    JIRA: STL-264

--- libs/sdk-ui-tests-e2e/.dependency-cruiser.cjs
+++ libs/sdk-ui-tests-e2e/.dependency-cruiser.cjs
@@ -1,7 +1,11 @@
 const depCruiser = require("../../common/config/dep-cruiser/default.config");
 
 options = {
-    forbidden: [...depCruiser.DefaultRules, ...depCruiser.DefaultSdkRules],
+    forbidden: [
+        // we need all dep in dev because this package is tooling and its dependencies ocurred in FOSSA scan
+        ...depCruiser.DefaultRules.filter((rule) => rule.name !== "not-to-dev-dep"),
+        ...depCruiser.DefaultSdkRules,
+    ],
     options: depCruiser.DefaultOptions,
 };
 
