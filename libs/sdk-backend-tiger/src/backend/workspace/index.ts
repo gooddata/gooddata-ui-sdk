be3996a96607ce8528181bbd35a47981f81d1068
Vojtěch Tollar <vojtech.tollar@gooddata.com>
2024-03-14 13:35:00 +0100

    feat: make parentPrefixes optional for getDescriptor
    As they are currently only used in Meditor, it doesn't
    make sense to pay the API call price in all cases.
    
    JIRA: F1-188

--- libs/sdk-backend-tiger/src/backend/workspace/index.ts
+++ libs/sdk-backend-tiger/src/backend/workspace/index.ts
@@ -1,4 +1,4 @@
-// (C) 2019-2023 GoodData Corporation
+// (C) 2019-2024 GoodData Corporation
 
 import {
     IAnalyticalWorkspace,
@@ -48,7 +48,7 @@ export class TigerWorkspace implements IAnalyticalWorkspace {
         private readonly descriptor?: IWorkspaceDescriptor,
     ) {}
 
-    public async getDescriptor(): Promise<IWorkspaceDescriptor> {
+    public async getDescriptor(includeParentPrefixes: boolean = false): Promise<IWorkspaceDescriptor> {
         if (!this.descriptor) {
             return workspaceConverter(
                 (
@@ -59,13 +59,15 @@ export class TigerWorkspace implements IAnalyticalWorkspace {
                         });
                     })
                 ).data.data,
-                (
-                    await this.authCall(async (client) => {
-                        return client.actions.inheritedEntityPrefixes({
-                            workspaceId: this.workspace,
-                        });
-                    })
-                ).data,
+                includeParentPrefixes
+                    ? (
+                          await this.authCall(async (client) => {
+                              return client.actions.inheritedEntityPrefixes({
+                                  workspaceId: this.workspace,
+                              });
+                          })
+                      ).data
+                    : [],
             );
         }
         return this.descriptor;
