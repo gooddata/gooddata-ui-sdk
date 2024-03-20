6c561f83c91f8c646e64501f31e68146be238363
nknbich <bich.nguyen@gooddata.com>
2024-03-20 10:57:19 +0700

    feat: upgrade goodsales version 1.32.15
    JIRA: QA-21618

--- libs/sdk-backend-tiger/scripts/refresh-md.sh
+++ libs/sdk-backend-tiger/scripts/refresh-md.sh
@@ -4,7 +4,7 @@ SCRIPTDIR=`dirname $0`
 ROOTDIR="${SCRIPTDIR}/.."
 EXPORTER="${ROOTDIR}/node_modules/.bin/gdc-catalog-export"
 OUTPUT="${ROOTDIR}/src/fixtures/full.ts"
-PROJECTID="f15bcbc5bcd04b8b8093c70a1a479c35"
+PROJECTID="12f912afa6d648c4953c254807eafd1f"
 
 $EXPORTER \
   --hostname "https://staging.dev-latest.stg11.panther.intgdc.com" \
