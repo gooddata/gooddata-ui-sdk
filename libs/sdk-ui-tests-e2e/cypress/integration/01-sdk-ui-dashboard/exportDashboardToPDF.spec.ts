6523091ac6d675824dca1c55f19824cb69dfdbaf
nknbich <bich.nguyen@gooddata.com>
2024-05-03 11:58:51 +0700

    fix: fix failed test because sdk limit
    JIRA: QA-22000

--- libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/exportDashboardToPDF.spec.ts
+++ libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/exportDashboardToPDF.spec.ts
@@ -76,4 +76,23 @@ describe("Export dashboard to pdf", { tags: ["checklist_integrated_tiger_export"
             });
         },
     );
+
+    it("should export insight over data points limit to PDF from dashboards", () => {
+        cy.fixture("dashboardInfosForExport").then((data) => {
+            data["insightsManyData"].forEach(
+                (dashboardInfo: {
+                    dashboardTitle: string;
+                    dashboardURL: string;
+                    fileName: string;
+                    contents: string;
+                }) => {
+                    Navigation.visit(dashboardInfo.dashboardURL);
+                    widget.waitChartLoaded();
+                    topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
+                    dashboardMenu.toggle().clickOption("Export to PDF");
+                    exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
+                },
+            );
+        });
+    });
 });
