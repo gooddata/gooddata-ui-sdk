9c1840f6b3606edc0936203d15e912c70effd203
scavnickyj <jakub.scavnicky@gooddata.com>
2024-05-03 10:39:21 +0200

    feat: make layout navigable by keyboard
    JIRA: F1-313

--- libs/sdk-ui-dashboard/src/presentation/widget/insightMenu/DefaultDashboardInsightMenu/DashboardInsightMenuButton.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/insightMenu/DefaultDashboardInsightMenu/DashboardInsightMenuButton.tsx
@@ -1,17 +1,30 @@
-// (C) 2021-2022 GoodData Corporation
+// (C) 2021-2024 GoodData Corporation
 import React, { useCallback } from "react";
 import cx from "classnames";
 import { stringUtils } from "@gooddata/util";
+import { useIntl } from "react-intl";
 
 import { IDashboardInsightMenuButtonProps } from "../types.js";
 import { objRefToString, widgetRef } from "@gooddata/sdk-model";
 
 export const DashboardInsightMenuButton = (props: IDashboardInsightMenuButtonProps): JSX.Element | null => {
     const { isOpen, items, widget, onClick } = props;
+    const intl = useIntl();
 
     const onMenuButtonClick = useCallback(() => {
         onClick();
     }, [onClick]);
+
+    const onKeyDown = useCallback(
+        (event: React.KeyboardEvent<HTMLDivElement>) => {
+            // This enables keyboard interaction events after focus
+            if (event.key === "Enter" || event.key === " ") {
+                event.preventDefault();
+                onClick();
+            }
+        },
+        [onClick],
+    );
 
     if (!items.length) {
         return null;
@@ -34,6 +47,10 @@ export const DashboardInsightMenuButton = (props: IDashboardInsightMenuButtonPro
         <div
             className="dash-item-action-placeholder s-dash-item-action-placeholder"
             onClick={onMenuButtonClick}
+            onKeyDown={onKeyDown}
+            role="button"
+            tabIndex={0}
+            title={intl.formatMessage({ id: "widget.options.menu" })}
         >
             <div className={optionsIconClasses} />
         </div>
