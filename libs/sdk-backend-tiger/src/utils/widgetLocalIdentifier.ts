739bbb9f8d237f066d6fd726f08bff9719d37335
scavnickyj <jakub.scavnicky@gooddata.com>
2024-08-01 09:45:30 +0200

    trivial: use safe optional chaining on window
    risk: nonprod

--- libs/sdk-backend-tiger/src/utils/widgetLocalIdentifier.ts
+++ libs/sdk-backend-tiger/src/utils/widgetLocalIdentifier.ts
@@ -8,7 +8,7 @@ let counter = 0;
  * When running e2e tests, we need to generate deterministic widget local identifiers.
  */
 export function generateWidgetLocalIdentifier() {
-    if (window.useSafeWidgetLocalIdentifiersForE2e === true) {
+    if (window?.useSafeWidgetLocalIdentifiersForE2e === true) {
         return `test_widget_local_identifier_${counter++}`;
     }
 
