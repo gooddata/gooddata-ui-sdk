d9389b9e19597f1fea74c40cb027d629b5c31be0
Petr Benes <petr.benes@gooddata.com>
2024-11-20 19:31:42 +0100

    feat: adjust build scripts to be compatible with rush cache
    The presence of placeholdered __version.ts files will make sure
    that these are correctly cached in rush cache.
    The version will get updated in post-build
    
    risk: low
    JIRA: STL-931

---
+++ libs/sdk-backend-bear/src/__version.ts
@@ -0,0 +1,6 @@
+// (C) 2021-2024 GoodData Corporation
+// The placeholders in this file are replaced by values
+// from package.json right after tsc build
+export const LIB_VERSION = "0.0.0";
+export const LIB_NAME = "LIB_NAME_PLACEHOLDER";
+export const LIB_DESCRIPTION = "LIB_DESCRIPTION_PLACEHOLDER";
