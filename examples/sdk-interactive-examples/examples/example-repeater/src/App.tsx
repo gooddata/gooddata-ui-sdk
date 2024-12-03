cd686c1f17fd75adc5c1d55a990c329cded3e4a1
ivanmjartan <ivan.mjartan@gooddata.com>
2024-12-03 14:35:34 +0100

    chore: fix examples template
    risk: nonprod

--- examples/sdk-interactive-examples/examples/example-repeater/src/App.tsx
+++ examples/sdk-interactive-examples/examples/example-repeater/src/App.tsx
@@ -1,4 +1,4 @@
-// (C) 2019-2024 GoodData Corporation
+// (C) 2024 GoodData Corporation
 import React from "react";
 import { BackendProvider, WorkspaceProvider } from "@gooddata/sdk-ui";
 import { backend } from "./backend.js";
