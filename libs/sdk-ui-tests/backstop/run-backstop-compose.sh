d648acaec20b51d0f4838caee62aca7e654735e6
Petr Benes <petr.benes@gooddata.com>
2024-12-23 12:39:53 +0100

    push

--- libs/sdk-ui-tests/backstop/run-backstop-compose.sh
+++ libs/sdk-ui-tests/backstop/run-backstop-compose.sh
@@ -3,13 +3,8 @@
 # Absolute root dir .. for the docker volumes
 ROOT_DIR=$(echo $(cd $(dirname $0)/.. && pwd -P))
 
-if [[ "$GITHUB_ACTIONS" != "true" ]]; then
-    export USER_UID=$(id -u)
-    export USER_GID=$(id -g)
-else
-    export USER_UID=1000
-    export USER_GID=1000
-fi
+export USER_UID=$(id -u)
+export USER_GID=$(id -g)
 
 cd $ROOT_DIR
 
