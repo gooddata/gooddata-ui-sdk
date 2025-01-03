9ff26e8ee3f77c33c2ac48b4b10f7e669543e588
hackerstanislav <stanislav.hacker@gooddata.com>
2025-01-03 13:28:44 +0100

    fix: notification channel did not store destination_type
    risk: low
    JIRA: F1-985

--- libs/sdk-backend-tiger/src/convertors/tests/NotificationChannelsConvertor.test.ts
+++ libs/sdk-backend-tiger/src/convertors/tests/NotificationChannelsConvertor.test.ts
@@ -1,4 +1,4 @@
-// (C) 2022-2024 GoodData Corporation
+// (C) 2022-2025 GoodData Corporation
 
 import { describe, it, expect } from "vitest";
 
@@ -24,7 +24,6 @@ describe("NotificationChannelsConvertor", () => {
             attributes: {
                 name: "name",
                 inPlatformNotification: "DISABLED",
-                destinationType: "WEBHOOK",
                 destination: {
                     type: "WEBHOOK",
                     token: "token",
