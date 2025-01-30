2101b112ebccac19d86d7454d6867abfbbb91c22
duongthimy <my.duong@gooddata.com>
2025-01-30 16:33:01 +0100

    fix: failed tests because of new dashboard building
    JIRA: QA-23924
    risk: nonprod

--- libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/dashboardShortenMetricName.spec.ts
+++ libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/dashboardShortenMetricName.spec.ts
@@ -1,11 +1,14 @@
 // (C) 2023-2025 GoodData Corporation
 
+import { Dashboard } from "../../tools/dashboards";
 import * as Navigation from "../../tools/navigation";
 import { Widget } from "../../tools/widget";
 
 const LEGEND_NAME_CSS = ".series-name";
 const TOOLTIP_TITLE_CSS = ".gd-viz-tooltip-title";
+const dashboard = new Dashboard();
 
+<<<<<<< HEAD
 describe(
     "Dashboard Shorten Metric Name",
     { tags: ["checklist_integrated_tiger", "checklist_integrated_tiger_releng"] },
@@ -18,6 +21,18 @@ describe(
             const table = new Widget(0).waitTableLoaded();
             table.getTable().assertShortenMetricName(496);
         });
+=======
+describe("Dashboard Shorten Metric Name", { tags: ["checklist_integrated_tiger"] }, () => {
+    beforeEach(() => {
+        Navigation.visit("dashboard/shorten-metric-name");
+        dashboard.isLoaded();
+    });
+
+    it("Table should shorten metric name", () => {
+        const table = new Widget(0).waitTableLoaded();
+        table.getTable().assertShortenMetricName(504);
+    });
+>>>>>>> 696be18769 (fix: failed tests because of new dashboard building)
 
         it("Column chart should shorten metric name in legend", () => {
             const chart = new Widget(1).waitChartLoaded().getChart();
