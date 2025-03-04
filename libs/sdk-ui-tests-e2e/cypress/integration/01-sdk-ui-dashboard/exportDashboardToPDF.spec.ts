d502776d4ee24080b4e002d4eb486facf175b960
nknbich <bich.nguyen@gooddata.com>
2025-03-04 17:01:11 +0700

    feat: enable slide export
    JIRA: QA-24107
    risk: nonprod

--- libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/exportDashboardToPDF.spec.ts
+++ libs/sdk-ui-tests-e2e/cypress/integration/01-sdk-ui-dashboard/exportDashboardToPDF.spec.ts
@@ -12,50 +12,73 @@ const productFilter = new AttributeFilter("Product");
 const exportControl = new Export();
 const widget = new Widget(0);
 const topBar = new TopBar();
+const currentTag = Cypress.env("grepTags");
 
-describe("Export dashboard to pdf", { tags: ["checklist_integrated_tiger_export"] }, () => {
-    it("should export insight to PDF from dashboards", () => {
-        cy.fixture("dashboardInfosForExport").then((data) => {
-            data["validInsightsForTigerPDFExport"].forEach(
-                (dashboardInfo: {
-                    dashboardTitle: string;
-                    dashboardURL: string;
-                    fileName: string;
-                    contents: string;
-                }) => {
-                    Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
-                    widget.waitTableLoaded();
-                    topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
-                    dashboardMenu.toggle().clickExport().clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
-                    exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
-                },
-            );
+describe(
+    "Export dashboard to pdf",
+    { tags: ["checklist_integrated_tiger_export", "checklist_integrated_new_feature"] },
+    () => {
+        it("should export insight to PDF from dashboards", () => {
+            cy.fixture("dashboardInfosForExport").then((data) => {
+                data["validInsightsForTigerPDFExport"].forEach(
+                    (dashboardInfo: {
+                        dashboardTitle: string;
+                        dashboardURL: string;
+                        fileName: string;
+                        contents: string;
+                    }) => {
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
+                        } else {
+                            Navigation.visit(dashboardInfo.dashboardURL);
+                        }
+                        widget.waitTableLoaded();
+                        topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            dashboardMenu
+                                .toggle()
+                                .clickExport()
+                                .clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
+                        } else {
+                            dashboardMenu.toggle().clickOption("Export to PDF");
+                        }
+                        exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
+                    },
+                );
+            });
         });
-    });
 
-    it("should export invalid insight to PDF from dashboards", () => {
-        cy.fixture("dashboardInfosForExport").then((data) => {
-            data["invalidInsight"].forEach(
-                (dashboardInfo: {
-                    dashboardTitle: string;
-                    dashboardURL: string;
-                    fileName: string;
-                    contents: string;
-                }) => {
-                    Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
-                    widget.waitChartLoaded();
-                    topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
-                    dashboardMenu.toggle().clickExport().clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
-                    exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
-                },
-            );
+        it("should export invalid insight to PDF from dashboards", () => {
+            cy.fixture("dashboardInfosForExport").then((data) => {
+                data["invalidInsight"].forEach(
+                    (dashboardInfo: {
+                        dashboardTitle: string;
+                        dashboardURL: string;
+                        fileName: string;
+                        contents: string;
+                    }) => {
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
+                        } else {
+                            Navigation.visit(dashboardInfo.dashboardURL);
+                        }
+                        widget.waitChartLoaded();
+                        topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            dashboardMenu
+                                .toggle()
+                                .clickExport()
+                                .clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
+                        } else {
+                            dashboardMenu.toggle().clickOption("Export to PDF");
+                        }
+                        exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
+                    },
+                );
+            });
         });
-    });
 
-    it(
-        "is able to export dashboard with temporary filter to pdf",
-        { tags: ["checklist_integrated_tiger_export"] },
-        () => {
+        it("is able to export dashboard with temporary filter to pdf", () => {
             cy.fixture("dashboardInfosForExport").then((data) => {
                 data["insightsAfterChangingFilterForPDFExport"].forEach(
                     (dashboardInfo: {
@@ -64,35 +87,57 @@ describe("Export dashboard to pdf", { tags: ["checklist_integrated_tiger_export"
                         fileName: string;
                         contents: string;
                     }) => {
-                        Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
+                        } else {
+                            Navigation.visit(dashboardInfo.dashboardURL);
+                        }
                         topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
                         productFilter.open().selectAttributeWithoutSearch("PhoenixSoft");
                         widget.waitTableLoaded();
 
-                        dashboardMenu.toggle().clickExport().clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            dashboardMenu
+                                .toggle()
+                                .clickExport()
+                                .clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
+                        } else {
+                            dashboardMenu.toggle().clickOption("Export to PDF");
+                        }
                         exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
                     },
                 );
             });
-        },
-    );
-
-    it("should export insight over data points limit to PDF from dashboards", () => {
-        cy.fixture("dashboardInfosForExport").then((data) => {
-            data["insightsManyData"].forEach(
-                (dashboardInfo: {
-                    dashboardTitle: string;
-                    dashboardURL: string;
-                    fileName: string;
-                    contents: string;
-                }) => {
-                    Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
-                    widget.waitChartLoaded();
-                    topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
-                    dashboardMenu.toggle().clickExport().clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
-                    exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
-                },
-            );
         });
-    });
-});
+
+        it("should export insight over data points limit to PDF from dashboards", () => {
+            cy.fixture("dashboardInfosForExport").then((data) => {
+                data["insightsManyData"].forEach(
+                    (dashboardInfo: {
+                        dashboardTitle: string;
+                        dashboardURL: string;
+                        fileName: string;
+                        contents: string;
+                    }) => {
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            Navigation.visit(dashboardInfo.dashboardURL, { enableSlideshowExports: true });
+                        } else {
+                            Navigation.visit(dashboardInfo.dashboardURL);
+                        }
+                        widget.waitChartLoaded();
+                        topBar.dashboardTitleExist().dashboardTitleHasValue(dashboardInfo.dashboardTitle);
+                        if (currentTag === "checklist_integrated_new_feature") {
+                            dashboardMenu
+                                .toggle()
+                                .clickExport()
+                                .clickExportOption(ExportMenu.EXPORT_SNAPSHOT);
+                        } else {
+                            dashboardMenu.toggle().clickOption("Export to PDF");
+                        }
+                        exportControl.expectExportedPDF(dashboardInfo.fileName, dashboardInfo.contents);
+                    },
+                );
+            });
+        });
+    },
+);
