b0527d1fe49e95cba128aeeef48cca12501487cf
Tomáš Gajdoš <tomasgajdos@gmail.com>
2025-03-19 12:59:25 +0100

    add type assertions to JSON imports
    This PR aligns the package to ECMAScript 2022 (ES13) specification, where importing non-javascript resources requires type assertion in ES modules. As of now, the currently published sdk packages are not valid ES modules.
    
    See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules#loading_non-javascript_resources. 
    
    For bundler use (Webpack, Rollup, Vite), code without these assertions is ok, however, if you try to import the package in Node, you run into `TypeError [ERR_IMPORT_ASSERTION_TYPE_MISSING]`. 
    
    Although with the current js landscape, you can expect that your users will use a bundler in production, for use cases such as unit testing, which these days can just run in Node without any preprocessing/bundling or for any server-side use, the current code is not valid.

--- libs/sdk-ui-dashboard/src/presentation/localization/translations.ts
+++ libs/sdk-ui-dashboard/src/presentation/localization/translations.ts
@@ -3,24 +3,24 @@ import merge from "lodash/merge.js";
 import { messagesMap as sdkUiTranslations } from "@gooddata/sdk-ui";
 import { translationUtils } from "@gooddata/util";
 
-import enUS from "./bundles/en-US.json";
-import deDE from "./bundles/de-DE.json";
-import esES from "./bundles/es-ES.json";
-import frFR from "./bundles/fr-FR.json";
-import jaJP from "./bundles/ja-JP.json";
-import nlNL from "./bundles/nl-NL.json";
-import ptBR from "./bundles/pt-BR.json";
-import ptPT from "./bundles/pt-PT.json";
-import zhHans from "./bundles/zh-Hans.json";
-import ruRU from "./bundles/ru-RU.json";
-import itIT from "./bundles/it-IT.json";
-import es419 from "./bundles/es-419.json";
-import frCA from "./bundles/fr-CA.json";
-import enGB from "./bundles/en-GB.json";
-import enAU from "./bundles/en-AU.json";
-import fiFI from "./bundles/fi-FI.json";
-import zhHant from "./bundles/zh-Hant.json";
-import zhHK from "./bundles/zh-HK.json";
+import enUS from "./bundles/en-US.json" with { type: "json" };
+import deDE from "./bundles/de-DE.json" with { type: "json" };
+import esES from "./bundles/es-ES.json" with { type: "json" };
+import frFR from "./bundles/fr-FR.json" with { type: "json" };
+import jaJP from "./bundles/ja-JP.json" with { type: "json" };
+import nlNL from "./bundles/nl-NL.json" with { type: "json" };
+import ptBR from "./bundles/pt-BR.json" with { type: "json" };
+import ptPT from "./bundles/pt-PT.json" with { type: "json" };
+import zhHans from "./bundles/zh-Hans.json" with { type: "json" };
+import ruRU from "./bundles/ru-RU.json" with { type: "json" };
+import itIT from "./bundles/it-IT.json" with { type: "json" };
+import es419 from "./bundles/es-419.json" with { type: "json" };
+import frCA from "./bundles/fr-CA.json" with { type: "json" };
+import enGB from "./bundles/en-GB.json" with { type: "json" };
+import enAU from "./bundles/en-AU.json" with { type: "json" };
+import fiFI from "./bundles/fi-FI.json" with { type: "json" };
+import zhHant from "./bundles/zh-Hant.json" with { type: "json" };
+import zhHK from "./bundles/zh-HK.json" with { type: "json" };
 
 const sdkUiDashboardTranslations: { [locale: string]: Record<string, string> } = {
     "en-US": translationUtils.removeMetadata(enUS),
