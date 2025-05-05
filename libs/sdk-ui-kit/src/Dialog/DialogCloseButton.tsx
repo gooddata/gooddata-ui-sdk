b4b5581bcb4f9747d4bb61128d80b022644480c1
Ivan Nejezchleb <ivan.nejezchleb@gooddata.com>
2025-05-05 18:38:24 +0200

    fix: add default label to dialog close button
    JIRA: LX-729
    risk: low

--- libs/sdk-ui-kit/src/Dialog/DialogCloseButton.tsx
+++ libs/sdk-ui-kit/src/Dialog/DialogCloseButton.tsx
@@ -2,19 +2,32 @@
 import React from "react";
 import { IDialogCloseButtonProps } from "./typings.js";
 import { Button } from "../Button/index.js";
-
-export const DialogCloseButton = React.memo<IDialogCloseButtonProps>(function DialogCloseButton({
+import { useIntl } from "react-intl";
+import { IntlWrapper } from "@gooddata/sdk-ui";
+const DialogCloseButtonCore = React.memo<IDialogCloseButtonProps>(function DialogCloseButton({
     accessibilityConfig,
     onClose,
 }) {
+    const intl = useIntl();
+    const closeButtonAccessibilityConfig = {
+        ariaLabel: intl.formatMessage({ id: "dialogs.closeLabel" }),
+        ...accessibilityConfig?.closeButton,
+    };
+
     return (
         <div className="gd-dialog-close">
             <Button
                 className="gd-button-link gd-button-icon-only gd-icon-cross s-dialog-close-button"
                 value=""
                 onClick={onClose}
-                accessibilityConfig={accessibilityConfig?.closeButton}
+                accessibilityConfig={closeButtonAccessibilityConfig}
             />
         </div>
     );
 });
+
+export const DialogCloseButton: React.FC<IDialogCloseButtonProps> = (props) => (
+    <IntlWrapper>
+        <DialogCloseButtonCore {...props} />
+    </IntlWrapper>
+);
