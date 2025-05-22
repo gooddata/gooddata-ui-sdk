36208c50b0ffbef833c8dfcbc9e35dcd399db534
scavnickyj <jakub.scavnicky@gooddata.com>
2025-05-22 14:09:13 +0200

    fix: use less strict automation context feature flag
    - this shows new alert dialog even when
    date filters do not have local ids
    - filters are not rendered in such case
    
    JIRA: F1-1351
    risk: low

--- libs/sdk-ui-dashboard/src/presentation/widget/insight/configuration/InsightAlerts.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/insight/configuration/InsightAlerts.tsx
@@ -1,14 +1,14 @@
 // (C) 2022-2025 GoodData Corporation
 import React from "react";
 import { IInsightMenuSubmenuComponentProps } from "../../insightMenu/types.js";
-import { useEnableAlertingAutomationFilterContext } from "../../../../model/index.js";
+import { selectEnableAutomationFilterContext, useDashboardSelector } from "../../../../model/index.js";
 import { InsightAlertsOld } from "./InsightAlertsOld.js";
 import { InsightAlertsNew } from "./InsightAlertsNew.js";
 
 export const InsightAlerts: React.FC<IInsightMenuSubmenuComponentProps> = ({ widget, onClose, onGoBack }) => {
-    const enableAutomationFilterContext = useEnableAlertingAutomationFilterContext();
+    const enableAutomationFilters = useDashboardSelector(selectEnableAutomationFilterContext);
 
-    if (enableAutomationFilterContext) {
+    if (enableAutomationFilters) {
         return <InsightAlertsNew widget={widget} onClose={onClose} onGoBack={onGoBack} />;
     }
 
