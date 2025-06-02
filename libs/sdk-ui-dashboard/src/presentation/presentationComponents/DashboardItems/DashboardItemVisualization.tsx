1f711330f7183a28c0bb3b68fb0a21a619827de3
scavnickyj <jakub.scavnicky@gooddata.com>
2025-06-02 15:52:51 +0200

    feat: tag visualization as aria-hidden in export mode
    JIRA: F1-1373
    risk: low

--- libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemVisualization.tsx
+++ libs/sdk-ui-dashboard/src/presentation/presentationComponents/DashboardItems/DashboardItemVisualization.tsx
@@ -3,25 +3,12 @@ import React from "react";
 import cx from "classnames";
 
 import { DashboardItemBase, IDashboardItemBaseProps } from "./DashboardItemBase.js";
-import {
-    selectEnableSnapshotExportAccessibility,
-    selectIsExport,
-    useDashboardSelector,
-} from "../../../model/index.js";
 
 export const DashboardItemVisualization: React.FC<IDashboardItemBaseProps> = ({
     visualizationClassName,
     ...props
 }) => {
-    const isExport = useDashboardSelector(selectIsExport);
-    const isSnapshotAccessibilityEnabled = useDashboardSelector(selectEnableSnapshotExportAccessibility);
-    const ariaHidden = isSnapshotAccessibilityEnabled && isExport ? true : undefined;
-
     return (
-        <DashboardItemBase
-            {...props}
-            ariaHidden={ariaHidden}
-            visualizationClassName={cx("visualization", visualizationClassName)}
-        />
+        <DashboardItemBase {...props} visualizationClassName={cx("visualization", visualizationClassName)} />
     );
 };
