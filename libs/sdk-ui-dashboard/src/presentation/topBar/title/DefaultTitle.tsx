47b5f84aa38875432b1ffde4e01fa91569670ea4
scavnickyj <jakub.scavnicky@gooddata.com>
2025-06-02 16:11:13 +0200

    feat: render dashboard title as H1
    JIRA: F1-1373
    risk: low

--- libs/sdk-ui-dashboard/src/presentation/topBar/title/DefaultTitle.tsx
+++ libs/sdk-ui-dashboard/src/presentation/topBar/title/DefaultTitle.tsx
@@ -3,11 +3,6 @@ import React from "react";
 
 import { TitleWrapper } from "./TitleWrapper.js";
 import { CustomTitleComponent } from "./types.js";
-import {
-    selectEnableSnapshotExportAccessibility,
-    selectIsExport,
-    useDashboardSelector,
-} from "../../../model/index.js";
 import { Typography } from "@gooddata/sdk-ui-kit";
 
 /**
@@ -15,18 +10,12 @@ import { Typography } from "@gooddata/sdk-ui-kit";
  */
 export const DefaultTitle: CustomTitleComponent = (props) => {
     const { title } = props;
-    const snapshotExportAccessibilityEnabled = useDashboardSelector(selectEnableSnapshotExportAccessibility);
-    const isExport = useDashboardSelector(selectIsExport);
 
     return (
         <TitleWrapper>
-            {isExport && snapshotExportAccessibilityEnabled ? (
-                <Typography tagName="h1" className={"s-gd-dashboard-title s-dash-title dash-title static"}>
-                    {title}
-                </Typography>
-            ) : (
-                <div className={"s-gd-dashboard-title s-dash-title dash-title static"}>{title}</div>
-            )}
+            <Typography tagName="h1" className={"s-gd-dashboard-title s-dash-title dash-title static"}>
+                {title}
+            </Typography>
         </TitleWrapper>
     );
 };
