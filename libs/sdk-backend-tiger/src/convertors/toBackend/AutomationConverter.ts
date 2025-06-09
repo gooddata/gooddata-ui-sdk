def6a86731c6556b90efa4ed830e95df2b09add8
zdenekornst <ornstz@me.com>
2025-06-09 11:09:09 +0200

    chore: generate api
    risk: low

--- libs/sdk-backend-tiger/src/convertors/toBackend/AutomationConverter.ts
+++ libs/sdk-backend-tiger/src/convertors/toBackend/AutomationConverter.ts
@@ -4,7 +4,7 @@ import {
     RelativeOperatorEnum,
     ArithmeticMeasureOperatorEnum,
     JsonApiAutomationIn,
-    JsonApiAutomationInAttributesAlert,
+    JsonApiAutomationPatchAttributesAlert,
 } from "@gooddata/api-client-tiger";
 import {
     IAutomationAlert,
@@ -135,7 +135,7 @@ export function convertAutomation(
 const convertAlert = (
     alert: IAutomationAlert,
     enableAutomationFilterContext: boolean,
-): JsonApiAutomationInAttributesAlert => {
+): JsonApiAutomationPatchAttributesAlert => {
     const { condition, execution } = alert;
 
     const { filters: convertedFilters } = convertAfmFilters(
