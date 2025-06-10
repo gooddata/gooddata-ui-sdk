f534aa8306fea5b036c380fb793c8161a1cee79f
lucydoan <lucy.doan@gooddata.com>
2025-06-10 11:32:04 +0700

    fix: update behavior of loading
    JIRA: QA-24956
    risk: nonprod

--- libs/sdk-ui-tests-e2e/cypress/tools/table.ts
+++ libs/sdk-ui-tests-e2e/cypress/tools/table.ts
@@ -1,4 +1,4 @@
-// (C) 2021-2024 GoodData Corporation
+// (C) 2021-2025 GoodData Corporation
 
 import { TotalTypes } from "./enum/TotalTypes";
 
@@ -21,6 +21,12 @@ export class Table {
 
     public waitLoadStarted() {
         this.getElement().find(".s-loading").should("exist");
+        return this;
+    }
+
+    public isLoaded(): this {
+        this.getElement().find(".s-loading").should("not.exist");
+        cy.get(".s-pivot-table .s-pivot-table-column-group-header").should("exist");
         return this;
     }
 
