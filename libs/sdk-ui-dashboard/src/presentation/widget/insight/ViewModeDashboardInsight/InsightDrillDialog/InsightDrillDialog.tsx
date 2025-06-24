cbefdc02be09f9f205b0879bf50e89cb58f9cc14
NestorEncinas <xesto05@gmail.com>
2025-06-24 11:48:20 +0200

    fix: raw exports dashboard filters
    risk: low
    JIRA: LX-1117

--- libs/sdk-ui-dashboard/src/presentation/widget/insight/ViewModeDashboardInsight/InsightDrillDialog/InsightDrillDialog.tsx
+++ libs/sdk-ui-dashboard/src/presentation/widget/insight/ViewModeDashboardInsight/InsightDrillDialog/InsightDrillDialog.tsx
@@ -1,4 +1,4 @@
-// (C) 2020-2024 GoodData Corporation
+// (C) 2020-2025 GoodData Corporation
 import React, { useCallback, useState } from "react";
 import { idRef, IInsight, insightTitle, IInsightWidget } from "@gooddata/sdk-model";
 import {
