95aaad555fe70e816d9e7e095142382d42679fcc
ivanmjartan <ivan.mjartan@gooddata.com>
2025-06-24 15:02:16 +0200

    test

--- libs/sdk-ui-dashboard/vite.config.ts
+++ libs/sdk-ui-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        environment: "jsdom",
+        environment: "happy-dom",
         setupFiles: "./setupTests.ts",
         include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
         exclude: ["**/node_modules/**", "**/esm/**"],
