f1b9099a5ef21bed6c5cca77e354d2960db146d1
xMort <petr.dolejsi@gooddata.com>
2025-07-04 16:20:18 +0200

    fix: failing e22
    JIRA: LX-1235
    risk: low

--- libs/sdk-ui-tests-e2e/cypress/tools/enum/DropZone.ts
+++ libs/sdk-ui-tests-e2e/cypress/tools/enum/DropZone.ts
@@ -1,3 +1,5 @@
+// (C) 2025 GoodData Corporation
+
 // eslint-disable-next-line header/header
 export const enum DropZone {
     PREV = ".attr-filter-dropzone.prev",
@@ -5,7 +7,7 @@ export const enum DropZone {
 }
 
 export const enum WidgetDropZone {
-    PREV = ".dropzone.prev",
-    NEXT = ".dropzone.next",
+    PREV = ".dropzone.prev,.gd-grid-layout-dropzone.prev",
+    NEXT = ".dropzone.next,.gd-grid-layout-dropzone.next",
     LAST = ".s-last-drop-position",
 }
