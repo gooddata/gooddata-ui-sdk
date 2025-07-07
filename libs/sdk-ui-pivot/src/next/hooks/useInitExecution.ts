cc14dbc7c428a4ba443c9cec05750fd1d393b821
kandl <matyas.kandl@gooddata.com>
2025-07-07 09:21:27 +0200

    feat: pivot table
    First steps for new pivot table implementation.
    
    Add new 2 dimensional data (rows-columns-values) representation
    to DataViewFacade, suitable for table components.
    
    Implement core layer of the pivot table:
        - use ag-grid server side row model
        - cover initialization, data fetching and infinite scrolling
        - cover base use cases, pivoting and transposition
        - support text-wrapping and copying to clipboard
    
    risk: low
    JIRA: F1-1552

---
+++ libs/sdk-ui-pivot/src/next/hooks/useInitExecution.ts
@@ -0,0 +1,39 @@
+// (C) 2025 GoodData Corporation
+import { IExecutionResult } from "@gooddata/sdk-backend-spi";
+import {
+    useBackendStrict,
+    useWorkspaceStrict,
+    useCancelablePromise,
+    GoodDataSdkError,
+} from "@gooddata/sdk-ui";
+import { getExecutionProps, getMeasureGroupDimension } from "../mapProps/props.js";
+import { getExecution } from "../execution/getExecution.js";
+import { IPivotTableNextProps } from "../types/public.js";
+
+/**
+ * @alpha
+ */
+export const useInitExecution = (props: IPivotTableNextProps) => {
+    const backend = useBackendStrict(props.backend, "useInitExecution");
+    const workspace = useWorkspaceStrict(props.workspace, "useInitExecution");
+    const measureGroupDimension = getMeasureGroupDimension(props);
+    const { columns, rows, measures, filters } = getExecutionProps(props);
+
+    return useCancelablePromise<IExecutionResult, GoodDataSdkError>(
+        {
+            promise: (signal) =>
+                getExecution({
+                    backend,
+                    workspace,
+                    columns,
+                    rows,
+                    measures,
+                    filters,
+                    measureGroupDimension,
+                    signal,
+                }),
+            enableAbortController: true,
+        },
+        [backend, workspace, columns, rows, measures, measureGroupDimension],
+    );
+};
