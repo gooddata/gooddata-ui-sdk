e94f3760e0786ea3b2cb69960d5bec0e15751dd2
xMort <petr.dolejsi@gooddata.com>
2025-07-09 13:04:36 +0200

    fix: refactor row end drop zone to be more readable
    JIRA: LX-1293
    risk: low

--- libs/sdk-ui-dashboard/src/presentation/flexibleLayout/rowEndHotspotHelper.ts
+++ libs/sdk-ui-dashboard/src/presentation/flexibleLayout/rowEndHotspotHelper.ts
@@ -1,14 +1,11 @@
 // (C) 2022-2025 GoodData Corporation
 
-import { useMemo } from "react";
-import { ScreenSize } from "@gooddata/sdk-model";
+import { ScreenSize, ISettings } from "@gooddata/sdk-model";
 
 import { DASHBOARD_LAYOUT_GRID_COLUMNS_COUNT } from "../../_staging/dashboard/flexibleLayout/config.js";
 import { IDashboardLayoutItemFacade } from "../../_staging/dashboard/flexibleLayout/index.js";
 import { getContainerHeight } from "../../_staging/layout/sizing.js";
-import { useDashboardSelector, selectSettings, ExtendedDashboardWidget } from "../../model/index.js";
-import { useScreenSize } from "../dashboard/components/DashboardScreenSizeContext.js";
-import { useDashboardItemPathAndSize } from "../dashboard/components/DashboardItemPathAndSizeContext.js";
+import { ExtendedDashboardWidget } from "../../model/index.js";
 
 export function getRemainingWidthInRow(
     item: IDashboardLayoutItemFacade<unknown>,
@@ -18,33 +15,29 @@ export function getRemainingWidthInRow(
     const parentWidth = item.section().layout().size()?.gridWidth ?? DASHBOARD_LAYOUT_GRID_COLUMNS_COUNT;
     const rows = item.section().items().asGridRows(screen);
     const lastRowLength = rows[rowIndex].reduce(
-        (acc, item) => acc + item.sizeForScreenWithFallback(screen).gridWidth,
+        (acc, rowItem) => acc + rowItem.sizeForScreenWithFallback(screen).gridWidth,
         0,
     );
     return parentWidth - lastRowLength;
 }
 
-export function useRemainingHeightInColumn(
+export function getRemainingHeightInColumn(
     item: IDashboardLayoutItemFacade<ExtendedDashboardWidget | unknown>,
-    isLastInColumn: boolean,
+    screen: ScreenSize,
+    parentLayoutItem: IDashboardLayoutItemFacade<ExtendedDashboardWidget> | undefined,
+    settings: ISettings,
 ): number {
-    const screen = useScreenSize();
-    const settings = useDashboardSelector(selectSettings);
-    const { layoutItem } = useDashboardItemPathAndSize();
-
     // Do not count the remaining height of the column for each row in the column, just the last.
-    return useMemo(() => {
-        if (!isLastInColumn) {
-            return 0;
-        }
+    if (!item.isLastInSection() || parentLayoutItem === undefined) {
+        return 0;
+    }
 
-        const container = layoutItem?.raw();
-        const containerContentHeight = getContainerHeight(container!, screen, settings);
+    const container = parentLayoutItem.raw();
+    const containerContentHeight = getContainerHeight(container!, screen, settings);
 
-        const parentHeight = item.section().layout().size()?.gridHeight ?? 0;
-        const remainingHeight = parentHeight - containerContentHeight;
+    const parentHeight = item.section().layout().size()?.gridHeight ?? 0;
+    const remainingHeight = parentHeight - containerContentHeight;
 
-        // sanitize for possible negative values caused by wrong metadata
-        return remainingHeight > 0 ? remainingHeight : 0;
-    }, [item, isLastInColumn, layoutItem, screen, settings]);
+    // sanitize for possible negative values caused by wrong metadata
+    return remainingHeight > 0 ? remainingHeight : 0;
 }
