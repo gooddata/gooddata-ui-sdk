f460c518cde1a5992709e3d2dff764f9f6bf9854
pkoza <peter.koza@gooddata.com>
2025-07-22 20:02:17 +0200

    fix: add missing stories
    risk: low

---
+++ libs/sdk-ui-tests/stories/visual-regression/ui/UiCheckbox.stories.tsx
@@ -0,0 +1,33 @@
+// (C) 2025 GoodData Corporation
+
+import { ComponentTable, propCombinationsFor, UiCheckbox, UiCheckboxProps } from "@gooddata/sdk-ui-kit";
+import React from "react";
+import { wrapWithTheme } from "../themeWrapper.js";
+
+export default {
+    title: "15 Ui/UiCheckbox",
+};
+
+const propCombination = propCombinationsFor({} as UiCheckboxProps);
+
+const checked = propCombination("checked", [false, true]);
+const indeterminate = propCombination("indeterminate", [true, false], { checked: true });
+
+const UiCheckboxExample: React.FC<{ showCode?: boolean }> = ({ showCode }) => (
+    <div className="screenshot-target">
+        <ComponentTable
+            rowsBy={[checked, indeterminate]}
+            Component={UiCheckbox}
+            codeSnippet={showCode ? "UiCheckbox" : undefined}
+        />
+    </div>
+);
+
+export const Default = () => <UiCheckboxExample />;
+Default.parameters = { kind: "default", screenshot: true };
+
+export const Themed = () => wrapWithTheme(<UiCheckboxExample />);
+Themed.parameters = { kind: "themed", screenshot: true };
+
+export const Interface = () => <UiCheckboxExample showCode />;
+Interface.parameters = { kind: "interface" };
---
+++ libs/sdk-ui-tests/stories/visual-regression/ui/UiSkeleton.stories.tsx
@@ -0,0 +1,39 @@
+// (C) 2025 GoodData Corporation
+
+import { ComponentTable, propCombinationsFor, UiSkeleton, UiSkeletonProps } from "@gooddata/sdk-ui-kit";
+import React from "react";
+import { wrapWithTheme } from "../themeWrapper.js";
+
+export default {
+    title: "15 Ui/UiSkeleton",
+};
+
+const propCombination = propCombinationsFor({
+    itemWidth: 100,
+    direction: "row",
+    itemHeight: 15,
+    itemsCount: 1,
+} as UiSkeletonProps);
+
+const itemWidth = propCombination("itemWidth", [100], { itemsCount: 3 });
+const differentItemWidths = propCombination("itemWidth", [[50, 100, 150]], { itemsCount: 3 });
+const itemsGap = propCombination("itemsGap", [30], { itemsCount: 3, itemWidth: 85 });
+const itemHeight = propCombination("itemHeight", [25], { itemWidth: 320 });
+const column = propCombination("direction", ["column"], { itemsCount: 3, itemHeight: 50, itemWidth: 15 });
+
+const UiSkeletonExample: React.FC<{ showCode?: boolean }> = ({ showCode }) => (
+    <div className="screenshot-target">
+        <ComponentTable
+            rowsBy={[itemWidth, differentItemWidths, itemsGap, itemHeight, column]}
+            Component={UiSkeleton}
+            codeSnippet={showCode ? "UiSkeleton" : undefined}
+        />
+    </div>
+);
+
+export const Default = () => <UiSkeletonExample />;
+Default.parameters = { kind: "default", screenshot: true };
+export const Themed = () => wrapWithTheme(<UiSkeletonExample />);
+Themed.parameters = { kind: "themed", screenshot: true };
+export const Interface = () => <UiSkeletonExample showCode />;
+Interface.parameters = { kind: "interface" };
