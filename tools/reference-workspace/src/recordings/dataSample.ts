da162403b6db129748185ff185e6913593ba5814
scavnickyj <jakub.scavnicky@gooddata.com>
2025-07-24 14:21:08 +0200

    test: refresh execution recordings for stories
    risk: low

--- tools/reference-workspace/src/recordings/dataSample.ts
+++ tools/reference-workspace/src/recordings/dataSample.ts
@@ -1,6 +1,6 @@
 // (C) 2025 GoodData Corporation
 
-/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2025-07-23T09:19:31.748Z; */
+/* THIS FILE WAS AUTO-GENERATED USING MOCK HANDLING TOOL; YOU SHOULD NOT EDIT THIS FILE; GENERATE TIME: 2025-07-24T13:08:30.903Z; */
 
 import df_label_f_activity_subject from "./metadata/displayForms/label.f_activity.subject/elements.json" with { type: "json" };
 import df_label_f_product_product_name from "./metadata/displayForms/label.f_product.product.name/elements.json" with { type: "json" };
